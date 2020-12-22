6c4317be1df582ed68918394934281172359c88c
JasonLandbridge <15127381+JasonLandbridge@users.noreply.github.com>
2020-12-22 17:02:23 +0100

    NPM fix

--- src/WebAPI/ClientApp/src/components/General/FileSize.vue
+++ src/WebAPI/ClientApp/src/components/General/FileSize.vue
@@ -19,13 +19,13 @@ export default class FileSize extends Vue {
 	 */
 	get formattedString(): string {
 		const bytes = this.size;
-		const kib = kib || false;
+		const kib = false;
 		const decimals = 2;
 
 		if (bytes === 0) return '0 Bytes';
 		if (isNaN(bytes) && !isFinite(bytes)) return 'Unknown';
 		const k = kib ? 1024 : 1000;
-		const dm = decimals != null && !isNaN(decimals) && decimals >= 0 ? decimals : 2;
+		const dm = !isNaN(decimals) && decimals >= 0 ? decimals : 2;
 		const sizes = kib
 			? ['Bytes', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB', 'BiB']
 			: ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB', 'BB'];

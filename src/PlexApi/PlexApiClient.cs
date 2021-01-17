6575626e81dafef3c6489a53d8b72d9fa99f010c
JasonLandbridge <15127381+JasonLandbridge@users.noreply.github.com>
2021-01-17 16:33:38 +0100

    Managed to get the PlexApi requests to work with Polly, no on failure it will retry max 3 times.

--- src/PlexApi/PlexApiClient.cs
+++ src/PlexApi/PlexApiClient.cs
@@ -8,15 +8,16 @@ using PlexRipper.Domain;
 using PlexRipper.PlexApi.Api;
 using PlexRipper.PlexApi.Config.Converters;
 using Polly;
-using Polly.Retry;
 using RestSharp;
 using RestSharp.Serialization.Xml;
 using RestSharp.Serializers.SystemTextJson;
 
 namespace PlexRipper.PlexApi
 {
-    public class PlexApiClient : RestClient
+    public class PlexApiClient
     {
+        private readonly IRestClient _client;
+
         public static JsonSerializerOptions SerializerOptions =>
             new JsonSerializerOptions
             {
@@ -26,70 +27,100 @@ namespace PlexRipper.PlexApi
                 Converters = { new LongToDateTime() },
             };
 
-        private readonly AsyncRetryPolicy _policy;
+        private readonly IAsyncPolicy<IRestResponse> _policy2;
 
-        public PlexApiClient()
+        private static readonly List<HttpStatusCode> invalidStatusCodes = new List<HttpStatusCode>
         {
-            this.UseSystemTextJson(SerializerOptions);
-            this.UseDotNetXmlSerializer();
-            ThrowOnAnyError = true;
-            Timeout = 10000;
+            HttpStatusCode.BadGateway,
+            HttpStatusCode.Unauthorized,
+            HttpStatusCode.InternalServerError,
+            HttpStatusCode.RequestTimeout,
+            HttpStatusCode.BadRequest,
+            HttpStatusCode.Forbidden,
+            HttpStatusCode.GatewayTimeout,
+            HttpStatusCode.ServiceUnavailable,
+        };
 
-            _policy = Policy
+        public PlexApiClient(IRestClient client)
+        {
+            _client = client;
+            _client.UseSystemTextJson(SerializerOptions);
+            _client.UseDotNetXmlSerializer();
+            _client.Timeout = 10000;
+
+            _client.ThrowOnAnyError = true;
+        }
+
+        public async Task<Result<T>> SendRequestAsync<T>(RestRequest request)
+        {
+            IRestResponse<T> response = null;
+
+            request = AddHeaders(request);
+
+            var policyResult = await Policy
                 .Handle<WebException>()
+                .OrResult<IRestResponse<T>>(x => invalidStatusCodes.Contains(x.StatusCode) || x.StatusCode == 0)
                 .WaitAndRetryAsync(3, retryAttempt =>
                     {
                         var timeToWait = TimeSpan.FromSeconds(retryAttempt * 1);
                         Log.Warning($"Waiting {timeToWait.TotalSeconds} seconds before retrying again.");
                         return timeToWait;
-                    },
-                    (exception, _, _) => { Log.Warning($"An exception occured at {exception.Source} with message {exception.Message}"); });
-        }
+                    }, (result, span) => { Log.Error(result.Result.ErrorException); }
+                ).ExecuteAndCaptureAsync(() => _client.ExecuteAsync<T>(request));
 
-        public async Task<Result<T>> SendRequestAsync<T>(RestRequest request)
-        {
-            request = AddHeaders(request);
-
-            var response = await _policy
-                .ExecuteAsync(async () =>
+            if (policyResult.Outcome == OutcomeType.Successful)
+            {
+                response = policyResult.Result;
+            }
+            else
+            {
+                response = new RestResponse<T>
                 {
-                    try
-                    {
-                        return await ExecuteAsync<T>(request);
-                    }
-                    catch (Exception e)
-                    {
-                        Log.Error(e);
-                        throw;
-                    }
-                });
+                    Request = request,
+                    ErrorException = policyResult.FinalException,
+                };
+            }
 
             var metaData = GetMetadata(response);
             if (response.IsSuccessful)
             {
                 var result = Result.Ok(response.Data)
-                    .WithReason(new Success($"Request to {response.ResponseUri} was successful!")
-                        .WithMetadata(metaData))
+                    .WithReason(new Success($"Request to {response.ResponseUri} was successful!"))
                     .LogDebug();
                 Log.Verbose($"Response was: {response.Content}");
                 return result;
             }
 
-            return Result.Fail(new ExceptionalError(response.ErrorException).WithMessage(
-                        $"PlexApi Error: Error on request to {request.Resource} ({response.StatusCode}) - {response.Content}")
-                    .WithMetadata(metaData))
-                .LogError();
+            if (response.ErrorException != null)
+            {
+                return Result.Fail(new ExceptionalError(response.ErrorException).WithMessage(
+                            $"PlexApi Error: Error on request to {response.ResponseUri} ({response.StatusCode}) - {response.Content}")
+                        .WithMetadata(metaData))
+                    .LogError();
+            }
+
+            return Result.Fail(new Error($"Time-out error on request {request.Resource}").WithMetadata(metaData)).LogError();
         }
 
         public async Task<byte[]> SendImageRequestAsync(RestRequest request)
         {
             request = AddHeaders(request);
+            var _policy = Policy
+                .Handle<Exception>()
+                .WaitAndRetryAsync(3, retryAttempt =>
+                    {
+                        var timeToWait = TimeSpan.FromSeconds(retryAttempt * 1);
+                        Log.Warning($"Waiting {timeToWait.TotalSeconds} seconds before retrying again.");
+                        return timeToWait;
+                    }
+                );
+
             return await _policy
                 .ExecuteAsync(async () =>
                 {
                     try
                     {
-                        var response = await Task.Run(() => DownloadData(request));
+                        var response = await Task.Run(() => _client.DownloadData(request));
                         return response;
                     }
                     catch (Exception e)

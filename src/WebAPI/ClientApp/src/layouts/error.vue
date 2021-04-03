97dfb886ffce1b2c132e66b783501ec06141e9b5
JasonLandbridge <15127381+JasonLandbridge@users.noreply.github.com>
2021-04-03 10:41:08 +0200

    Improved styling of the error page

--- src/WebAPI/ClientApp/src/layouts/error.vue
+++ src/WebAPI/ClientApp/src/layouts/error.vue
@@ -1,7 +1,21 @@
 <template>
-	<v-app dark>
-		<h1>{{ error }}</h1>
-		<NuxtLink to="/"> Home page </NuxtLink>
+	<v-app dark class="no-background">
+		<v-container>
+			<v-row justify="center">
+				<v-col cols="auto">
+					<h1 class="text-left">
+						<pre><code class="no-background">{{ errorFormatted }}</code></pre>
+					</h1>
+				</v-col>
+			</v-row>
+			<v-row justify="center">
+				<v-col cols="auto">
+					<h1>
+						<NuxtLink to="/">Go back to home page </NuxtLink>
+					</h1>
+				</v-col>
+			</v-row>
+		</v-container>
 	</v-app>
 </template>
 
@@ -13,6 +27,10 @@ export default class ErrorLayout extends Vue {
 	@Prop()
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	error!: any;
+
+	get errorFormatted(): string {
+		return JSON.stringify(this.error, null, 4);
+	}
 
 	layout(): string {
 		return 'error';

ec125bdaea5340b2f49c82830657d42058d1c930
JasonLandbridge <15127381+JasonLandbridge@users.noreply.github.com>
2021-05-24 13:37:01 +0200

    Updated Fluent Results

--- tests/DownloadManager.Tests/UnitTests/DownloadManagerTests.cs
+++ tests/DownloadManager.Tests/UnitTests/DownloadManagerTests.cs
@@ -1,12 +1,13 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using PlexRipper.Application.PlexDownloads;
+using PlexRipper.Application.PlexServers;
 using PlexRipper.BaseTests;
 using PlexRipper.Domain;
 using Shouldly;
 using WireMock.Server;
 using Xunit;
 using Xunit.Abstractions;
+using DownloadManager = PlexRipper.DownloadManager.DownloadManager;
 
 namespace DownloadManager.Tests.UnitTests
 {
@@ -55,26 +56,6 @@ namespace DownloadManager.Tests.UnitTests
             // Assert
             result.IsFailed.ShouldBeTrue();
         }
-
-        [Fact]
-        public async Task AddToDownloadQueueAsync_ShouldAddOnlyValidDownloadTasks_WhenAMixOfValidDownloadTasksAreGiven()
-        {
-            //Arrange
-            var downloadManager = Container.GetDownloadManager;
-            var downloadTasks = FakeData.MovieDownloadTasks().Generate(2);
-            downloadTasks.AddRange(new List<DownloadTask>
-            {
-                new(),
-                new(),
-            });
-
-            // Act
-            var result = await downloadManager.AddToDownloadQueueAsync(downloadTasks);
-            var dbTasks = await Container.Mediator.Send(new GetAllDownloadTasksQuery());
-
-            // Assert
-            result.IsSuccess.ShouldBeTrue();
-            dbTasks.Value.Count.ShouldBeEquivalentTo(2);
-        }
+        
     }
 }

2498bee0d2de2eea482100e31019a106eb75ea84
JasonLandbridge <15127381+JasonLandbridge@users.noreply.github.com>
2021-09-18 14:20:29 +0200

    Exception fix

--- src/Domain/Common/Converters/FolderTypeConverter.cs
+++ src/Domain/Common/Converters/FolderTypeConverter.cs
@@ -4,16 +4,16 @@ using System.Text.Json.Serialization;
 
 namespace PlexRipper.Domain
 {
-    public class FolderTypeConverter: JsonConverter<FolderType>
+public class FolderTypeConverter: JsonConverter<FolderType>
+{
+    public override FolderType Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
-        public override FolderType Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-        {
-            return reader.GetString().ToFolderType();
-        }
-
-        public override void Write(Utf8JsonWriter writer, FolderType value, JsonSerializerOptions options)
-        {
-            writer.WriteStringValue(value.ToFolderTypeString());
-        }
+        return reader.GetString().ToFolderType();
     }
+
+    public override void Write(Utf8JsonWriter writer, FolderType value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToFolderTypeString());
+    }
+}
 }

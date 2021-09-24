d1e7f0c471dc5bc3dee3d2de67be1b9bd0ad994c
JasonLandbridge <15127381+JasonLandbridge@users.noreply.github.com>
2021-09-24 21:53:47 +0200

    Prevent wrapping of the server drawer servers

--- src/WebAPI/ClientApp/src/components/Navigation/ServerDrawer.vue
+++ src/WebAPI/ClientApp/src/components/Navigation/ServerDrawer.vue
@@ -5,7 +5,7 @@
 			<template v-if="plexServers.length > 0">
 				<v-expansion-panel v-for="(server, i) in plexServers" :key="i">
 					<v-expansion-panel-header>
-						<v-row align="center" no-gutters>
+						<v-row align="center" no-gutters class="flex-nowrap">
 							<v-col cols="auto">
 								<div class="server-name"><status :value="server.status.isSuccessful" /> {{ server.name }}</div>
 							</v-col>

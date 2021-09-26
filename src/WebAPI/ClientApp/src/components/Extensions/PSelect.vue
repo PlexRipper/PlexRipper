8736b8a962ebfe196d5ddf80bd7c96c2bcf16b8f
JasonLandbridge <15127381+JasonLandbridge@users.noreply.github.com>
2021-09-26 20:01:20 +0200

    Tweaked PSelect

--- src/WebAPI/ClientApp/src/components/Extensions/PSelect.vue
+++ src/WebAPI/ClientApp/src/components/Extensions/PSelect.vue
@@ -1,8 +1,25 @@
+<template>
+	<v-select
+		:dense="dense"
+		:filled="filled"
+		:outlined="outlined"
+		:color="color"
+		:hide-details="hideDetails"
+		:menu-props="menuProps"
+		v-bind="$attrs"
+		v-on="$listeners"
+	>
+		<template #item="slotProps">
+			<slot name="item" v-bind="slotProps"></slot>
+		</template>
+	</v-select>
+</template>
+
 <script lang="ts">
 import { Component, Prop } from 'vue-property-decorator';
 import { VSelect } from 'vuetify/lib';
 
-@Component
+@Component<PSelect>({})
 export default class PSelect extends VSelect {
 	@Prop({ required: false, type: Boolean, default: true })
 	readonly dense!: boolean;

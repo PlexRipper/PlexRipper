63a2d326c79127dcafec520a1479b030fa64f6db
JasonLandbridge <15127381+JasonLandbridge@users.noreply.github.com>
2021-09-27 18:46:28 +0200

    Installed I18n eslint helper to detect untranslated text

--- src/WebAPI/ClientApp/.eslintrc.js
+++ src/WebAPI/ClientApp/.eslintrc.js
@@ -4,7 +4,13 @@ module.exports = {
 		browser: true,
 		node: true,
 	},
-	extends: ['@nuxtjs/eslint-config-typescript', 'prettier', 'plugin:prettier/recommended', 'plugin:nuxt/recommended'],
+	extends: [
+		'@nuxtjs/eslint-config-typescript',
+		'prettier',
+		'plugin:prettier/recommended',
+		'plugin:nuxt/recommended',
+		'plugin:@intlify/vue-i18n/recommended',
+	],
 	plugins: ['prettier'],
 	// add your custom rules here
 	rules: {
@@ -12,5 +18,21 @@ module.exports = {
 		'vue/no-v-html': 'off',
 		'no-use-before-define': 'off',
 		'no-extend-native': 'off',
+		'@intlify/vue-i18n/no-raw-text': [
+			'warn',
+			{
+				ignoreNodes: ['md-icon', 'v-icon'],
+				ignorePattern: '^[-#:()&]+$',
+				ignoreText: ['EUR', 'HKD', 'USD'],
+			},
+		],
+	},
+	settings: {
+		'vue-i18n': {
+			localeDir: './src/lang/*.{json,json5,yaml,yml}', // extension is glob formatting!
+			// Specify the version of `vue-i18n` you are using.
+			// If not specified, the message will be parsed twice.
+			messageSyntaxVersion: '^9.0.0',
+		},
 	},
 };

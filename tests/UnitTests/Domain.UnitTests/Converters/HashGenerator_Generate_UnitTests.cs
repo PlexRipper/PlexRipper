fa592cd0facba4f6fea74f1a686610c5b4f8387d
JasonLandbridge <15127381+JasonLandbridge@users.noreply.github.com>
2021-11-04 17:45:22 +0100

    Created MD5 hash generator for DownloadTasks to compare with

---
+++ tests/UnitTests/Domain.UnitTests/Converters/HashGenerator_Generate_UnitTests.cs
@@ -0,0 +1,46 @@
+﻿using Logging;
+using PlexRipper.BaseTests;
+using PlexRipper.Domain.Converters;
+using Shouldly;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Domain.UnitTests.Converters
+{
+    public class HashGenerator_Generate_UnitTests
+    {
+        public HashGenerator_Generate_UnitTests(ITestOutputHelper output)
+        {
+            Log.SetupTestLogging(output);
+        }
+
+        [Fact]
+        public void ShouldGenerateAValidMD5Hash_WhenGivenAValidDownloadTask()
+        {
+            // Arrange
+            var downloadTask = FakeData.GetMovieDownloadTask().Generate();
+
+            // Act
+            var hash = HashGenerator.Generate(downloadTask);
+
+            // Assert
+            hash.Length.ShouldBe(32);
+        }
+
+        [Fact]
+        public void ShouldGenerateTheValidMD5Hash_WhenGivenAValidDownloadTaskTwice()
+        {
+            // Arrange
+            var downloadTask = FakeData.GetMovieDownloadTask().Generate();
+
+            // Act
+            var hash1 = HashGenerator.Generate(downloadTask);
+            var hash2 = HashGenerator.Generate(downloadTask);
+
+            // Assert
+            hash1.Length.ShouldBe(32);
+            hash2.Length.ShouldBe(32);
+            hash1.ShouldBe(hash2);
+        }
+    }
+}

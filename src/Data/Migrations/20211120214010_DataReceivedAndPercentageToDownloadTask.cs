d6b51f6c5f3b029e7cd627f2dc2eeebf78bdad46
JasonLandbridge <15127381+JasonLandbridge@users.noreply.github.com>
2021-11-20 22:57:45 +0100

    Added DataReceived and Percentage to DownloadTasks

---
+++ src/Data/Migrations/20211120214010_DataReceivedAndPercentageToDownloadTask.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PlexRipper.Data.Migrations
+{
+    public partial class DataReceivedAndPercentageToDownloadTask : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "DataReceived",
+                table: "DownloadTasks",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Percentage",
+                table: "DownloadTasks",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataReceived",
+                table: "DownloadTasks");
+
+            migrationBuilder.DropColumn(
+                name: "Percentage",
+                table: "DownloadTasks");
+        }
+    }
+}

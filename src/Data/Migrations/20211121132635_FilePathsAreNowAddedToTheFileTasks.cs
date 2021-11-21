82626fe1af65e9276005760994e742ab07ad7dcb
JasonLandbridge <15127381+JasonLandbridge@users.noreply.github.com>
2021-11-21 14:53:05 +0100

    Added filepaths to fileTasks migration

---
+++ src/Data/Migrations/20211121132635_FilePathsAreNowAddedToTheFileTasks.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PlexRipper.Data.Migrations
+{
+    public partial class FilePathsAreNowAddedToTheFileTasks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FilePathsCompressed",
+                table: "FileTasks",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FilePathsCompressed",
+                table: "FileTasks");
+        }
+    }
+}

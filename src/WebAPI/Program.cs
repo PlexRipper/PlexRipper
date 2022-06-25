e338483c3ae37562847062826da344987e60535e
JasonLandbridge <xsruhqq8mn@protonmail.com>
2022-06-25 16:05:10 +0200

    Upgraded all .net5 to .net6 reference in proj files

--- src/WebAPI/Program.cs
+++ src/WebAPI/Program.cs
@@ -1,31 +1,31 @@
-﻿// This application entry point is based on ASP.NET Core new project templates and is included
-// as a starting point for app host configuration.
-// This file may need updated according to the specific scenario of the application being upgraded.
-// For more information on ASP.NET Core hosting, see https://docs.microsoft.com/aspnet/core/fundamentals/host/web-host
-
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using FluentResults;
+using Logging;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using PlexRipper.WebAPI.Common;
+using Serilog.Events;
 
 namespace PlexRipper.WebAPI
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            Log.SetupLogging(LogEventLevel.Verbose);
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+            try
+            {
+                PlexRipperHost.Setup().Build().Run();
+            }
+            catch (Exception e)
+            {
+                Result.Fail(new ExceptionalError(e)).LogFatal();
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                Log.CloseAndFlush();
+            }
+        }
     }
 }

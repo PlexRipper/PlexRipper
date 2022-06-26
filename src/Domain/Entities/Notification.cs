66512dec9332824d685526a1b9fb0d27877791f5
JasonLandbridge <xsruhqq8mn@protonmail.com>
2022-06-26 14:14:21 +0200

    Corrected the column ordering of the database entities

--- src/Domain/Entities/Notification.cs
+++ src/Domain/Entities/Notification.cs
@@ -2,37 +2,36 @@
 using System.ComponentModel.DataAnnotations.Schema;
 using FluentResults;
 
-namespace PlexRipper.Domain
+namespace PlexRipper.Domain;
+
+public class Notification : BaseEntity
 {
-    public class Notification : BaseEntity
+    #region Constructor
+
+    public Notification() { }
+
+    public Notification(IError error)
     {
-        #region Constructor
-
-        public Notification() { }
-
-        public Notification(IError error)
-        {
-            Level = NotificationLevel.Error;
-            CreatedAt = DateTime.Now;
-            Message = error.Message;
-        }
-
-        #endregion
-
-        #region Properties
-
-        [Column(Order = 2)]
-        public DateTime CreatedAt { get; set; }
-
-        [Column(Order = 3)]
-        public bool Hidden { get; set; }
-
-        [Column(Order = 1)]
-        public NotificationLevel Level { get; set; }
-
-        [Column(Order = 3)]
-        public string Message { get; set; }
-
-        #endregion
+        Level = NotificationLevel.Error;
+        CreatedAt = DateTime.Now;
+        Message = error.Message;
     }
+
+    #endregion
+
+    #region Properties
+
+    [Column(Order = 1)]
+    public NotificationLevel Level { get; set; }
+
+    [Column(Order = 2)]
+    public DateTime CreatedAt { get; set; }
+
+    [Column(Order = 3)]
+    public string Message { get; set; }
+
+    [Column(Order = 4)]
+    public bool Hidden { get; set; }
+
+    #endregion
 }

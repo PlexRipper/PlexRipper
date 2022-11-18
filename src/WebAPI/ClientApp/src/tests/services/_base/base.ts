d32359c04c8afa75aa3e0e3727b6740fdce82acf
JasonLandbridge <jasonlandbridge@protonmail.com>
2022-11-18 20:45:53 +0100

    Setup Observer-spy Added several setup tests for services

---
+++ src/WebAPI/ClientApp/src/tests/services/_base/base.ts
@@ -0,0 +1,37 @@
+import { Context } from '@nuxt/types';
+import Log, { LogLevel } from 'consola';
+import MockAdapter from 'axios-mock-adapter';
+import Axios from 'axios-observable';
+import { MockConfig } from '@mock';
+
+export * from '@hirez_io/observer-spy';
+
+export function baseVars(): { ctx: Context; mock: MockAdapter; config: MockConfig } {
+	let ctx, mock;
+	return {
+		ctx,
+		mock,
+		config: {},
+	};
+}
+
+export function baseSetup(): { ctx: Context } {
+	const ctx: Context = {
+		$config: {
+			nodeEnv: 'TESTING',
+			version: '1.0',
+		},
+	} as Context;
+	process.env.NODE_ENV = 'dev';
+	process.client = true;
+	// Minimum LogLevel displayed
+	Log.level = LogLevel.Trace;
+	return {
+		ctx,
+	};
+}
+
+export function getAxiosMock() {
+	// @ts-ignore
+	return new MockAdapter(Axios, { onNoMatch: 'throwException' });
+}

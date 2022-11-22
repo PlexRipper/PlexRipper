c8ca75e3e06b4d7ddb74880015f25c10065b7e58
JasonLandbridge <jasonlandbridge@protonmail.com>
2022-11-22 12:38:10 +0100

    Fixed the getServer with ids and added some tests for it

--- src/WebAPI/ClientApp/src/tests/services/server-service/get-servers.test.ts
+++ src/WebAPI/ClientApp/src/tests/services/server-service/get-servers.test.ts
@@ -1,12 +1,9 @@
-import Log from 'consola';
 import { describe, beforeAll, expect, test } from '@jest/globals';
 import { take } from 'rxjs/operators';
-import { of } from 'rxjs';
 import { baseSetup, baseVars, getAxiosMock, subscribeSpyTo } from '@services-test-base';
-import { DOWNLOAD_RELATIVE_PATH, PLEX_SERVER_RELATIVE_PATH } from '@api-urls';
+import { PLEX_SERVER_RELATIVE_PATH } from '@api-urls';
 import { generatePlexServers, generateResultDTO } from '@mock';
 import { ServerService } from '@service';
-import ISetupResult from '@interfaces/service/ISetupResult';
 
 describe('ServerService.getServers()', () => {
 	let { ctx, mock, config } = baseVars();
@@ -20,7 +17,7 @@ describe('ServerService.getServers()', () => {
 		mock = getAxiosMock();
 	});
 
-	test('Should return success and complete when setup is run', async () => {
+	test('Should return all servers when servers are set in the store', async () => {
 		// Arrange
 		config = {
 			plexServerCount: 3,
@@ -39,5 +36,31 @@ describe('ServerService.getServers()', () => {
 		// Assert
 		expect(serversResult.receivedComplete()).toBe(true);
 		expect(serversResult.getFirstValue()).toEqual(servers);
+	});
+
+	test('Should return all servers by id when servers are set in the store', async () => {
+		// Arrange
+		config = {
+			seed: 4587,
+			plexServerCount: 6,
+		};
+		const servers = generatePlexServers(config);
+		mock.onGet(PLEX_SERVER_RELATIVE_PATH).reply(200, generateResultDTO(servers));
+		const setup$ = ServerService.setup(ctx);
+		const serversResult$ = ServerService.getServers([2, 3, 4]).pipe(take(1));
+
+		// Act
+		const setupResult = subscribeSpyTo(setup$);
+		await setupResult.onComplete();
+		const serversResult = subscribeSpyTo(serversResult$);
+		await serversResult.onComplete();
+
+		// Assert
+		expect(serversResult.receivedComplete()).toBe(true);
+		const firstValue = serversResult.getFirstValue();
+		expect(firstValue.length).toEqual(3);
+		expect(firstValue[0].id).toEqual(2);
+		expect(firstValue[1].id).toEqual(3);
+		expect(firstValue[2].id).toEqual(4);
 	});
 });

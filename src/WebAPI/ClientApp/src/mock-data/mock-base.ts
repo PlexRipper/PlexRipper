0daa893865227796d11e24413597567fb88997db
JasonLandbridge <jasonlandbridge@protonmail.com>
2022-11-24 15:34:03 +0100

    Added a test for LibraryServer.GetServerByLibrary() and it is now working as expected Added Mock Functionality for PlexLibrary object generating

--- src/WebAPI/ClientApp/src/mock-data/mock-base.ts
+++ src/WebAPI/ClientApp/src/mock-data/mock-base.ts
@@ -1,6 +1,7 @@
 import { cloneDeep } from 'lodash-es';
 import { faker } from '@faker-js/faker';
 import { MockConfig } from '@mock/interfaces';
+import { PlexMediaType } from '@dto/mainApi';
 
 export function checkConfig(config: MockConfig | null = null): MockConfig {
 	if (config === null || config === undefined) {
@@ -8,20 +9,28 @@ export function checkConfig(config: MockConfig | null = null): MockConfig {
 	}
 
 	const newConfig: MockConfig = cloneDeep(config);
-	if (!config.hasOwnProperty('plexServerCount')) {
+	if (!hasConfigProperty(config, 'plexServerCount')) {
 		newConfig.plexServerCount = 5;
 	}
 
-	if (!config.hasOwnProperty('seed')) {
+	if (!hasConfigProperty(config, 'seed')) {
 		newConfig.seed = 1234;
 	}
 
-	if (!config.hasOwnProperty('plexAccountCount')) {
+	if (!hasConfigProperty(config, 'plexAccountCount')) {
 		newConfig.plexAccountCount = 2;
 	}
 
-	if (!config.hasOwnProperty('firstTimeSetup')) {
+	if (!hasConfigProperty(config, 'firstTimeSetup')) {
 		newConfig.firstTimeSetup = false;
+	}
+
+	if (!hasConfigProperty(config, 'plexLibraryCount')) {
+		newConfig.plexLibraryCount = 5;
+	}
+
+	if (!hasConfigProperty(config, 'plexLibraryTypes')) {
+		newConfig.plexLibraryTypes = [PlexMediaType.Movie, PlexMediaType.TvShow];
 	}
 
 	faker.seed(config.seed);
@@ -38,4 +47,8 @@ export function incrementSeed(increment: number) {
 		return;
 	}
 	setSeed(faker.seed() + increment);
+}
+
+function hasConfigProperty(config: MockConfig, key: keyof MockConfig) {
+	return config.hasOwnProperty(key);
 }

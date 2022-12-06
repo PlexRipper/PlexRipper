95bc98c69192946830fa0327a7b63dec94ec022c
JasonLandbridge <jasonlandbridge@protonmail.com>
2022-12-06 21:50:09 +0100

    Added migration for the IPv4 and PortFix property for PlexServerConnection

--- src/Data/Migrations/20221206204904_AddedIpv4AndPortFixToPlexServerConnection.Designer.cs
+++ src/Data/Migrations/20221206204904_AddedIpv4AndPortFixToPlexServerConnection.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PlexRipper.Data;
 
@@ -10,9 +11,10 @@ using PlexRipper.Data;
 namespace PlexRipper.Data.Migrations
 {
     [DbContext(typeof(PlexRipperDbContext))]
-    partial class PlexRipperDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221206204904_AddedIpv4AndPortFixToPlexServerConnection")]
+    partial class AddedIpv4AndPortFixToPlexServerConnection
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.6");
---
+++ src/Data/Migrations/20221206204904_AddedIpv4AndPortFixToPlexServerConnection.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlexRipper.Data.Migrations
+{
+    public partial class AddedIpv4AndPortFixToPlexServerConnection : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "StatusMessage",
+                table: "PlexServerStatuses",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true)
+                .Annotation("Relational:ColumnOrder", 3);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "StatusCode",
+                table: "PlexServerStatuses",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Relational:ColumnOrder", 2);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PlexServerId",
+                table: "PlexServerStatuses",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Relational:ColumnOrder", 5);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PlexServerConnectionId",
+                table: "PlexServerStatuses",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Relational:ColumnOrder", 6);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "LastChecked",
+                table: "PlexServerStatuses",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT")
+                .Annotation("Relational:ColumnOrder", 4);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsSuccessful",
+                table: "PlexServerStatuses",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "INTEGER")
+                .Annotation("Relational:ColumnOrder", 1);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IPv6",
+                table: "PlexServerConnections",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "INTEGER")
+                .Annotation("Relational:ColumnOrder", 7)
+                .OldAnnotation("Relational:ColumnOrder", 6);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IPv4",
+                table: "PlexServerConnections",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false)
+                .Annotation("Relational:ColumnOrder", 6);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "PortFix",
+                table: "PlexServerConnections",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false)
+                .Annotation("Relational:ColumnOrder", 8);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IPv4",
+                table: "PlexServerConnections");
+
+            migrationBuilder.DropColumn(
+                name: "PortFix",
+                table: "PlexServerConnections");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StatusMessage",
+                table: "PlexServerStatuses",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true)
+                .OldAnnotation("Relational:ColumnOrder", 3);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "StatusCode",
+                table: "PlexServerStatuses",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Relational:ColumnOrder", 2);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PlexServerId",
+                table: "PlexServerStatuses",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Relational:ColumnOrder", 5);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PlexServerConnectionId",
+                table: "PlexServerStatuses",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Relational:ColumnOrder", 6);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "LastChecked",
+                table: "PlexServerStatuses",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT")
+                .OldAnnotation("Relational:ColumnOrder", 4);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsSuccessful",
+                table: "PlexServerStatuses",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "INTEGER")
+                .OldAnnotation("Relational:ColumnOrder", 1);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IPv6",
+                table: "PlexServerConnections",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "INTEGER")
+                .Annotation("Relational:ColumnOrder", 6)
+                .OldAnnotation("Relational:ColumnOrder", 7);
+        }
+    }
+}

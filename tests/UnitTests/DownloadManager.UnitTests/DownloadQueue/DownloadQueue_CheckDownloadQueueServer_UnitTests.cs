c70ce72462ddba524ee34c3478a0e005ac97dad5
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-03-10 20:33:54 +0100

    Added unit test for getting the next download task when there is a nested Queued task

--- tests/UnitTests/DownloadManager.UnitTests/DownloadQueue/DownloadQueue_CheckDownloadQueueServer_UnitTests.cs
+++ tests/UnitTests/DownloadManager.UnitTests/DownloadQueue/DownloadQueue_CheckDownloadQueueServer_UnitTests.cs
@@ -169,4 +169,42 @@ public class DownloadQueue_CheckDownloadQueue_UnitTests : BaseUnitTest<DownloadQ
         result.IsSuccess.ShouldBeTrue();
         result.Value.Id.ShouldBe(downloadTasks[1].Children[0].Children[0].Children[0].Id);
     }
+
+    [Fact]
+    public async Task ShouldHaveNextQueuedDownloadTask_WhenNestedInsideATvShowsDownloadTasksWithDownloadFinished()
+    {
+        // Arrange
+        await SetupDatabase(config =>
+        {
+            config.PlexServerCount = 1;
+            config.PlexLibraryCount = 1;
+            config.TvShowCount = 10;
+            config.TvShowDownloadTasksCount = 2;
+            config.TvShowSeasonDownloadTasksCount = 2;
+            config.TvShowEpisodeDownloadTasksCount = 5;
+        });
+
+        var downloadTasks = await DbContext.DownloadTasks.AsTracking().IncludeDownloadTasks().IncludeByRoot().ToListAsync();
+        downloadTasks.SetToDownloadFinished();
+
+        foreach (var tvShowDownloadTask in downloadTasks)
+            tvShowDownloadTask.Children[0].Children[1].DownloadStatus = DownloadStatus.Queued;
+
+        await DbContext.SaveChangesAsync();
+
+        mock.Mock<IDownloadTaskScheduler>().Setup(x => x.StartDownloadTaskJob(It.IsAny<int>(), It.IsAny<int>())).ReturnOk();
+        mock.SetupMediator(It.IsAny<GetDownloadTasksByPlexServerIdQuery>)
+            .ReturnsAsync((GetDownloadTasksByPlexServerIdQuery query, CancellationToken _) =>
+                Result.Ok(downloadTasks.Where(x => x.PlexServerId == query.PlexServerId).ToList()));
+        mock.SetupMediator(It.IsAny<GetPlexServerNameByIdQuery>)
+            .ReturnsAsync((GetPlexServerNameByIdQuery query, CancellationToken _) =>
+                Result.Ok(downloadTasks.FirstOrDefault(x => x.PlexServerId == query.Id).Title));
+
+        // Act
+        var result = await _sut.CheckDownloadQueueServer(1);
+
+        // Assert
+        result.IsSuccess.ShouldBeTrue();
+        result.Value.Id.ShouldBe(downloadTasks[0].Children[0].Children[1].Children[0].Id);
+    }
 }

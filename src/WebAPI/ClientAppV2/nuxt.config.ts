0913176d45b90f31ab825c82a075dcef72e02f94
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-03-16 14:44:57 +0100

    Imported Quasar UI Enabled Typechecking during compile time in nuxt

--- src/WebAPI/ClientAppV2/nuxt.config.ts
+++ src/WebAPI/ClientAppV2/nuxt.config.ts
@@ -1,4 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    devServer: {
+        port: 3001,
+    },
+    modules: [
+        'nuxt-quasar-ui'
+    ],
+    quasar: {
+        // Plugins: https://quasar.dev/quasar-plugins
+        plugins: [],
+        // Truthy values requires `sass@1.32.12`.
+        sassVariables: false,
+        // Requires `@quasar/extras` package
+        extras: {
+            // string | null: Auto-import roboto font. https://quasar.dev/style/typography#default-font
+            font: null,
+            // string[]: Auto-import webfont icons. Usage: https://quasar.dev/vue-components/icon#webfont-usage
+            fontIcons: [],
+            // string[]: Auto-import svg icon collections. Usage: https://quasar.dev/vue-components/icon#svg-usage
+            svgIcons: [],
+            // string[]: Auto-import animations from 'animate.css'. Usage: https://quasar.dev/options/animations#usage
+            animations: [],
+        }
+    },
+    typescript: {
+        // Doc: https://typescript.nuxtjs.org/guide/setup.html#configuration
+        // Packages,  @types/node, vue-tsc and typescript are required
+        typeCheck: true,
+    }
 })

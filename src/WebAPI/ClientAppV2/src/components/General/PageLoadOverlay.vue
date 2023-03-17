35dac9b6f6ac40221ce4334f84b1f05aca31644f
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-03-17 13:06:50 +0100

    Imported Logo and changed the name of PageOverlay to PageLoadOverlay

---
+++ src/WebAPI/ClientAppV2/src/components/General/PageLoadOverlay.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="q-pa-md">
+        <div class="logo-square">
+            <logo :size="256"/>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+import {defineProps} from "vue";
+
+const props = defineProps<{ value: boolean }>();
+
+</script>
+<style>
+.logo-square {
+    width: 256px;
+    height: 256px;
+    animation: rotate-animation 3s cubic-bezier(0.25, 0.46, 0.45, 0.94) infinite;
+}
+
+@keyframes rotate-animation {
+    from {
+        transform: rotateY(0deg);
+    }
+
+    to {
+        transform: rotateY(359deg);
+    }
+}
+</style>

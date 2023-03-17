c08fd5a0baea5108abee42695bbdf7ed38d3d178
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-03-17 13:52:56 +0100

    Copied AppBar and DarkModeToggle back to old project

---
+++ src/WebAPI/ClientApp/src/components/General/DarkModeToggle.vue
@@ -0,0 +1,17 @@
+<template>
+	<v-btn class="mx-2" icon @click="setDarkMode">
+		<v-icon v-if="$vuetify.theme.dark">mdi-white-balance-sunny</v-icon>
+		<v-icon v-else>mdi-moon-waxing-crescent</v-icon>
+	</v-btn>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class DarkModeToggle extends Vue {
+	setDarkMode(): void {
+		this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+	}
+}
+</script>

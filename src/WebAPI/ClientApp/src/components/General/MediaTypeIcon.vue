421be327c221f4872e544a4725ae05c825c3fd42
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-03-17 21:58:45 +0100

    Copied components back to Nuxt2 project

---
+++ src/WebAPI/ClientApp/src/components/General/MediaTypeIcon.vue
@@ -0,0 +1,34 @@
+<script lang="ts">
+// eslint-disable-next-line import/named
+import Vue, { VNode, CreateElement } from 'vue';
+import { RenderContext } from 'vue/types/options';
+import Convert from '@mediaOverview/MediaTable/types/Convert';
+
+export default Vue.extend({
+	functional: true,
+	props: {
+		mediaType: {
+			type: String,
+			default: '',
+		},
+		size: {
+			type: Number,
+			default: 24,
+		},
+	},
+	render(h: CreateElement, context: RenderContext): VNode {
+		return h(
+			'v-icon',
+			{
+				class: {
+					...(context.data.staticClass && {
+						[context.data.staticClass]: true,
+					}),
+				},
+				props: { size: context.props.size },
+			},
+			Convert.mediaTypeToIcon(context.props.mediaType),
+		);
+	},
+});
+</script>
---
+++ src/WebAPI/ClientApp/src/components/General/Status.vue
@@ -0,0 +1,17 @@
+<template>
+	<status-indicator class="mx-2" :status="value ? 'positive' : 'negative'" :pulse="pulse" />
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component
+export default class Status extends Vue {
+	@Prop({ required: true, type: Boolean })
+	readonly value!: boolean;
+
+	get pulse(): boolean {
+		return this.value;
+	}
+}
+</script>

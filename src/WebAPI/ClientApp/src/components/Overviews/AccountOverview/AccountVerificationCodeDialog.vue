9f8d04d2ce2406c8a0900dbbdc13efdb1fdfa028
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-03-22 17:49:21 +0100

    Copied back the Overview components

---
+++ src/WebAPI/ClientApp/src/components/Overviews/AccountOverview/AccountVerificationCodeDialog.vue
@@ -0,0 +1,94 @@
+<template>
+	<v-dialog :value="dialog" persistent :max-width="500">
+		<v-card :max-width="600">
+			<v-card-title>{{ $t('components.account-verification-code-dialog.title') }}</v-card-title>
+			<v-card-subtitle>{{ $t('components.account-verification-code-dialog.sub-title') }}</v-card-subtitle>
+			<v-card-text>
+				<!--	Verification Code input	-->
+				<v-row justify="center">
+					<v-col cols="auto">
+						<CodeInput
+							:loading="false"
+							class="input"
+							@change="onChange"
+							@complete="onComplete"
+							@keyup.enter="onEnter"
+						/>
+					</v-col>
+				</v-row>
+				<v-row v-if="errors.length > 0" justify="center">
+					<v-col cols="auto">
+						<span style="color: red; font-weight: bold">{{
+							$t('components.account-verification-code-dialog.error')
+						}}</span>
+					</v-col>
+				</v-row>
+			</v-card-text>
+			<v-card-actions>
+				<!--	Submit button	-->
+				<v-row justify="center">
+					<v-col cols="auto">
+						<CancelButton @click="closeDialog" />
+					</v-col>
+					<v-spacer />
+					<v-col cols="auto">
+						<ConfirmButton :disabled="code.length < 6" @click="submitCode" />
+					</v-col>
+				</v-row>
+			</v-card-actions>
+		</v-card>
+	</v-dialog>
+</template>
+
+<script lang="ts">
+import Log from 'consola';
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
+import CodeInput from 'vue-verification-code-input';
+import { IError } from '@dto/mainApi';
+
+@Component<AccountVerificationCodeDialog>({ components: { CodeInput } })
+export default class AccountVerificationCodeDialog extends Vue {
+	@Prop({ required: false, type: Boolean, default: false })
+	readonly dialog!: boolean;
+
+	@Prop({ required: true, type: Array as () => IError[] })
+	readonly errors!: IError[];
+
+	@Watch('errors')
+	onChildChanged(val: string) {
+		// If an error appears, then clear the code
+		if (val.length > 0) {
+			this.code = '0';
+		}
+	}
+
+	code: string = '0';
+
+	onChange(v) {
+		this.code = v;
+	}
+
+	onComplete(v) {
+		Log.info('onComplete ', v);
+	}
+
+	closeDialog() {
+		this.$emit('close');
+	}
+
+	submitCode() {
+		this.$emit('submit', this.code);
+	}
+
+	onEnter() {
+		Log.info('Enter pressed');
+	}
+}
+</script>
+
+<style>
+.react-code-input > input[data-v-e1087700]:focus {
+	border: 1px solid #f00 !important;
+	caret-color: #f00 !important;
+}
+</style>
---
+++ src/WebAPI/ClientApp/src/components/Overviews/NotificationsDrawer.vue
@@ -0,0 +1,95 @@
+<template>
+	<v-navigation-drawer :value="showDrawer" :width="450" right app clipped class="no-background">
+		<vue-scroll>
+			<!-- Render All Notifications	-->
+			<template v-if="notifications.length > 0">
+				<v-alert
+					v-for="notification in getVisibleNotifications"
+					:key="notification.id"
+					:min-width="200"
+					:max-width="450"
+					:type="notification.level.toLowerCase()"
+					dense
+					dismissible
+					outlined
+					elevation="10"
+					@click="hideNotification(notification.id)"
+				>
+					<span class="text-wrap" style="overflow-wrap: anywhere">
+						{{ notification.message }}
+					</span>
+				</v-alert>
+			</template>
+			<!-- No Notifications	-->
+			<template v-else>
+				<v-list>
+					<v-list-item>
+						<v-list-item-icon>
+							<v-icon>mdi-check-circle-outline</v-icon>
+						</v-list-item-icon>
+						<v-list-item-content>
+							<v-list-item-title> {{ $t('components.notifications-drawer.no-notifications') }}</v-list-item-title>
+						</v-list-item-content>
+					</v-list-item>
+				</v-list>
+			</template>
+		</vue-scroll>
+		<!-- Menu items -->
+		<template v-if="notifications.length > 0" #append>
+			<v-list>
+				<v-list-item @click="clearAllNotifications">
+					<v-list-item-icon>
+						<v-icon>mdi-close-circle</v-icon>
+					</v-list-item-icon>
+					<v-list-item-content>
+						<v-list-item-title>
+							{{ $t('components.notifications-drawer.clear-notifications') }}
+						</v-list-item-title>
+					</v-list-item-content>
+				</v-list-item>
+			</v-list>
+		</template>
+	</v-navigation-drawer>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import { useSubscription } from '@vueuse/rxjs';
+import { NotificationService } from '@service';
+import { NotificationDTO } from '@dto/mainApi';
+import notificationService from '~/service/notificationService';
+
+@Component<NotificationsDrawer>({})
+export default class NotificationsDrawer extends Vue {
+	private notifications: NotificationDTO[] = [];
+
+	@Prop({ required: true, type: Boolean })
+	readonly showDrawer!: boolean;
+
+	get getVisibleNotifications(): NotificationDTO[] {
+		return this.notifications?.filter((x) => !x.hidden) ?? [];
+	}
+
+	hideNotification(id: number): void {
+		NotificationService.hideNotification(id);
+	}
+
+	clearAllNotifications() {
+		NotificationService.clearAllNotifications();
+		this.$emit('cleared');
+	}
+
+	mounted(): void {
+		useSubscription(
+			notificationService.getNotifications().subscribe((value) => {
+				this.notifications = value;
+			}),
+		);
+	}
+}
+</script>
+<style scoped lang="scss">
+.v-alert:hover {
+	cursor: pointer;
+}
+</style>

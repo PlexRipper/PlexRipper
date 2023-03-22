1f03925aa4a185d04b2b450c46ba0f42097a688b
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-03-22 21:04:57 +0100

    Copied back

---
+++ src/WebAPI/ClientApp/src/components/General/ValidIcon.vue
@@ -0,0 +1,30 @@
+<template>
+	<v-tooltip top :disabled="valid">
+		<template #activator="{ on }">
+			<v-icon class="valid-icon" :size="size" :color="valid ? 'green' : 'red'" dark v-on="on">
+				{{ getIcon }}
+			</v-icon>
+		</template>
+		<span>{{ text }}</span>
+	</v-tooltip>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class ValidIcon extends Vue {
+	@Prop({ required: true, type: Boolean })
+	readonly valid!: boolean;
+
+	@Prop({ required: true, type: String })
+	readonly text!: string;
+
+	@Prop({ required: false, type: Number, default: 30 })
+	readonly size!: number;
+
+	get getIcon(): string {
+		return this.valid ? 'mdi-check-circle-outline' : 'mdi-alert-circle';
+	}
+}
+</script>

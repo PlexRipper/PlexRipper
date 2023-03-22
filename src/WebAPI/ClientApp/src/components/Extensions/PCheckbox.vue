c92a873394f874cfdc3e216b54003de2ef1ef31b
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-03-22 13:16:42 +0100

    Copied back the Extensions

---
+++ src/WebAPI/ClientApp/src/components/Extensions/PCheckbox.vue
@@ -0,0 +1,17 @@
+<template>
+	<v-checkbox :value="value" color="red" class="ma-3 pt-0" hide-details @click="handleInput()" />
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class PCheckbox extends Vue {
+	@Prop({ required: true, type: Boolean })
+	readonly value!: boolean;
+
+	handleInput(): void {
+		this.$emit('input', !this.value);
+	}
+}
+</script>

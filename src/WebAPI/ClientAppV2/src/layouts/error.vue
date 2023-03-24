96ec3ba7204dc9bbdc0ffeffef6c9758abc8f21d
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-03-24 21:08:21 +0100

    Fixed the error layout and migrated it to Quasar and Vue 3

--- src/WebAPI/ClientAppV2/src/layouts/error.vue
+++ src/WebAPI/ClientAppV2/src/layouts/error.vue
@@ -1,24 +1,26 @@
 <template>
 	<q-layout view="hHh lpR fFf" dark class="no-background">
-		<v-container>
-			<v-row justify="center">
-				<v-col cols="auto">
-					<h1>{{ $t('pages.error.header') }}</h1>
-					<h1 class="text-left">
-						<pre><code class="no-background">{{ errorFormatted }}</code></pre>
-					</h1>
-				</v-col>
-			</v-row>
-			<v-row justify="center">
-				<v-col cols="auto">
-					<h1>
-						<NuxtLink to="/">
-							{{ $t('pages.error.return-link') }}
-						</NuxtLink>
-					</h1>
-				</v-col>
-			</v-row>
-		</v-container>
+		<q-page-container>
+			<q-page>
+				<q-row justify="center">
+					<q-col cols="auto">
+						<h1>{{ $t('pages.error.header') }}</h1>
+						<h1 class="text-left">
+							<pre><code class="no-background">{{ errorFormatted }}</code></pre>
+						</h1>
+					</q-col>
+				</q-row>
+				<q-row justify="center">
+					<q-col cols="auto">
+						<h1>
+							<NuxtLink to="/">
+								{{ $t('pages.error.return-link') }}
+							</NuxtLink>
+						</h1>
+					</q-col>
+				</q-row>
+			</q-page>
+		</q-page-container>
 	</q-layout>
 </template>
 

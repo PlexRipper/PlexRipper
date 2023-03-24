6552c22a34113c673d250580adc2a95b642a0aa9
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-03-24 18:03:57 +0100

    Copied back

---
+++ src/WebAPI/ClientApp/src/pages/settings/debug.vue
@@ -0,0 +1,12 @@
+<template>
+	<page-container>
+		<debug-section />
+	</page-container>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class DebugSettings extends Vue {}
+</script>

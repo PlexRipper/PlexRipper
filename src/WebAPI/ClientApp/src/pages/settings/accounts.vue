070e520692a3e189d63de33d00a0374e77b4fcda
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-03-24 16:40:00 +0100

    Copied back

---
+++ src/WebAPI/ClientApp/src/pages/settings/accounts.vue
@@ -0,0 +1,15 @@
+<template>
+	<page-container>
+		<p-section>
+			<template #header> {{ $t('pages.settings.accounts.header') }} </template>
+			<account-overview />
+		</p-section>
+	</page-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Accounts extends Vue {}
+</script>

e09e4c419d9435a16f04c52472be637ece59aaa3
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-03-25 12:34:25 +0100

    Copied back

---
+++ src/WebAPI/ClientApp/src/components/General/VerticalButton.vue
@@ -0,0 +1,30 @@
+<template>
+	<v-btn class="vertical-button" text tile :height="height" :disabled="disabled" :width="width" @click="$emit('click', $event)">
+		<div class="vertical-button-content">
+			<v-icon x-large>{{ icon }}</v-icon>
+			<span class="vertical-button-label">{{ label }}</span>
+		</div>
+	</v-btn>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component
+export default class VerticalButton extends Vue {
+	@Prop({ required: true, type: String })
+	readonly icon!: string;
+
+	@Prop({ required: true, type: String })
+	readonly label!: string;
+
+	@Prop({ required: true, type: Number })
+	readonly height!: number;
+
+	@Prop({ required: false, type: Number })
+	readonly width!: number;
+
+	@Prop({ required: false, type: Boolean, default: false })
+	readonly disabled!: boolean;
+}
+</script>

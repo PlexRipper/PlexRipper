04ec843f4928d6aa3550a4532c9f3d9eb60b164b
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-03-25 10:14:25 +0100

    Copied back

---
+++ src/WebAPI/ClientApp/src/pages/music/_musicId.vue
@@ -0,0 +1,16 @@
+<template>
+	<page-container>
+		<v-row>
+			<v-col>
+				<h1>{{ $t('pages.music._musicId.header') }}</h1>
+			</v-col>
+		</v-row>
+	</page-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Music extends Vue {}
+</script>
---
+++ src/WebAPI/ClientApp/src/pages/tvshows/_id.vue
@@ -0,0 +1,17 @@
+<template>
+	<media-overview :library-id="libraryId" />
+</template>
+
+<script lang="ts">
+import Log from 'consola';
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class TvShowsDetail extends Vue {
+	libraryId: number = +this.$route.params.id;
+
+	mounted() {
+		Log.info('route', this.$route);
+	}
+}
+</script>

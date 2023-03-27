2ac6ea7ffcb10c80524edc15a5ea01b1f03de4f5
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-03-27 13:26:58 +0200

    Changed from [id] to [libraryId]

--- src/WebAPI/ClientAppV2/src/pages/tvshows/[libraryId].vue
+++ src/WebAPI/ClientAppV2/src/pages/tvshows/[libraryId].vue
@@ -1,5 +1,5 @@
 <template>
-	<media-overview :library-id="+$route.params.id" :media-type="PlexMediaType.TvShow" />
+	<media-overview :library-id="+$route.params.libraryId" :media-type="PlexMediaType.TvShow" />
 </template>
 <script setup lang="ts">
 import { PlexMediaType } from '@dto/mainApi';

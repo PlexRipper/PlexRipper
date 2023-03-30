b02a9f3ab003a619e74c1a0d1ac260f7989b4abb
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-03-30 11:48:10 +0200

    Added EF Core migration for the NaturalSort

---
+++ src/Data/Migrations/20230330094624_AddNaturalSortOnADatabaseLevel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlexRipper.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNaturalSortOnADatabaseLevel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase(
+                collation: "NATURALSORT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase(
+                oldCollation: "NATURALSORT");
+        }
+    }
+}

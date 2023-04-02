4a1d7e2553538912110bb9c4d46f8605a2ecce8d
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-04-02 12:52:27 +0200

    Migrated the setup page to Vue 3

--- src/WebAPI/ClientAppV2/src/components/Views/Setup/NavigationBar.vue
+++ src/WebAPI/ClientAppV2/src/components/Views/Setup/NavigationBar.vue
@@ -1,46 +1,38 @@
 <template>
-	<v-row justify="center">
-		<v-col cols="auto" style="position: absolute; left: 0">
+	<q-row justify="center">
+		<q-col cols="auto" style="position: absolute; left: 0">
 			<dark-mode-toggle />
-		</v-col>
-		<v-spacer />
-		<v-col v-if="!isLast" cols="2">
-			<NavigationPreviousButton class="mx-2" :disabled="disableBack" cy="setup-page-previous-button" @click="back" />
-		</v-col>
-		<v-col v-if="!isLast" cols="2">
-			<NavigationNextButton class="mx-2" :disabled="disableNext" cy="setup-page-next-button" @click="next" />
-		</v-col>
-		<v-col v-else cols="3">
-			<NavigationFinishSetupButton class="mx-2" cy="setup-page-skip-setup-button" @click="finishSetup" />
-		</v-col>
-		<v-spacer />
-	</v-row>
+		</q-col>
+		<q-space />
+		<q-col v-if="!isLast" cols="2">
+			<NavigationPreviousButton
+				class="mx-2"
+				:disabled="disableBack"
+				cy="setup-page-previous-button"
+				@click="$emit('back')" />
+		</q-col>
+		<q-col v-if="!isLast" cols="2">
+			<NavigationNextButton class="mx-2" :disabled="disableNext" cy="setup-page-next-button" @click="$emit('next')" />
+		</q-col>
+		<q-col v-else cols="3">
+			<NavigationFinishSetupButton class="mx-2" cy="setup-page-skip-setup-button" @click="$emit('finish')" />
+		</q-col>
+		<q-space />
+	</q-row>
 </template>
 
-<script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue';
 
-@Component
-export default class NavigationBar extends Vue {
-	@Prop({ required: true, type: Boolean })
-	readonly disableBack!: boolean;
+defineProps<{
+	disableBack: boolean;
+	disableNext: boolean;
+	isLast: boolean;
+}>();
 
-	@Prop({ required: true, type: Boolean })
-	readonly disableNext!: boolean;
-
-	@Prop({ required: true, type: Boolean })
-	readonly isLast!: boolean;
-
-	back(): void {
-		this.$emit('back');
-	}
-
-	next(): void {
-		this.$emit('next');
-	}
-
-	finishSetup(): void {
-		this.$emit('finish');
-	}
-}
+defineEmits<{
+	(e: 'back'): void;
+	(e: 'next'): void;
+	(e: 'finish'): void;
+}>();
 </script>
--- src/WebAPI/ClientAppV2/src/pages/setup/index.vue
+++ src/WebAPI/ClientAppV2/src/pages/setup/index.vue
@@ -1,221 +1,205 @@
 <template>
-	<v-container class="pa-0" style="max-width: 900px">
+	<q-page>
 		<!-- Logo	-->
-		<v-row justify="center" no-gutters>
-			<v-col cols="auto">
+		<q-row justify="center" no-gutters>
+			<q-col cols="auto">
 				<logo :size="128" />
-			</v-col>
-		</v-row>
+			</q-col>
+		</q-row>
 		<!--	Steppers	-->
-		<v-row>
-			<v-col>
-				<v-stepper id="stepper-main" v-model="stepIndex" non-linear>
-					<!-- Step headers	-->
-					<v-stepper-header>
-						<template v-for="(header, i) in headers">
-							<v-stepper-step
-								:key="i"
-								:step="i + 1"
-								:color="i + 1 === stepPagesCount ? 'green' : stepIndex > i + 1 ? 'green' : 'red'"
-								:complete="i + 1 === stepPagesCount ? stepIndex > i : stepIndex > i + 1"
-								editable
-								edit-icon="$complete"
-							>
-								{{ $t(`pages.setup.${header}.header`) }}
-							</v-stepper-step>
-							<v-divider v-if="i < stepPagesCount - 1" :key="i + 100" />
-						</template>
-					</v-stepper-header>
+		<q-row>
+			<q-col>
+				<!-- Step headers	-->
+				<q-stepper id="stepper-main" v-model="stepIndex" non-linear>
+					<!-- Introduction	-->
+					<q-step :name="1" :title="$t(`pages.setup.intro.header`)">
+						<q-row no-gutters>
+							<q-col>
+								<h2 class="mt-2">{{ $t('pages.setup.intro.title') }}</h2>
+								<p>{{ $t('pages.setup.intro.text.p-1') }}</p>
+								<ul>
+									<li>
+										{{ $t('pages.setup.intro.list.item-1') }}
+										<ExternalLinkButton href="https://github.com/PlexRipper/PlexRipper/issues" />
+									</li>
+									<li>{{ $t('pages.setup.intro.list.item-2') }}</li>
+									<li>{{ $t('pages.setup.intro.list.item-3') }}</li>
+									<li>{{ $t('pages.setup.intro.list.item-4') }}</li>
+									<li>{{ $t('pages.setup.intro.list.item-5') }}</li>
+								</ul>
+							</q-col>
+						</q-row>
+					</q-step>
+					<!-- Future plans!	-->
+					<q-step :name="2" :title="$t(`pages.setup.future-plans.header`)">
+						<q-row no-gutters>
+							<q-col>
+								<h2>{{ $t('pages.setup.future-plans.title') }}</h2>
+								<p>{{ $t('pages.setup.future-plans.text.p-1') }}</p>
+								<ul>
+									<li>
+										{{ $t('pages.setup.future-plans.list.item-1') }}
+										<ul>
+											<li>
+												{{ $t('pages.setup.future-plans.list.item-1-1') }}
+											</li>
+										</ul>
+									</li>
+									<li>{{ $t('pages.setup.future-plans.list.item-2') }}</li>
+									<li>{{ $t('pages.setup.future-plans.list.item-3') }}</li>
+									<li>{{ $t('pages.setup.future-plans.list.item-4') }}</li>
+									<li>{{ $t('pages.setup.future-plans.list.item-5') }}</li>
+									<li>
+										{{ $t('pages.setup.future-plans.list.item-6') }}
+										<ul>
+											<li>
+												{{ $t('pages.setup.future-plans.list.item-6-1') }}
+											</li>
+										</ul>
+									</li>
+								</ul>
+								<h2 class="text-center">{{ $t('pages.setup.future-plans.text.p-2') }}</h2>
+							</q-col>
+						</q-row>
+					</q-step>
+					<!-- Checking paths	-->
+					<q-step :name="3" :title="$t(`pages.setup.paths.header`)">
+						<h2 class="mt-2">{{ $t('pages.setup.paths.title') }}</h2>
+						<paths-default-overview />
+					</q-step>
+					<!-- Plex Accounts	-->
+					<q-step :name="4" :title="$t(`pages.setup.accounts.header`)">
+						<h2 class="mt-2">{{ $t('pages.setup.accounts.title') }}</h2>
+						<account-overview />
+					</q-step>
+					<!-- Finished	-->
+					<q-step :name="5" :title="$t(`pages.setup.finished.header`)">
+						<q-row no-gutters>
+							<q-col>
+								<h2 class="mt-2">{{ $t('pages.setup.finished.title') }}</h2>
+							</q-col>
+						</q-row>
+						<q-row no-gutters>
+							<q-col>
+								<p>{{ $t('pages.setup.finished.text.p-1') }}</p>
+								<q-list dense class="no-background">
+									<q-item v-for="(link, i) in links" :key="i" :href="link" target="_blank">
+										<q-item-section avatar>
+											<ul>
+												<li>
+													<span v-if="messages" style="font-weight: normal">
+														{{ messages['finished'].list['item-' + (i + 1)] }}
+													</span>
+												</li>
+											</ul>
+										</q-item-section>
+										<q-item-section side>
+											<ExternalLinkButton :href="link" />
+										</q-item-section>
+									</q-item>
+								</q-list>
+							</q-col>
+						</q-row>
+					</q-step>
+					<!--					<q-stepper-header>-->
+					<!--						<template v-for="(header, i) in headers">-->
+					<!--							<v-stepper-step-->
+					<!--								:key="i"-->
+					<!--								:step="i + 1"-->
+					<!--								:color="i + 1 === stepPagesCount ? 'green' : stepIndex > i + 1 ? 'green' : 'red'"-->
+					<!--								:complete="i + 1 === stepPagesCount ? stepIndex > i : stepIndex > i + 1"-->
+					<!--								editable-->
+					<!--								edit-icon="$complete">-->
+					<!--								{{ $t(`pages.setup.${header}.header`) }}-->
+					<!--							</v-stepper-step>-->
+					<!--							<q-separator v-if="i < stepPagesCount - 1" :key="i + 100" />-->
+					<!--						</template>-->
+					<!--					</q-stepper-header>-->
 
 					<!-- Step pages	-->
-					<v-stepper-items>
-						<!-- Introduction	-->
-						<v-stepper-content class="stepper-content" :step="1">
-							<v-container fluid>
-								<v-row no-gutters>
-									<v-col>
-										<h2 class="mt-2">{{ $t('pages.setup.intro.title') }}</h2>
-										<p>{{ $t('pages.setup.intro.text.p-1') }}</p>
-										<ul>
-											<li>
-												{{ $t('pages.setup.intro.list.item-1') }}
-												<ExternalLinkButton href="https://github.com/PlexRipper/PlexRipper/issues" />
-											</li>
-											<li>{{ $t('pages.setup.intro.list.item-2') }}</li>
-											<li>{{ $t('pages.setup.intro.list.item-3') }}</li>
-											<li>{{ $t('pages.setup.intro.list.item-4') }}</li>
-											<li>{{ $t('pages.setup.intro.list.item-5') }}</li>
-										</ul>
-									</v-col>
-								</v-row>
-							</v-container>
-						</v-stepper-content>
-
-						<!-- Future plans!	-->
-						<v-stepper-content class="stepper-content" :step="2">
-							<v-container fluid>
-								<v-row no-gutters>
-									<v-col>
-										<h2>{{ $t('pages.setup.future-plans.title') }}</h2>
-										<p>{{ $t('pages.setup.future-plans.text.p-1') }}</p>
-										<ul>
-											<li>
-												{{ $t('pages.setup.future-plans.list.item-1') }}
-												<ul>
-													<li>
-														{{ $t('pages.setup.future-plans.list.item-1-1') }}
-													</li>
-												</ul>
-											</li>
-											<li>{{ $t('pages.setup.future-plans.list.item-2') }}</li>
-											<li>{{ $t('pages.setup.future-plans.list.item-3') }}</li>
-											<li>{{ $t('pages.setup.future-plans.list.item-4') }}</li>
-											<li>{{ $t('pages.setup.future-plans.list.item-5') }}</li>
-											<li>
-												{{ $t('pages.setup.future-plans.list.item-6') }}
-												<ul>
-													<li>
-														{{ $t('pages.setup.future-plans.list.item-6-1') }}
-													</li>
-												</ul>
-											</li>
-										</ul>
-										<h2 class="text-center">{{ $t('pages.setup.future-plans.text.p-2') }}</h2>
-									</v-col>
-								</v-row>
-							</v-container>
-						</v-stepper-content>
-
-						<!-- Checking paths	-->
-						<v-stepper-content class="stepper-content" :step="3">
-							<h2 class="mt-2">{{ $t('pages.setup.paths.title') }}</h2>
-							<paths-default-overview />
-						</v-stepper-content>
-
-						<!-- Plex Accounts	-->
-						<v-stepper-content class="stepper-content" :step="4">
-							<h2 class="mt-2">{{ $t('pages.setup.accounts.title') }}</h2>
-							<account-overview />
-						</v-stepper-content>
-
-						<!-- Finished	-->
-						<v-stepper-content class="stepper-content" :step="5">
-							<v-container fluid>
-								<v-row no-gutters>
-									<v-col>
-										<h2 class="mt-2">{{ $t('pages.setup.finished.title') }}</h2>
-									</v-col>
-								</v-row>
-								<v-row no-gutters>
-									<v-col>
-										<p>{{ $t('pages.setup.finished.text.p-1') }}</p>
-										<v-list dense class="no-background">
-											<v-list-item v-for="(link, i) in links" :key="i" :href="link" target="_blank">
-												<v-list-item-title>
-													<ul>
-														<li>
-															<span v-if="messages" style="font-weight: normal">
-																{{ messages['finished'].list['item-' + (i + 1)] }}
-															</span>
-														</li>
-													</ul>
-												</v-list-item-title>
-												<v-list-item-action>
-													<ExternalLinkButton :href="link" />
-												</v-list-item-action>
-											</v-list-item>
-										</v-list>
-									</v-col>
-								</v-row>
-							</v-container>
-						</v-stepper-content>
-					</v-stepper-items>
-					<v-divider />
-					<!-- Stepper navigation bar	-->
-					<navigation-bar
-						:disable-back="isBackDisabled"
-						:disable-next="isNextDisabled"
-						:is-last="isNextDisabled"
-						@back="back"
-						@next="next"
-						@finish="finishSetup"
-					/>
-				</v-stepper>
-			</v-col>
-		</v-row>
-
+				</q-stepper>
+			</q-col>
+		</q-row>
+		<q-row>
+			<q-col>
+				<q-separator />
+				<!-- Stepper navigation bar	-->
+				<navigation-bar
+					:disable-back="isBackDisabled"
+					:disable-next="isNextDisabled"
+					:is-last="isNextDisabled"
+					@back="back"
+					@next="next"
+					@finish="finishSetup" />
+			</q-col>
+		</q-row>
 		<!--	Skip button	-->
-		<v-row justify="center">
-			<v-col cols="3">
+		<q-row justify="center">
+			<q-col cols="3">
 				<NavigationSkipSetupButton :disabled="isNextDisabled" :width="100" @click="skipDialogOpen = true" />
 				<confirmation-dialog
 					text-id="skip-setup"
 					:dialog="skipDialogOpen"
 					@confirm="finishSetup"
-					@cancel="skipDialogOpen = false"
-				/>
-			</v-col>
-		</v-row>
-	</v-container>
+					@cancel="skipDialogOpen = false" />
+			</q-col>
+		</q-row>
+	</q-page>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+<script setup lang="ts">
+import { ref, computed } from 'vue';
 import { useSubscription } from '@vueuse/rxjs';
 import Log from 'consola';
 import { SettingsService } from '@service';
+import { getMessage, useRouter } from '#imports';
 
-@Component
-export default class Setup extends Vue {
-	stepIndex: number = 1;
-	stepPagesCount: number = 5;
-	skipDialogOpen: boolean = false;
+const router = useRouter();
 
-	links: string[] = [
-		'https://github.com/PlexRipper/PlexRipper/',
-		'https://github.com/PlexRipper/PlexRipper/issues',
-		'https://github.com/PlexRipper/PlexRipper#translate-plexripper',
-		'https://github.com/PlexRipper/PlexRipper/',
-	];
+const stepIndex = ref(1);
+const stepPagesCount = ref(5);
+const skipDialogOpen = ref(false);
 
-	back(): void {
-		if (this.stepIndex > 1) {
-			this.stepIndex--;
-		}
+const links = ref([
+	'https://github.com/PlexRipper/PlexRipper/',
+	'https://github.com/PlexRipper/PlexRipper/issues',
+	'https://github.com/PlexRipper/PlexRipper#translate-plexripper',
+	'https://github.com/PlexRipper/PlexRipper/',
+]);
+
+const isBackDisabled = computed(() => {
+	return stepIndex.value === 1;
+});
+
+const isNextDisabled = computed(() => {
+	return stepIndex.value === stepPagesCount.value;
+});
+
+const messages = computed(() => {
+	return getMessage('pages.setup');
+});
+
+const next = () => {
+	if (stepIndex.value < stepPagesCount.value) {
+		stepIndex.value++;
 	}
+};
 
-	next(): void {
-		if (this.stepIndex < this.stepPagesCount) {
-			this.stepIndex++;
-		}
+const back = () => {
+	if (stepIndex.value > 1) {
+		stepIndex.value--;
 	}
+};
 
-	finishSetup(): void {
-		useSubscription(
-			SettingsService.updateGeneralSettings('firstTimeSetup', false).subscribe(() => {
-				Log.info('Setup process is finished or skipped, redirecting to home page now and refreshing the page');
-				this.$router.push('/', () => {
-					// Refresh the page when we go to the home page to make sure we get all new data.
-					location.reload();
-				});
-			}),
-		);
-	}
-
-	get headers(): string[] {
-		return ['intro', 'future-plans', 'paths', 'accounts', 'finished'];
-	}
-
-	get isBackDisabled(): boolean {
-		return this.stepIndex === 1;
-	}
-
-	get isNextDisabled(): boolean {
-		return this.stepIndex === this.stepPagesCount;
-	}
-
-	get messages(): any {
-		return this.$getMessage('pages.setup');
-	}
-}
+const finishSetup = () => {
+	useSubscription(
+		SettingsService.updateGeneralSettings('firstTimeSetup', false).subscribe(() => {
+			Log.info('Setup process is finished or skipped, redirecting to home page now and refreshing the page');
+			router.push('/').then(() => {
+				// Refresh the page when we go to the home page to make sure we get all new data.
+				// location.reload();
+			});
+		}),
+	);
+};
 </script>

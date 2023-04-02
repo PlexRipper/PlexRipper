4fb0b334fdb461d320d99a0091f73b40224b6aeb
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-04-02 19:58:03 +0200

    Updated the baseButtonProps

--- src/WebAPI/ClientAppV2/src/types/props/base-button/IBaseButtonProps.ts
+++ src/WebAPI/ClientAppV2/src/types/props/base-button/IBaseButtonProps.ts
@@ -1,16 +1,31 @@
-import { QBtnProps } from './QBtnProps';
-import { PlexBtnProps } from './PlexBtnProps';
+import ButtonType from '@enums/buttonType';
 
-export interface IBaseButtonProps extends QBtnProps, PlexBtnProps {
-	block?: boolean;
-	disabled?: boolean;
-	iconSize?: number;
-	iconOnly?: boolean;
-	color?: string;
-	lightColor?: string;
-	darkColor?: string;
-	href?: string;
-	to?: string;
-	width?: number;
-	height?: number;
+export interface IBaseButtonProps {
+	// PlexRipper
+	cy: string;
+	type: ButtonType;
+	textId: string;
+	tooltipId: string;
+	width: number;
+	height: number;
+	iconAlign: 'left' | 'right';
+	// Quasar native
+	label: string;
+	icon: string;
+	size: string | 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+	flat: boolean;
+	glossy: boolean;
+	round: boolean;
+	rounded: boolean;
+	outline: boolean;
+	loading: boolean;
+	disabled: boolean;
+	// Vuetify Legacy
+	block: boolean;
+	iconOnly: boolean;
+	// Unorganized
+	iconSize: number;
+	color: 'default' | 'positive' | 'warning' | 'negative';
+	href: string;
+	to: string;
 }

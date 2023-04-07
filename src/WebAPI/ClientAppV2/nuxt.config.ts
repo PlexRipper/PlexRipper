6197f3f961e68a183074db48c21490a66fa31911
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-04-07 18:01:02 +0200

    Corrected @Fixtures path

--- src/WebAPI/ClientAppV2/nuxt.config.ts
+++ src/WebAPI/ClientAppV2/nuxt.config.ts
@@ -91,7 +91,7 @@ export default defineNuxtConfig({
 		'@buttons': fileURLToPath(new URL('./src/components/Buttons/', import.meta.url)),
 		'@api-urls': fileURLToPath(new URL('./src/types/const/api-urls.ts', import.meta.url)),
 		'@props': fileURLToPath(new URL('./src/types/props/', import.meta.url)),
-		'@fixtures': fileURLToPath(new URL('../cypress/fixtures/', import.meta.url)),
+		'@fixtures': fileURLToPath(new URL('./cypress/fixtures/', import.meta.url)),
 		'@services-test-base': fileURLToPath(new URL('./src/tests/services/_base/base.ts', import.meta.url)),
 		'@lib': fileURLToPath(new URL('./src/types/lib/', import.meta.url)),
 		'@service': fileURLToPath(new URL('./src/service/', import.meta.url)),

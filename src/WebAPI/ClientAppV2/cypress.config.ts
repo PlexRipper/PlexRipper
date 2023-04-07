c47fb8912069e46df5ce2ed1ccbabff5ce7f5a78
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-04-07 15:21:00 +0200

    Removed component testing config, too many errors

--- src/WebAPI/ClientAppV2/cypress.config.ts
+++ src/WebAPI/ClientAppV2/cypress.config.ts
@@ -1,32 +1,6 @@
 import { defineConfig } from 'cypress';
-import { defineConfig as defineVite, InlineConfig, mergeConfig } from 'vite';
-import { esbuildCommonjs, viteCommonjs } from '@originjs/vite-plugin-commonjs';
-import { buildNuxt, loadNuxt } from '@nuxt/kit';
 
 export default defineConfig({
-	component: {
-		viewportHeight: 1080,
-		viewportWidth: 1920,
-		devServer: {
-			framework: 'vue',
-			bundler: 'vite',
-			viteConfig: async () => {
-				// Source: https://github.com/nuxt/nuxt/discussions/19304
-				const config = await mergeConfig(viteConfig, await nuxtViteConfig());
-
-				// @ts-ignore
-				config.plugins = config.plugins?.filter((item) => !['replace', 'vite-plugin-eslint'].includes(item.name));
-
-				// @ts-ignore
-				config.server.middlewareMode = false;
-
-				// eslint-disable-next-line no-console
-				console.log('log:entering viteconfig', config);
-				return config;
-			},
-		},
-	},
-
 	e2e: {
 		viewportHeight: 1080,
 		viewportWidth: 1920,
@@ -35,35 +9,3 @@ export default defineConfig({
 		},
 	},
 });
-
-const viteConfig = defineVite({
-	optimizeDeps: {
-		disabled: true,
-		esbuildOptions: {
-			plugins: [esbuildCommonjs(['consola', 'date-fns'])],
-		},
-	},
-	plugins: [viteCommonjs()],
-});
-
-// https://github.com/nuxt/framework/issues/6496
-async function nuxtViteConfig() {
-	const nuxt = await loadNuxt({
-		dev: false,
-		overrides: {
-			srcDir: 'src',
-			ssr: false,
-		},
-	});
-	return new Promise<InlineConfig>((resolve, reject) => {
-		nuxt.hook('vite:extendConfig', (config) => {
-			resolve(config);
-			throw new Error('_stop_');
-		});
-		buildNuxt(nuxt).catch((err) => {
-			if (!err.toString().includes('_stop_')) {
-				reject(err);
-			}
-		});
-	}).finally(() => nuxt.close());
-}

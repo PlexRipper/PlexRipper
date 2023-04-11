54c45c46874d69254b68aeefbc957f1db445d538
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-04-11 22:12:37 +0200

    Deleted non-functional component cypress tests

--- src/WebAPI/ClientAppV2/cypress/component/TestComponent.cy.ts
+++
@@ -1,8 +0,0 @@
-import TestComponent from '../../src/components/DebugTools/TestComponent.vue';
-
-describe('<TestComponent />', () => {
-	it('renders', () => {
-		// see: https://on.cypress.io/mounting-vue
-		cy.mount(TestComponent);
-	});
-});

37877a013ba9489b78077748e0bad583071bcf97
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-04-23 12:36:45 +0200

    Migrated the home page to Vue 3

--- src/WebAPI/ClientAppV2/src/pages/index.vue
+++ src/WebAPI/ClientAppV2/src/pages/index.vue
@@ -1,50 +1,48 @@
 <template>
-	<page-container>
-		<v-row v-if="firstTimeSetup">
-			<v-col cols="12">
+	<q-page>
+		<q-row v-if="firstTimeSetup">
+			<q-col cols="12">
 				<h2>{{ $t('pages.home.setup-question') }}</h2>
-				<v-row justify="center">
-					<v-col cols="3">
-						<NavigationSkipSetupButton :block="true" @click="skipSetup()" />
-					</v-col>
-					<v-col cols="3">
-						<GoToButton text-id="go-to-setup-page" :block="true" to="/setup" color="green" />
-					</v-col>
-				</v-row>
-			</v-col>
-		</v-row>
-		<v-row v-else>
-			<v-col>
+				<q-row justify="center">
+					<q-col cols="3">
+						<NavigationSkipSetupButton block @click="skipSetup()" />
+					</q-col>
+					<q-col cols="3">
+						<GoToButton text-id="go-to-setup-page" block to="/setup" color="green" />
+					</q-col>
+				</q-row>
+			</q-col>
+		</q-row>
+		<q-row v-else>
+			<q-col>
 				<h1>{{ $t('pages.home.header') }}</h1>
-			</v-col>
-		</v-row>
-	</page-container>
+			</q-col>
+		</q-row>
+	</q-page>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import Log from 'consola';
-import { Component, Vue } from 'vue-property-decorator';
+import { ref, onMounted } from 'vue';
 import { useSubscription } from '@vueuse/rxjs';
+import { set } from '@vueuse/core';
 import { SettingsService } from '@service';
 
-@Component<Home>({})
-export default class Home extends Vue {
-	firstTimeSetup: boolean = false;
+const firstTimeSetup = ref(false);
 
-	skipSetup(): void {
-		useSubscription(
-			SettingsService.updateGeneralSettings('firstTimeSetup', false).subscribe(() => {
-				Log.info('Setup process skipped');
-			}),
-		);
-	}
+const skipSetup = () => {
+	useSubscription(
+		SettingsService.updateGeneralSettings('firstTimeSetup', false).subscribe(() => {
+			Log.info('Setup process skipped');
+		}),
+	);
+};
 
-	mounted() {
-		useSubscription(
-			SettingsService.getFirstTimeSetup().subscribe((state) => {
-				this.firstTimeSetup = state;
-			}),
-		);
-	}
-}
+onMounted(() => {
+	useSubscription(
+		SettingsService.getFirstTimeSetup().subscribe((state) => {
+			set(firstTimeSetup, state);
+		}),
+	);
+});
 </script>

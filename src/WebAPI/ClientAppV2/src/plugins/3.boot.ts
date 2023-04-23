96b959fffc51e8fddfe051b6f5e97afee4d1cc2c
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-04-23 19:35:19 +0200

    Fixed Consola logging config deprecation

--- src/WebAPI/ClientAppV2/src/plugins/3.boot.ts
+++ src/WebAPI/ClientAppV2/src/plugins/3.boot.ts
@@ -1,10 +1,10 @@
-import Log, { LogLevel } from 'consola';
+import Log from 'consola';
 import { GlobalService } from '@service';
 
 export default defineNuxtPlugin(() => {
 	const config = useRuntimeConfig();
 
-	Log.level = LogLevel.Debug;
+	Log.level = 4;
 	// Log.level = config.public.isProduction ? LogLevel.Debug : LogLevel.Debug;
 	Log.info(`Nuxt Environment: ${config.public.version}`);
 

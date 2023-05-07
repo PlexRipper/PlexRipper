031a4b541ca211aee80245d4f23fa962a62a602e
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-05-07 23:49:00 +0200

    Changed to cy.getCy helpers

--- src/WebAPI/ClientAppV2/cypress/e2e/pages/setup/plexripper-setup.cy.ts
+++ src/WebAPI/ClientAppV2/cypress/e2e/pages/setup/plexripper-setup.cy.ts
@@ -17,31 +17,31 @@ describe('PlexRipper new setup process', () => {
 	});
 
 	it('Should navigate the setup process from the first to the last page when the navigation buttons are used', () => {
-		cy.get('[data-cy="setup-page-next-button"]').click();
+		cy.getCy('setup-page-next-button').click();
 
-		cy.get('[data-cy="setup-page-next-button"]').click();
+		cy.getCy('setup-page-next-button').click();
 
-		cy.get('[data-cy="setup-page-next-button"]').click();
+		cy.getCy('setup-page-next-button').click();
 
-		cy.get('[data-cy="setup-page-next-button"]').click();
+		cy.getCy('setup-page-next-button').click();
 
-		cy.get('[data-cy="setup-page-skip-setup-button"]').click();
+		cy.getCy('setup-page-skip-setup-button').click();
 
 		cy.url().should('eq', route('/'));
 	});
 
 	it('Should navigate the setup process from the first to the last page by clicking the tab header navigation buttons', () => {
-		cy.get('[data-cy="setup-header-tab-1"]').click();
+		cy.getCy('setup-header-tab-1').click();
 
-		cy.get('[data-cy="setup-header-tab-2"]').click();
+		cy.getCy('setup-header-tab-2').click();
 
-		cy.get('[data-cy="setup-header-tab-3"]').click();
+		cy.getCy('setup-header-tab-3').click();
 
-		cy.get('[data-cy="setup-header-tab-4"]').click();
+		cy.getCy('setup-header-tab-4').click();
 
-		cy.get('[data-cy="setup-header-tab-5"]').click();
+		cy.getCy('setup-header-tab-5').click();
 
-		cy.get('[data-cy="setup-page-skip-setup-button"]').click();
+		cy.getCy('setup-page-skip-setup-button').click();
 
 		cy.url().should('eq', route('/'));
 	});

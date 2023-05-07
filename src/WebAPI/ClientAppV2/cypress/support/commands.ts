c1aeaa8c7698bc9b41648c42bacee2039b424c22
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-05-07 15:00:25 +0200

    Installed @testing-library

--- src/WebAPI/ClientAppV2/cypress/support/commands.ts
+++ src/WebAPI/ClientAppV2/cypress/support/commands.ts
@@ -1,1 +1,11 @@
 import 'cypress-signalr-mock';
+
+import '@testing-library/cypress/add-commands';
+
+// Source: https://stackoverflow.com/a/63519375/8205497
+Cypress.on('uncaught:exception', (err) => {
+	/* returning false here prevents Cypress from failing the test */
+	if (err.message.includes('ResizeObserver loop limit exceeded')) {
+		return false;
+	}
+});

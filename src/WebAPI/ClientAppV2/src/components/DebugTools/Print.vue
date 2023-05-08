23654c85bcd857bdf4534ece39c3ad9dd66df5d6
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-05-08 22:07:53 +0200

    feat(Web-UI): The Print component will now only be displayed when debugMode is enabled

--- src/WebAPI/ClientAppV2/src/components/DebugTools/Print.vue
+++ src/WebAPI/ClientAppV2/src/components/DebugTools/Print.vue
@@ -1,15 +1,22 @@
 <template>
-	<q-expansion-item switch-toggle-side expand-separator :label="title">
-		<q-card :style="{ height: height + 'px' }">
-			<q-card-section>
-				<pre><slot/></pre>
-			</q-card-section>
-		</q-card>
-	</q-expansion-item>
+	<q-list v-if="debugMode" bordered class="rounded-borders">
+		<q-expansion-item switch-toggle-side expand-separator :label="title" icon="mdi-debug">
+			<q-card :style="{ maxHeight: height + 'px' }" class="scroll">
+				<q-card-section>
+					<pre><slot /></pre>
+				</q-card-section>
+			</q-card>
+		</q-expansion-item>
+	</q-list>
 </template>
 
 <script setup lang="ts">
-import { withDefaults, defineProps } from 'vue';
+import { withDefaults, defineProps, onMounted, ref } from 'vue';
+import { useSubscription } from '@vueuse/rxjs';
+import { set } from '@vueuse/core';
+import { SettingsService } from '@service';
+
+const debugMode = ref(false);
 
 withDefaults(
 	defineProps<{
@@ -21,4 +28,12 @@ withDefaults(
 		height: 500,
 	},
 );
+
+onMounted(() => {
+	useSubscription(
+		SettingsService.getDebugMode().subscribe((value) => {
+			set(debugMode, value);
+		}),
+	);
+});
 </script>

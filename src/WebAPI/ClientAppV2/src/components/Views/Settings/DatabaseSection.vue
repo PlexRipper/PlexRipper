1dcf2df66e766ede25ea52032d46de28fa2cd5fd
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-05-08 22:09:12 +0200

    fix(Web-UI): Fixed the confirmation dialog not showing wen pressing the Reset Database button under settings => advanced

--- src/WebAPI/ClientAppV2/src/components/Views/Settings/DatabaseSection.vue
+++ src/WebAPI/ClientAppV2/src/components/Views/Settings/DatabaseSection.vue
@@ -8,40 +8,30 @@
 			</q-col>
 			<!--	Reset Database button	-->
 			<q-col cols="8" align-self="center">
-				<WarningButton :width="400" text-id="reset-db" block @click="confirmationDialog = true" />
-				<confirmation-dialog
-					text-id="reset-db"
-					:dialog="confirmationDialog"
-					@confirm="resetDatabaseCommand"
-					@cancel="confirmationDialog = false" />
+				<WarningButton :width="400" text-id="reset-db" block @click="useOpenControlDialog(confirmationDialogName)" />
+				<confirmation-dialog text-id="reset-db" :name="confirmationDialogName" @confirm="resetDatabaseCommand" />
 			</q-col>
 		</q-row>
 	</q-section>
 </template>
 
 <script setup lang="ts">
-import Log from 'consola';
-import { ref } from 'vue';
 import { useSubscription } from '@vueuse/rxjs';
 import { resetDatabase } from '@api/settingsApi';
 import { GlobalService } from '@service';
-
-const confirmationDialog = ref(false);
+import { useOpenControlDialog } from '#imports';
 
 const router = useRouter();
-
+const confirmationDialogName = 'reset-database-confirmation-dialog';
 const resetDatabaseCommand = (): void => {
 	useSubscription(
 		resetDatabase().subscribe((value) => {
 			GlobalService.resetStore();
-			Log.debug('reset db', value);
 			if (value.isSuccess) {
 				router.push('/setup').then(() => {
 					// Refresh the page when we go to the home page to make sure we get all new data.
 					location.reload();
 				});
-			} else {
-				Log.error('reset db', value);
 			}
 		}),
 	);

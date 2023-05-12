2805884bc223cecb2f56397b3060d1ace811f666
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-05-12 08:16:28 +0200

    test(Cypress): Cleaning up of the Movie library tests

--- src/WebAPI/ClientAppV2/cypress/e2e/pages/movies/movies-library.cy.ts
+++ src/WebAPI/ClientAppV2/cypress/e2e/pages/movies/movies-library.cy.ts
@@ -1,69 +1,45 @@
-import { get } from '@vueuse/core';
 import { apiRoute, route } from '@fixtures/baseE2E';
-import { PLEX_LIBRARY_RELATIVE_PATH, PLEX_MEDIA_RELATIVE_PATH } from '@api-urls';
-import { generateResultDTO } from '@mock';
-import { PlexLibraryDTO, PlexMediaSlimDTO, PlexMediaType } from '@dto/mainApi';
-import { generatePlexMedias } from '@factories';
+import { SETTINGS_RELATIVE_PATH } from '@api-urls';
+import { PlexMediaType } from '@dto/mainApi';
 
 describe('Display media collection on the Library detail page', () => {
 	beforeEach(() => {
 		cy.basePageSetup({
-			plexAccountCount: 2,
-			plexServerCount: 2,
-			plexLibraryCount: 3,
+			plexAccountCount: 1,
+			plexServerCount: 1,
+			plexMovieLibraryCount: 1,
+			movieCount: 10000,
 		})
-			.then((pageData) => {
-				// Generate a bunch of movies
-				const movieCount = 1000;
-				const plexLibrary: PlexLibraryDTO = {
-					...pageData.plexLibraries[0],
-					type: PlexMediaType.Movie,
-					count: movieCount,
-					mediaSize: 10000,
-				};
-				const movies: PlexMediaSlimDTO[] = generatePlexMedias({
-					plexLibraryId: plexLibrary.id,
-					plexServerId: plexLibrary.plexServerId,
-					type: PlexMediaType.Movie,
-					config: {
-						movieCount,
-					},
-				});
-				// Intercept the Library detail call
-				cy.intercept('GET', apiRoute(PLEX_LIBRARY_RELATIVE_PATH + `/${plexLibrary.id}`), {
-					statusCode: 200,
-					body: generateResultDTO(plexLibrary),
-				});
-
-				// Intercept the Library media call
-				cy.intercept('GET', apiRoute(PLEX_MEDIA_RELATIVE_PATH + `/library/${plexLibrary.id}`, '?page=*&size=*'), {
-					statusCode: 200,
-					body: generateResultDTO(movies),
-				});
+			.then((data) => {
+				const movieLibrary = data.plexLibraries.find((x) => x.type === PlexMediaType.Movie);
+				if (!movieLibrary) {
+					throw new Error('Movie library not found');
+				}
 				// Visit the page
-				cy.visit(route(`/movies/${pageData.plexLibraries[0].id}`));
-
+				cy.visit(route(`/movies/${movieLibrary.id}`));
 				return cy.wrap({
-					plexLibrary,
-					movies,
+					...data,
+					movieLibrary,
 				});
 			})
-			.as('movieData');
+			.as('movieLibrary');
 	});
 
-	xit('Should successfully scroll to the bottom when scrolling the page', () => {
-		cy.getCy('media-table-scroll').scrollTo('bottom', { duration: 10000 });
+	it('Should successfully scroll to the bottom when scrolling the page', () => {
+		cy.getPageData().then((data) => {
+			cy.wait(1000).getCy('media-table-scroll').scrollTo('bottom', { duration: 10000 });
 
-		cy.getCy(`media-table-row-${1000 - 1}`)
-			.should('exist')
-			.and('be.visible');
+			cy.getCy(`media-table-row-${data.config.movieCount - 1}`)
+				.should('exist')
+				.and('be.visible');
+		});
 	});
 
 	it('Should display and click on all the letters in the alphabet navigation when movies are available', function () {
-		cy.get('@movieData').then((movieData) => {
-			cy.wait(1000);
-			const movies = (movieData as any).movies as PlexMediaSlimDTO[];
-			const sortTitles = get(movies).map((x) => x.sortTitle[0].toLowerCase());
+		// @ts-ignore
+		cy.get('@movieLibrary').then(({ movieLibrary, mediaData }) => {
+			const movies = mediaData.find((x) => x.libraryId === movieLibrary.id)?.media ?? [];
+			const sortTitles = movies.map((x) => x.sortTitle[0].toLowerCase());
 			for (const letter of 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.toLowerCase()) {
 				const index = sortTitles.indexOf(letter);
 				if (index > -1) {
@@ -71,9 +47,16 @@ describe('Display media collection on the Library detail page', () => {
 					cy.getCy(`letter-${letter}-alphabet-navigation-btn`, { timeout: 10000 }).click();
 				}
 			}
-			cy.getCy(`media-table-row-${1000 - 1}`)
-				.should('exist')
-				.and('be.visible');
 		});
+	});
+
+	it('Should switch to poster view when changing the view the media', () => {
+		// Once the option has changed, the settings are saved
+		cy.intercept('PUT', apiRoute(SETTINGS_RELATIVE_PATH), {
+			statusCode: 200,
+		});
+		cy.getCy('change-view-mode-btn').click();
+		cy.getCy('view-mode-poster-btn').click();
+		cy.getCy('poster-table').should('be.visible');
 	});
 });

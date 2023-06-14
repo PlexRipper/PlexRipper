8aeb3efff460867b2b8035d261ed6db4fcb6821b
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-06-14 17:46:37 +0200

    fix(Web-UI): Fixed empty help dialog appearing for options that have no text available

---
+++ src/WebAPI/ClientApp/src/components/Help/HelpRow.vue
@@ -0,0 +1,16 @@
+<template>
+	<q-row>
+		<q-col cols="4" align-self="center">
+			<help-icon :help-id="helpId" />
+		</q-col>
+		<q-col cols="4" align-self="center">
+			<slot />
+		</q-col>
+	</q-row>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+	helpId: string;
+}>();
+</script>

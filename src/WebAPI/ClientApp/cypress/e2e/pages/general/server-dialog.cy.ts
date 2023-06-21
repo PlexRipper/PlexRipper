06c2d87f51ed70ed4535ff9b0b3e324d89c7ed4f
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-06-21 17:00:50 +0200

    test(Web-UI): Added Cypress test for testing the Download Speed Limit in the Server Dialog

--- src/WebAPI/ClientApp/cypress/e2e/pages/general/server-dialog.cy.ts
+++ src/WebAPI/ClientApp/cypress/e2e/pages/general/server-dialog.cy.ts
@@ -1,3 +1,6 @@
+import { APIRoute, apiRoute } from '@fixtures/baseE2E';
+import { generateResultDTO } from '@mock';
+import { SettingsModelDTO } from '@dto/mainApi';
 describe('PlexRipper Server Dialog', () => {
 	beforeEach(() => {
 		cy.basePageSetup({
@@ -21,5 +24,32 @@ describe('PlexRipper Server Dialog', () => {
 
 		cy.getCy('server-dialog-close-btn').click();
 		cy.getCy('server-dialog-cy').should('not.exist');
+	});
+
+	it('Should slide the download speed limit when the slider is used', () => {
+		cy.getPageData().then((pageData) => {
+			const plexServer = pageData.plexServers[2];
+			cy.getCy('server-dialog-2').click();
+
+			cy.getCy('server-dialog-tab-3').click();
+			cy.getCy('server-dialog-tab-content-3').should('exist').and('be.visible');
+
+			cy.intercept('PUT', apiRoute({ type: APIRoute.Settings }), {
+				statusCode: 200,
+				body: generateResultDTO(pageData.settings),
+			}).as('settingsUpdate');
+
+			cy.getCy('download-speed-limit-slider').click();
+
+			cy.wait('@settingsUpdate').then((interception) => {
+				const newSettings = interception.request.body as SettingsModelDTO;
+
+				const serverSettings = newSettings.serverSettings.data.find(
+					(x) => x.machineIdentifier === plexServer.machineIdentifier,
+				);
+
+				expect(serverSettings?.downloadSpeedLimit).to.eq(50000);
+			});
+		});
 	});
 });

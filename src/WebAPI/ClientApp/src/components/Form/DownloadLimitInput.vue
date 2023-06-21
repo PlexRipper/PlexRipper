e4a059f02a40ede3ed42fc5f58763e67398fa080
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-06-21 16:59:39 +0200

    fix(Web-UI): Fixed the DownloadSpeedLimit not displaying the correct value when changing in the server dialog

--- src/WebAPI/ClientApp/src/components/Form/DownloadLimitInput.vue
+++ src/WebAPI/ClientApp/src/components/Form/DownloadLimitInput.vue
@@ -2,28 +2,28 @@
 	<q-row class="no-wrap" justify="between">
 		<q-col :cols="8">
 			<q-slider
-				:model-value="value"
+				v-model:model-value="value"
 				:min="0"
 				:step="500"
 				:max="100000"
 				snap
 				label
 				style="margin-top: 24px"
-				@pan="mouseEvent = $event"
-				@change="changeValue"
-				@update:model-value="updateDownloadLimit" />
+				data-cy="download-speed-limit-slider" />
 		</q-col>
 		<q-col :cols="3">
-			<q-input :model-value="value" type="number" suffix="kB/s" @update="changeValue" />
+			<q-input
+				v-model:model-value="value"
+				type="number"
+				debounce="1000"
+				suffix="kB/s"
+				data-cy="download-speed-limit-input" />
 		</q-col>
 	</q-row>
 </template>
 
 <script setup lang="ts">
-import { get, set } from '@vueuse/core';
-
 const props = defineProps<{
-	plexServerId: number;
 	downloadSpeedLimit: number;
 }>();
 
@@ -31,32 +31,15 @@ const emit = defineEmits<{
 	(e: 'change', value: number): void;
 }>();
 
-const mouseEvent = ref('');
-const sliderValue = ref(0);
-
-const downloadSpeedLimit = computed(() => {
-	return props.downloadSpeedLimit ?? 0;
+const value = computed({
+	get: () => {
+		return props.downloadSpeedLimit ?? 0;
+	},
+	set: (value) => {
+		if (value == null || value < 0) {
+			value = 0;
+		}
+		emit('change', value);
+	},
 });
-
-const value = computed(() => {
-	if (get(mouseEvent) === 'start') {
-		return get(sliderValue);
-	}
-	return get(downloadSpeedLimit);
-});
-
-function updateDownloadLimit(value: number | null): void {
-	if (value == null || value < 0) {
-		value = 0;
-	}
-	set(sliderValue, value);
-}
-
-function changeValue(value: number | null): void {
-	if (value === null) {
-		return;
-	}
-	set(sliderValue, value);
-	emit('change', value);
-}
 </script>
--- src/WebAPI/ClientApp/src/components/Navigation/ServerDialog/Tabs/ServerConfigTabContent.vue
+++ src/WebAPI/ClientApp/src/components/Navigation/ServerDialog/Tabs/ServerConfigTabContent.vue
@@ -1,17 +1,11 @@
 <template>
-	<q-row align="center">
-		<q-col cols="4">
-			<help-icon help-id="help.server-dialog.server-config.download-speed-limit" class="q-mt-sm" />
-		</q-col>
-		<q-col cols="8">
-			<download-limit-input
-				v-if="plexServer"
-				:plex-server-id="plexServer.id"
-				:download-speed-limit="downloadSpeedLimit"
-				@change="settingsStore.updateDownloadLimit(plexServer.machineIdentifier, $event)" />
-			<span v-else> Plex Server was null </span>
-		</q-col>
-	</q-row>
+	<HelpRow help-id="help.server-dialog.server-config.download-speed-limit">
+		<download-limit-input
+			v-if="plexServer"
+			:download-speed-limit="settingsStore.getServerSettings(plexServer.machineIdentifier)?.downloadSpeedLimit ?? 0"
+			@change="settingsStore.updateDownloadLimit(plexServer.machineIdentifier, $event)" />
+		<span v-else> Plex Server was null </span>
+	</HelpRow>
 </template>
 
 <script setup lang="ts">
@@ -19,12 +13,8 @@ import type { PlexServerDTO, PlexServerSettingsModel } from '@dto/mainApi';
 import { useSettingsStore } from '~/store';
 const settingsStore = useSettingsStore();
 
-const props = defineProps<{
+defineProps<{
 	plexServer: PlexServerDTO | null;
 	plexServerSettings: PlexServerSettingsModel | null;
 }>();
-
-const downloadSpeedLimit = computed((): number => {
-	return props.plexServerSettings?.downloadSpeedLimit ?? 0;
-});
 </script>

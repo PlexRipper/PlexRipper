a5dd524b7111465310515d1e2e082ab5d0e962f8
JasonLandbridge <jasonlandbridge@protonmail.com>
2023-11-18 16:46:30 +0100

    refactor(WebAPI): Renamed to GetAllPlexAccountQuery

---
+++ src/Data.Contracts/CQRS/PlexAccounts/Queries/GetAllPlexAccountsQuery.cs
@@ -0,0 +1,36 @@
+﻿using FluentResults;
+using MediatR;
+using PlexRipper.Domain;
+
+namespace Data.Contracts;
+
+public class GetAllPlexAccountsQuery : IRequest<Result<List<PlexAccount>>>
+{
+    #region Constructors
+
+    /// <summary>
+    /// Retrieves all <see cref="PlexAccount"/>s with the included <see cref="PlexServer"/>s and <see cref="PlexLibrary"/>s.
+    /// </summary>
+    /// <param name="includePlexLibraries">Should include accessible <see cref="PlexLibrary">PlexLibraries</see> for each <see cref="PlexAccount">PlexAccount</see>.</param>
+    /// <param name="includePlexServers">Should include accessible <see cref="PlexServer">PlexServers</see> for each <see cref="PlexAccount">PlexAccount</see>.</param>
+    /// <param name="onlyEnabled">Should only return enabled <see cref="PlexAccount">PlexAccounts</see>.</param>
+    /// <returns>A list of all <see cref="PlexAccount"/>s.</returns>
+    public GetAllPlexAccountsQuery(bool includePlexServers = false, bool includePlexLibraries = false, bool onlyEnabled = false)
+    {
+        IncludePlexServers = includePlexServers;
+        IncludePlexLibraries = includePlexLibraries;
+        OnlyEnabled = onlyEnabled;
+    }
+
+    #endregion
+
+    #region Properties
+
+    public bool IncludePlexServers { get; }
+
+    public bool IncludePlexLibraries { get; }
+
+    public bool OnlyEnabled { get; }
+
+    #endregion
+}

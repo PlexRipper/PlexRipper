1e95a43ed6c4fc8cbc0e7322562441003b5df38b
JasonLandbridge <jasonlandbridge@protonmail.com>
2024-02-24 16:32:54 +0100

    refactor(WebAPI): Changed to record

--- src/Application.Contracts/Notification/SendResult/SendNotificationResultCommand.cs
+++ src/Application.Contracts/Notification/SendResult/SendNotificationResultCommand.cs
@@ -3,12 +3,4 @@ using MediatR;
 
 namespace Application.Contracts;
 
-public class SendNotificationResult : INotification
-{
-    public Result Result { get; }
-
-    public SendNotificationResult(Result result)
-    {
-        Result = result;
-    }
-}
+public record SendNotificationResult(Result Result) : INotification;

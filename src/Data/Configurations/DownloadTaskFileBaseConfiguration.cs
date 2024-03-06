87e4f138703b23e5c92411961773e5b8bdd9c647
JasonLandbridge <jasonlandbridge@protonmail.com>
2024-03-06 17:43:34 +0100

    refactor(WebAPI): Moved UseTpcMappingStrategy to individual configuration files

---
+++ src/Data/Configurations/DownloadTaskFileBaseConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace PlexRipper.Data.Configurations;
+
+public class DownloadTaskFileBaseConfiguration : IEntityTypeConfiguration<DownloadTaskFileBase>
+{
+    public void Configure(EntityTypeBuilder<DownloadTaskFileBase> builder)
+    {
+        builder.UseTpcMappingStrategy();
+
+        builder
+            .HasMany(x => x.DownloadWorkerTasks)
+            .WithOne(x => x.DownloadTask)
+            .HasForeignKey(x => x.DownloadTaskId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
---
+++ src/Data/Configurations/DownloadTaskParentBaseConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace PlexRipper.Data.Configurations;
+
+public class DownloadTaskBaseConfiguration : IEntityTypeConfiguration<DownloadTaskBase>
+{
+    public void Configure(EntityTypeBuilder<DownloadTaskBase> builder)
+    {
+        builder.UseTpcMappingStrategy();
+    }
+}

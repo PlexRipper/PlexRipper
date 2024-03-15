fb8ef494bc2d7bbaa69d0df4fb152a24081a3f56
JasonLandbridge <jasonlandbridge@protonmail.com>
2024-03-15 16:53:15 +0100

    refactor(WebAPI): Refactored ToGeneric methods for DownloadTasks to Mapperly

---
+++ src/Domain/Entities/DownloadV2/Mappers/DownloadTaskGenericMapper.cs
@@ -0,0 +1,105 @@
+using Riok.Mapperly.Abstractions;
+
+namespace PlexRipper.Domain;
+
+[Mapper(UseReferenceHandling = true, RequiredMappingStrategy = RequiredMappingStrategy.Target)]
+public static partial class DownloadTaskGenericMapper
+{
+    public static DownloadTaskGeneric ToGeneric(this DownloadTaskMovie downloadTaskMovieFile)
+    {
+        var result = downloadTaskMovieFile.ToGenericMapper();
+        result.Children ??= new List<DownloadTaskGeneric>();
+        result.DownloadWorkerTasks ??= new List<DownloadWorkerTask>();
+        result.DataReceived = result.Children.Sum(x => x.DataReceived);
+        result.DataTotal = result.Children.Sum(x => x.DataTotal);
+        result.DownloadSpeed = result.Children.Sum(x => x.DownloadSpeed);
+        result.FileTransferSpeed = result.Children.Sum(x => x.FileTransferSpeed);
+        result.Percentage = result.Children.Any() ? result.Children.Average(x => x.Percentage) : 0;
+        return result;
+    }
+
+    private static partial DownloadTaskGeneric ToGenericMapper(this DownloadTaskMovie downloadTaskMovie);
+
+    public static partial DownloadTaskGeneric ToGeneric(this DownloadTaskMovieFile downloadTaskMovieFile);
+
+    #region TvShow
+
+    public static DownloadTaskGeneric ToGeneric(this DownloadTaskTvShow downloadTaskTvShow)
+    {
+        var result = downloadTaskTvShow.ToGenericMapper();
+        result.DownloadWorkerTasks ??= new List<DownloadWorkerTask>();
+        result.Children ??= new List<DownloadTaskGeneric>();
+        result.Children = downloadTaskTvShow.Children.Select(x =>
+            {
+                x.Calculate();
+                return x.ToGeneric();
+            })
+            .ToList();
+
+        result.DataReceived = result.Children.Sum(x => x.DataReceived);
+        result.DataTotal = result.Children.Sum(x => x.DataTotal);
+        result.DownloadSpeed = result.Children.Sum(x => x.DownloadSpeed);
+        result.FileTransferSpeed = result.Children.Sum(x => x.FileTransferSpeed);
+        result.Percentage = result.Children.Any() ? result.Children.Average(x => x.Percentage) : 0;
+        return result;
+    }
+
+    private static partial DownloadTaskGeneric ToGenericMapper(this DownloadTaskTvShow downloadTaskTvShow);
+
+    #endregion
+
+    #region Season
+
+    public static DownloadTaskGeneric ToGeneric(this DownloadTaskTvShowSeason downloadTaskTvShowSeason)
+    {
+        var result = downloadTaskTvShowSeason.ToGenericMapper();
+        result.DownloadWorkerTasks ??= new List<DownloadWorkerTask>();
+        result.Children ??= new List<DownloadTaskGeneric>();
+        result.Children = downloadTaskTvShowSeason.Children.Select(x =>
+            {
+                x.Calculate();
+                return x.ToGeneric();
+            })
+            .ToList();
+
+        result.DataReceived = result.Children.Sum(x => x.DataReceived);
+        result.DataTotal = result.Children.Sum(x => x.DataTotal);
+        result.DownloadSpeed = result.Children.Sum(x => x.DownloadSpeed);
+        result.FileTransferSpeed = result.Children.Sum(x => x.FileTransferSpeed);
+        result.Percentage = result.Children.Any() ? result.Children.Average(x => x.Percentage) : 0;
+        return result;
+    }
+
+    private static partial DownloadTaskGeneric ToGenericMapper(this DownloadTaskTvShowSeason downloadTaskTvShowSeason);
+
+    #endregion
+
+    #region Episode
+
+    public static DownloadTaskGeneric ToGeneric(this DownloadTaskTvShowEpisode downloadTaskTvShowEpisode)
+    {
+        var result = downloadTaskTvShowEpisode.ToGenericMapper();
+        result.Children ??= new List<DownloadTaskGeneric>();
+        result.DownloadWorkerTasks ??= new List<DownloadWorkerTask>();
+        result.DataReceived = result.Children.Sum(x => x.DataReceived);
+        result.DataTotal = result.Children.Sum(x => x.DataTotal);
+        result.DownloadSpeed = result.Children.Sum(x => x.DownloadSpeed);
+        result.FileTransferSpeed = result.Children.Sum(x => x.FileTransferSpeed);
+        result.Percentage = result.Children.Any() ? result.Children.Average(x => x.Percentage) : 0;
+        return result;
+    }
+
+    [MapperIgnoreTarget(nameof(DownloadTaskGeneric.DestinationDirectory))]
+    [MapperIgnoreTarget(nameof(DownloadTaskGeneric.DownloadDirectory))]
+    [MapperIgnoreTarget(nameof(DownloadTaskGeneric.FileLocationUrl))]
+    [MapperIgnoreTarget(nameof(DownloadTaskGeneric.FileName))]
+    private static partial DownloadTaskGeneric ToGenericMapper(this DownloadTaskTvShowEpisode downloadTaskTvShowEpisode);
+
+    #endregion
+
+    #region EpisodeFile
+
+    public static partial DownloadTaskGeneric ToGeneric(this DownloadTaskTvShowEpisodeFile downloadTaskTvShowEpisodeFile);
+
+    #endregion
+}

9274298191cd01d3db14c2bfaadc49de42841e6c
JasonLandbridge <jasonlandbridge@protonmail.com>
2024-03-16 17:05:18 +0100

    refactor(WebAPI): Moved DownloadManager unit tests to Application.UnitTest project

--- tests/UnitTests/Application.UnitTests/PlexDownloads/DownloadQueue/DownloadQueue_GetNextDownloadTask_UnitTests.cs
+++ tests/UnitTests/Application.UnitTests/PlexDownloads/DownloadQueue/DownloadQueue_GetNextDownloadTask_UnitTests.cs
@@ -1,62 +1,10 @@
 ﻿using Data.Contracts;
-using PlexRipper.Application;
 
-namespace DownloadManager.UnitTests;
+namespace PlexRipper.Application.UnitTests;
 
 public class DownloadQueue_GetNextDownloadTask_UnitTests : BaseUnitTest<DownloadQueue>
 {
     public DownloadQueue_GetNextDownloadTask_UnitTests(ITestOutputHelper output) : base(output) { }
-
-    private List<DownloadTaskGeneric> TestDownloadTasks(int count)
-    {
-        var downloadTasks = new List<DownloadTaskGeneric>();
-        var index = 1;
-        for (var i = 0; i < count; i++)
-        {
-            var downloadTask_i = new DownloadTaskGeneric
-            {
-                Id = Guid.NewGuid(),
-                DownloadStatus = DownloadStatus.Queued,
-                Children = new List<DownloadTaskGeneric>(),
-            };
-            for (var j = 0; j < count; j++)
-            {
-                var downloadTask_j = new DownloadTaskGeneric
-                {
-                    Id = Guid.NewGuid(),
-                    DownloadStatus = DownloadStatus.Queued,
-                    Children = new List<DownloadTaskGeneric>(),
-                };
-                for (var k = 0; k < count; k++)
-                {
-                    var downloadTask_k = new DownloadTaskGeneric
-                    {
-                        Id = Guid.NewGuid(),
-                        DownloadStatus = DownloadStatus.Queued,
-                        Children = new List<DownloadTaskGeneric>(),
-                    };
-                    for (var l = 0; l < count; l++)
-                    {
-                        var downloadTask_l = new DownloadTaskGeneric
-                        {
-                            Id = Guid.NewGuid(),
-                            DownloadStatus = DownloadStatus.Queued,
-                            Children = new List<DownloadTaskGeneric>(),
-                        };
-                        downloadTask_k.Children.Add(downloadTask_l);
-                    }
-
-                    downloadTask_j.Children.Add(downloadTask_k);
-                }
-
-                downloadTask_i.Children.Add(downloadTask_j);
-            }
-
-            downloadTasks.Add(downloadTask_i);
-        }
-
-        return downloadTasks;
-    }
 
     [Fact]
     public async Task ShouldHaveNextDownloadTask_WhenAllAreQueued()
--- tests/UnitTests/Application.UnitTests/PlexDownloads/Stop/DownloadCommands_StopDownloadTasksAsync_UnitTests.cs
+++ tests/UnitTests/Application.UnitTests/PlexDownloads/Stop/DownloadCommands_StopDownloadTasksAsync_UnitTests.cs
@@ -48,10 +48,7 @@ public class DownloadCommands_StopDownloadTasksAsync_UnitTests : BaseUnitTest<St
     {
         // Arrange
         Seed = 9999;
-        await SetupDatabase(config =>
-        {
-            config.MovieDownloadTasksCount = 2;
-        });
+        await SetupDatabase(config => { config.MovieDownloadTasksCount = 2; });
         var movieDownloadTasks = await DbContext.GetAllDownloadTasksAsync();
 
         mock.Mock<IDownloadTaskScheduler>().Setup(x => x.StopDownloadTaskJob(It.IsAny<Guid>())).ReturnOk();

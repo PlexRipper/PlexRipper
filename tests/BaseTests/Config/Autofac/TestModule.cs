930aefd44ab5b91867b7a8c2f26c0029d296b157
JasonLandbridge <jasonlandbridge@protonmail.com>
2024-03-23 11:40:19 +0100

    refactor(WebAPI): Moved Integration tests autofac registrations to a single module

--- tests/BaseTests/Config/Autofac/TestModule.cs
+++ tests/BaseTests/Config/Autofac/TestModule.cs
@@ -1,6 +1,10 @@
-﻿using Application.Contracts;
+﻿using System.Collections.Specialized;
+using Application.Contracts;
 using Autofac;
+using Autofac.Extras.Quartz;
+using Data.Contracts;
 using FileSystem.Contracts;
+using PlexRipper.Data;
 using Settings.Contracts;
 
 namespace PlexRipper.BaseTests;
@@ -10,8 +14,23 @@ namespace PlexRipper.BaseTests;
 /// </summary>
 public class TestModule : Module
 {
+    public string MemoryDbName { get; init; }
+    public MockPlexApi MockPlexApi { get; init; }
+    public UnitTestDataConfig Config { get; init; }
+
     protected override void Load(ContainerBuilder builder)
     {
+        // Database context can be setup once and then retrieved by its DB name.
+        builder
+            .Register((_, _) => MockDatabase.GetMemoryDbContext(MemoryDbName))
+            .As<PlexRipperDbContext>()
+            .InstancePerDependency();
+
+        builder
+            .Register((_, _) => MockDatabase.GetMemoryDbContext(MemoryDbName))
+            .As<IPlexRipperDbContext>()
+            .InstancePerDependency();
+
         builder.RegisterType<TestStreamTracker>().As<ITestStreamTracker>().SingleInstance();
         builder.RegisterType<MockDownloadFileStream>().As<IDownloadFileStream>().SingleInstance();
         builder.RegisterType<MockFileMergeStreamProvider>().As<IFileMergeStreamProvider>().SingleInstance();
@@ -21,5 +40,52 @@ public class TestModule : Module
         builder.RegisterType<TestLoggingClass>().SingleInstance();
 
         builder.RegisterType<MockSignalRService>().As<ISignalRService>().SingleInstance();
+
+        SetMockedDependencies(builder);
+        RegisterBackgroundScheduler(builder);
+    }
+
+    private void SetMockedDependencies(ContainerBuilder builder)
+    {
+        if (MockPlexApi is not null)
+        {
+            builder
+                .RegisterInstance(MockPlexApi.CreateClient())
+                .As<HttpClient>()
+                .SingleInstance();
+        }
+
+        if (Config.MockFileSystem is not null)
+            builder.RegisterInstance(Config.MockFileSystem).As<IFileSystem>();
+
+        if (Config.MockConfigManager is not null)
+            builder.RegisterInstance(Config.MockConfigManager).As<IConfigManager>();
+    }
+
+    private void RegisterBackgroundScheduler(ContainerBuilder builder)
+    {
+        var testQuartzProps = new NameValueCollection
+        {
+            { "quartz.scheduler.instanceName", "TestPlexRipper Scheduler" },
+            { "quartz.serializer.type", "json" },
+            { "quartz.threadPool.type", "Quartz.Simpl.SimpleThreadPool, Quartz" },
+            { "quartz.threadPool.threadCount", "10" },
+            { "quartz.jobStore.misfireThreshold", "60000" },
+        };
+
+        // Register Quartz dependencies
+        builder.RegisterModule(new QuartzAutofacFactoryModule
+        {
+            // JobScopeConfigurator = (cb, tag) =>
+            // {
+            //     // override dependency for job scope
+            //     cb.Register(_ => new ScopedDependency("job-local " + DateTime.UtcNow.ToLongTimeString()))
+            //         .AsImplementedInterfaces()
+            //         .InstancePerMatchingLifetimeScope(tag);
+            // },
+
+            // During integration testing, we cannot use a real JobStore so we revert to default
+            ConfigurationProvider = _ => testQuartzProps,
+        });
     }
 }

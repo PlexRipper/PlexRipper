d4e26ca10ff6afdbbcc1cd765800d9ca237358e0
JasonLandbridge <jasonlandbridge@protonmail.com>
2024-03-30 22:07:59 +0100

    test(WebAPI): Added unit ttests for the clear completed and download status update of DownloadTasks

---
+++ tests/UnitTests/Application.UnitTests/PlexDownloads/ClearCompleted/ClearCompletedDownloadTasksEndpoint_UnitTests.cs
@@ -0,0 +1,83 @@
+using Data.Contracts;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PlexRipper.Application.UnitTests.ClearCompleted;
+
+public class ClearCompletedDownloadTasksEndpoint_UnitTests : BaseUnitTest<ClearCompletedDownloadTasksEndpoint>
+{
+    public ClearCompletedDownloadTasksEndpoint_UnitTests(ITestOutputHelper output) : base(output) { }
+
+    [Fact]
+    public async Task ShouldRemoveOnlySpecifiedCompletedDownloadTasks_WhenClearCompletedEndpointIsCalledWithGuidList()
+    {
+        // Arrange
+        await SetupDatabase(config =>
+        {
+            config.PlexServerCount = 1;
+            config.PlexLibraryCount = 1;
+            config.MovieDownloadTasksCount = 10;
+        });
+
+        // Set download tasks to completed
+        var dbContext = IDbContext;
+        var downloadTasks = await dbContext.DownloadTaskMovie.AsTracking().Include(x => x.Children).ToListAsync();
+
+        downloadTasks.SetDownloadStatus(DownloadStatus.Completed);
+        await dbContext.SaveChangesAsync(CancellationToken.None);
+
+        var ep = Factory.Create<ClearCompletedDownloadTasksEndpoint>(ctx =>
+            ctx.AddTestServices(s => s.AddTransient(_ => mock.Create<IPlexRipperDbContext>()))
+        );
+
+        // Act
+        await ep.HandleAsync(downloadTasks.Select(x => x.Id).Take(5).ToList(), default);
+        var result = ep.Response;
+
+        // Assert
+        result.ShouldNotBeNull();
+        result.IsSuccess.ShouldBeTrue();
+
+        var downloadTasksDb = await IDbContext.DownloadTaskMovie.ToListAsync();
+        var downloadTasksFileDb = await IDbContext.DownloadTaskMovieFile.ToListAsync();
+        downloadTasksDb.Count.ShouldBe(5);
+        downloadTasksFileDb.Count.ShouldBe(5);
+    }
+
+    [Fact]
+    public async Task ShouldRemoveAllCompletedDownloadTasks_WhenClearCompletedEndpointIsCalled()
+    {
+        // Arrange
+        await SetupDatabase(config =>
+        {
+            config.PlexServerCount = 1;
+            config.PlexLibraryCount = 1;
+            config.MovieDownloadTasksCount = 10;
+        });
+
+        // Set download tasks to completed
+        var dbContext = IDbContext;
+        var downloadTasks = await dbContext.DownloadTaskMovie.AsTracking().Include(x => x.Children).ToListAsync();
+
+        downloadTasks.SetDownloadStatus(DownloadStatus.Completed);
+        await dbContext.SaveChangesAsync(CancellationToken.None);
+
+        var ep = Factory.Create<ClearCompletedDownloadTasksEndpoint>(ctx =>
+            ctx.AddTestServices(s => s.AddTransient(_ => mock.Create<IPlexRipperDbContext>()))
+        );
+
+        // Act
+        await ep.HandleAsync(new List<Guid>(), default);
+        var result = ep.Response;
+
+        // Assert
+        result.ShouldNotBeNull();
+        result.IsSuccess.ShouldBeTrue();
+
+        var downloadTasksDb = await IDbContext.DownloadTaskMovie.ToListAsync();
+        var downloadTasksFileDb = await IDbContext.DownloadTaskMovieFile.ToListAsync();
+        downloadTasksDb.ShouldBeEmpty();
+        downloadTasksFileDb.ShouldBeEmpty();
+    }
+}

f796ab38f9072d45ed4bd062cb89aca52835d619
JasonLandbridge <jasonlandbridge@protonmail.com>
2024-04-06 21:45:03 +0200

    refactor(WebAPI): Added GetAsync for Entities with a Guid primary key

---
+++ src/Domain/Entities/Base/BaseEntityGuid.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PlexRipper.Domain;
+
+public abstract class BaseEntityGuid
+{
+    [Key]
+    [Column(Order = 0)]
+    public required Guid Id { get; set; }
+}
--- src/Domain/Entities/Download/Base/DownloadTaskBase.cs
+++ src/Domain/Entities/Download/Base/DownloadTaskBase.cs
@@ -1,14 +1,9 @@
-using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PlexRipper.Domain;
 
-public abstract class DownloadTaskBase
+public abstract class DownloadTaskBase : BaseEntityGuid
 {
-    [Key]
-    [Column(Order = 0)]
-    public required Guid Id { get; set; }
-
     /// <summary>
     /// Gets or sets the unique identifier used by the Plex Api to keep track of media.
     /// This is only unique on that specific server.

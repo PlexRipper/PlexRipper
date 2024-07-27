a5124a82221f7177f63e1b4fdf340e65734b419d
JasonLandbridge <jasonlandbridge@protonmail.com>
2024-07-27 13:37:53 +0200

    refactor(Web-UI): Moved primvue theme to its own file

--- src/WebAPI/ClientApp/nuxt.config.ts
+++ src/WebAPI/ClientApp/nuxt.config.ts
@@ -3,9 +3,6 @@ import { resolve } from 'path';
 import { defineNuxtConfig } from 'nuxt/config';
 import { createCommonJS } from 'mlly';
 
-import Aura from '@primevue/themes/aura';
-import { definePreset } from '@primevue/themes';
-
 const { __dirname } = createCommonJS(import.meta.url);
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
@@ -68,111 +65,7 @@ export default defineNuxtConfig({
 	},
 
 	primevue: {
-		options: {
-			ripple: true,
-			theme: {
-				preset: definePreset(Aura, {
-					semantic: {
-						primary: {
-							50: '{red.50}',
-							100: '{red.100}',
-							200: '{red.200}',
-							300: '{red.300}',
-							400: '{red.400}',
-							500: '{red.500}',
-							600: '{red.600}',
-							700: '{red.700}',
-							800: '{red.800}',
-							900: '{red.900}',
-							950: '{red.950}',
-						},
-						colorScheme: {
-							light: {
-								surface: {
-									0: '#ffffff',
-									50: '{red.50}',
-									100: '{red.100}',
-									200: '{red.200}',
-									300: '{red.300}',
-									400: '{red.400}',
-									500: '{red.500}',
-									600: '{red.600}',
-									700: '{red.700}',
-									800: '{red.800}',
-									900: 'transparent',
-									950: 'transparent',
-								},
-							},
-							dark: {
-								surface: {
-									0: '#ffffff',
-									50: '{slate.50}',
-									100: '{slate.100}',
-									200: '{slate.200}',
-									300: '{slate.300}',
-									400: '{slate.400}',
-									500: '{slate.500}',
-									600: '{slate.600}',
-									700: '{slate.700}',
-									800: 'transparent',
-									900: 'transparent',
-									950: 'transparent',
-								},
-							},
-						},
-					},
-					colorScheme: {
-						light: {
-							primary: {
-								color: '#ff0000',
-							},
-						},
-						dark: {
-							primary: {
-								color: '#FF4032',
-							},
-							highlight: {
-								background: 'rgba(255, 0, 0, 0.47)',
-								focusBackground: 'rgba(250, 250, 250, .24)',
-								color: 'rgba(255,255,255,.87)',
-								focusColor: 'rgba(255,255,255,.87)',
-							},
-						},
-					},
-					components: {
-						include: ['TreeTable', 'Column', 'Checkbox'],
-						treetable: {
-							colorScheme: {
-								light: {
-									border: {
-										color: '{zinc.400}',
-									},
-								},
-								dark: {
-									border: {
-										color: '{zinc.400}',
-									},
-								},
-							},
-						},
-						checkbox: {
-							colorScheme: {
-								light: {
-									border: {
-										color: '{neutral.100}',
-									},
-								},
-								dark: {
-									border: {
-										color: '{neutral.100}',
-									},
-								},
-							},
-						},
-					},
-				}),
-			},
-		},
+		importTheme: { from: '@/assets/scss/primevue/plexripper-theme.ts' },
 	},
 
 	typescript: {

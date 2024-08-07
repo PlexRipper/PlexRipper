9aa3167e2c3056483a8d2403742a2efd7b1b284f
JasonLandbridge <jasonlandbridge@protonmail.com>
2024-08-07 17:37:42 +0200

    fix(Web-UI): Fixed broken percentage display due to missing translation

--- src/WebAPI/ClientApp/src/config/vueI18n.config.ts
+++ src/WebAPI/ClientApp/src/config/vueI18n.config.ts
@@ -1,6 +1,23 @@
 import { defineI18nConfig } from '#i18n';
 
+const defaultNumberFormat = {
+	decimal: {
+		style: 'decimal',
+		minimumFractionDigits: 2,
+		maximumFractionDigits: 2,
+	},
+	percent: {
+		style: 'percent',
+		useGrouping: false,
+	},
+} as const;
+
 export default defineI18nConfig(() => ({
 	locale: 'en-US',
 	fallbackLocale: 'en-US',
+	numberFormats: {
+		'en-US': defaultNumberFormat,
+		'de-DE': defaultNumberFormat,
+		'fr-FR': defaultNumberFormat,
+	},
 }));

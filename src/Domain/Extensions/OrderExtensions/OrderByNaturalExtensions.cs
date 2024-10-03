2567b98e7a4a5bc8710271076e515d2203236e0f
JasonLandbridge <jasonlandbridge@protonmail.com>
2024-10-03 22:00:46 +0200

    perf(WebAPI): Greatly increased performance of opening a TvShow

--- src/Domain/Extensions/OrderExtensions/OrderByNaturalExtensions.cs
+++ src/Domain/Extensions/OrderExtensions/OrderByNaturalExtensions.cs
@@ -5,15 +5,11 @@ namespace PlexRipper.Domain;
 // Source: https://github.com/tompazourek/NaturalSort.Extension
 public static class OrderByNaturalExtensions
 {
-    public static IEnumerable<T> OrderByNatural<T>(this IEnumerable<T> objects, Func<T, string> func)
-    {
-        return objects.OrderBy(func, StringComparison.OrdinalIgnoreCase.WithNaturalSort());
-    }
+    public static IEnumerable<T> OrderByNatural<T>(this IEnumerable<T> objects, Func<T, string> func) =>
+        objects.OrderBy(func, StringComparison.OrdinalIgnoreCase.WithNaturalSort());
 
-    public static IEnumerable<T> OrderByNaturalDesc<T>(this IEnumerable<T> objects, Func<T, string> func)
-    {
-        return objects.OrderByNatural(func).Reverse();
-    }
+    public static IEnumerable<T> OrderByNaturalDesc<T>(this IEnumerable<T> objects, Func<T, string> func) =>
+        objects.OrderByNatural(func).Reverse();
 
     public static string CollationName => "NATURALSORT";
 }

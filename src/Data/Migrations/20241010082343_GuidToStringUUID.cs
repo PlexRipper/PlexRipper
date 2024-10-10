dd66e9d4846f1c19e1e3326c5f7079407f9baf22
JasonLandbridge <jasonlandbridge@protonmail.com>
2024-10-10 12:01:33 +0200

    fix(WebAPI): Fixed out or range unix time errors by parsing long from milliseconds instead of seconds

--- src/Data/Migrations/20241010082343_GuidToStringUUID.cs
+++ src/Data/Migrations/20241010082343_GuidToStringUUID.cs
@@ -8,15 +8,9 @@ namespace PlexRipper.Data.Migrations
     public partial class GuidToStringUUID : Migration
     {
         /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-
-        }
+        protected override void Up(MigrationBuilder migrationBuilder) { }
 
         /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-
-        }
+        protected override void Down(MigrationBuilder migrationBuilder) { }
     }
 }
--- src/PlexApi/Extensions/DateTimeExtensions.cs
+++ src/PlexApi/Extensions/DateTimeExtensions.cs
@@ -4,7 +4,7 @@ namespace PlexRipper.PlexApi;
 
 public static class DateTimeExtensions
 {
-    public static DateTime FromUnixTime(long unixTime) => DateTimeOffset.FromUnixTimeSeconds(unixTime).DateTime;
+    public static DateTime FromUnixTime(long unixTime) => DateTimeOffset.FromUnixTimeMilliseconds(unixTime).DateTime;
 
     public static DateTime? FromUnixTime(long? unixTime) =>
         unixTime != null ? DateTimeOffset.FromUnixTimeSeconds((long)unixTime).DateTime : null;

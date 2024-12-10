cddb8e78278801ecad7878d82539e41535030d0f
JasonLandbridge <jasonlandbridge@protonmail.com>
2024-12-10 14:06:19 +0100

    refactor(WebAPI): Moved the large data set to a seperate file to avoid the IDE freezing up

---
+++ src/Domain/Extensions/HashSetExtensions.cs
@@ -0,0 +1,27 @@
+namespace PlexRipper.Domain;
+
+public static class HashSetExtensions
+{
+    /// <summary>
+    /// Get an item from a HashSet by index.
+    /// </summary>
+    /// <param name="hashSet"> The HashSet to get the item from. </param>
+    /// <param name="index"> The index of the item to get. </param>
+    /// <typeparam name="T"> The type of the HashSet. </typeparam>
+    /// <returns> The item at the index. </returns>
+    public static T GetByIndex<T>(this HashSet<T> hashSet, int index)
+    {
+        var currentIndex = 0;
+        foreach (var item in hashSet)
+        {
+            if (currentIndex == index)
+            {
+                return item;
+            }
+
+            currentIndex++;
+        }
+
+        return hashSet.First();
+    }
+}

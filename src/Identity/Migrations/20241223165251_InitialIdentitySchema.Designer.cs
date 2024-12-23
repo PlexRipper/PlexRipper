e02d7d03a48f20a65eb0e47f21d796bd3a76c124
JasonLandbridge <jasonlandbridge@protonmail.com>
2024-12-23 18:05:22 +0100

    style(WebAPI): formatting

--- src/Identity/Migrations/20241223165251_InitialIdentitySchema.Designer.cs
+++ src/Identity/Migrations/20241223165251_InitialIdentitySchema.Designer.cs
@@ -4,7 +4,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using PlexRipper.Data;
 
 #nullable disable
 

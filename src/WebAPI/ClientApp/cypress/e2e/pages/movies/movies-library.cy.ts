54b7aca25a405c9f356008e4ae555dd074b148e0
JasonLandbridge <jasonlandbridge@protonmail.com>
2025-01-13 10:46:59 +0100

    test(Web-UI): hopeful cypress flakey test fix

--- src/WebAPI/ClientApp/cypress/e2e/pages/movies/movies-library.cy.ts
+++ src/WebAPI/ClientApp/cypress/e2e/pages/movies/movies-library.cy.ts
@@ -48,7 +48,7 @@ describe('Display media collection on the Library detail page', () => {
 				for (const letter of 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.toLowerCase()) {
 					const index = sortTitles.indexOf(letter);
 					if (index > -1) {
-						cy.getCy(`letter-${letter}-alphabet-navigation-btn`).should('be.visible');
+						cy.getCy(`letter-${letter}-alphabet-navigation-btn`, { timeout: 10000 }).should('be.visible');
 						cy.getCy(`letter-${letter}-alphabet-navigation-btn`, { timeout: 10000 }).click();
 					}
 				}

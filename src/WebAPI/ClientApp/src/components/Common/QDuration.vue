5d7b25891c2dd6f3011cc7b1b186421fa48c7e39
JasonLandbridge <jasonlandbridge@protonmail.com>
2025-01-18 22:03:34 +0100

    fix(Web-UI): Fixed the duration display with the correct zero padding

--- src/WebAPI/ClientApp/src/components/Common/QDuration.vue
+++ src/WebAPI/ClientApp/src/components/Common/QDuration.vue
@@ -22,13 +22,20 @@ const duration = computed(() => {
 
 	if (props.short) {
 		const duration = intervalToDuration({ start: 0, end: props.value * 1000 });
-		const zeroPad = (num) => String(num).padStart(2, '0');
-		return [duration.hours, duration.minutes, duration.seconds].filter(Boolean).map(zeroPad).join(':');
+		const zeroPad = (num: number | undefined) => String(num || 0).padStart(2, '0');
+		const minutes = zeroPad(duration.minutes);
+		const seconds = zeroPad(duration.seconds);
+
+		if (duration.hours) {
+			return `${zeroPad(duration.hours)}:${minutes}:${seconds}`;
+		}
+
+		return `${minutes}:${seconds}`;
 	}
 
 	const date = new Date(props.value * 1000);
 	return formatDuration(
-		{ hours: date.getUTCHours(), minutes: date.getUTCMinutes(), seconds: date.getSeconds() },
+		{ hours: date.getUTCHours(), minutes: date.getUTCMinutes(), seconds: date.getUTCSeconds() },
 		{ delimiter: ' ', format: ['hours', 'minutes', 'seconds'] },
 	);
 });

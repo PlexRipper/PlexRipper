6d62c672c580569ff354d552da53c7b2255f6ca8
JasonLandbridge <jasonlandbridge@protonmail.com>
2025-01-24 21:36:05 +0100

    test(Web-UI): fixed unit tests

--- src/WebAPI/ClientApp/tests/stores/background-jobs-store/getJobStatusUpdate.test.ts
+++ src/WebAPI/ClientApp/tests/stores/background-jobs-store/getJobStatusUpdate.test.ts
@@ -1,16 +1,22 @@
 import { beforeAll, describe, expect, test } from 'vitest';
 import { createPinia, setActivePinia } from 'pinia';
-import { baseSetup, subscribeSpyTo } from '@services-test-base';
+import { baseSetup, baseVars, getAxiosMock, subscribeSpyTo } from '@services-test-base';
+import type { InspectPlexServerJobUpdateDTO, SyncServerMediaJobUpdateDTO } from '@dto';
 import { JobStatus, JobTypes } from '@dto';
 import { generateJobStatusUpdate } from '@factories';
 import { useBackgroundJobsStore } from '@store';
+import { generateResultDTO } from '@mock';
+import { BackgroundJobsPaths } from '@api/generated/BackgroundJobs';
 
 describe('BackgroundJobsStore.getJobStatusUpdate()', () => {
+	let { mock } = baseVars();
+
 	beforeAll(() => {
 		baseSetup();
 	});
 
 	beforeEach(() => {
+		mock = getAxiosMock();
 		setActivePinia(createPinia());
 	});
 
@@ -21,8 +27,10 @@ describe('BackgroundJobsStore.getJobStatusUpdate()', () => {
 		const testMsg = generateJobStatusUpdate({
 			jobType: JobTypes.InspectPlexServerJob,
 			jobStatus: JobStatus.Started,
-			data: [4],
+			data: { plexServerIds: [4] } as InspectPlexServerJobUpdateDTO,
 		});
+
+		mock.onGet(BackgroundJobsPaths.getAllBackgroundJobsEndpoint()).reply(200, generateResultDTO([]));
 
 		// Act
 		backgroundJobsStore.setup();
@@ -44,11 +52,14 @@ describe('BackgroundJobsStore.getJobStatusUpdate()', () => {
 			data: {
 				plexServerId: 1,
 				forceSync: false,
-			},
+			} as SyncServerMediaJobUpdateDTO,
 		});
+
+		mock.onGet(BackgroundJobsPaths.getAllBackgroundJobsEndpoint()).reply(200, generateResultDTO([]));
 
 		// Act
 		backgroundJobsStore.setup();
+
 		const result = subscribeSpyTo(backgroundJobsStore.getSyncServerMediaJobUpdate(JobStatus.Started));
 
 		backgroundJobsStore.setStatusJobUpdate(testMsg);

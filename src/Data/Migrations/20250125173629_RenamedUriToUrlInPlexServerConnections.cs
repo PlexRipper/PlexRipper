a0f03eb1cddb368eb970cfb16567ee8acb71cbf5
JasonLandbridge <jasonlandbridge@protonmail.com>
2025-01-25 18:36:46 +0100

    build(WebAPI): RenamedUriToUrlInPlexServerConnections ef core migration

---
+++ src/Data/Migrations/20250125173629_RenamedUriToUrlInPlexServerConnections.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlexRipper.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenamedUriToUrlInPlexServerConnections : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Uri",
+                table: "PlexServerConnections",
+                newName: "Url");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Url",
+                table: "PlexServerConnections",
+                newName: "Uri");
+        }
+    }
+}

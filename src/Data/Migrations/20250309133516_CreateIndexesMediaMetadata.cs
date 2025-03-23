fd0eec66c56fbe9660c14bc64d0c1fe130965a66
JasonLandbridge <jasonlandbridge@protonmail.com>
2025-03-23 10:40:29 +0100

    style(WebAPI): formatting

--- src/Data/Migrations/20250309133516_CreateIndexesMediaMetadata.cs
+++ src/Data/Migrations/20250309133516_CreateIndexesMediaMetadata.cs
@@ -10,47 +10,30 @@ namespace PlexRipper.Data.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropIndex(
-                name: "IX_PlexGenres_PlexKey",
-                table: "PlexGenres");
+            migrationBuilder.DropIndex(name: "IX_PlexGenres_PlexKey", table: "PlexGenres");
 
-            migrationBuilder.DropIndex(
-                name: "IX_PlexCountries_PlexKey",
-                table: "PlexCountries");
+            migrationBuilder.DropIndex(name: "IX_PlexCountries_PlexKey", table: "PlexCountries");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_PlexGenres_Name",
-                table: "PlexGenres",
-                column: "Name",
-                unique: true);
+            migrationBuilder.CreateIndex(name: "IX_PlexGenres_Name", table: "PlexGenres", column: "Name", unique: true);
 
             migrationBuilder.CreateIndex(
                 name: "IX_PlexCountries_Name",
                 table: "PlexCountries",
                 column: "Name",
-                unique: true);
+                unique: true
+            );
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropIndex(
-                name: "IX_PlexGenres_Name",
-                table: "PlexGenres");
+            migrationBuilder.DropIndex(name: "IX_PlexGenres_Name", table: "PlexGenres");
 
-            migrationBuilder.DropIndex(
-                name: "IX_PlexCountries_Name",
-                table: "PlexCountries");
+            migrationBuilder.DropIndex(name: "IX_PlexCountries_Name", table: "PlexCountries");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_PlexGenres_PlexKey",
-                table: "PlexGenres",
-                column: "PlexKey");
+            migrationBuilder.CreateIndex(name: "IX_PlexGenres_PlexKey", table: "PlexGenres", column: "PlexKey");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_PlexCountries_PlexKey",
-                table: "PlexCountries",
-                column: "PlexKey");
+            migrationBuilder.CreateIndex(name: "IX_PlexCountries_PlexKey", table: "PlexCountries", column: "PlexKey");
         }
     }
 }

4b85eb5436c93be27108234ac3090d7023870278
JasonLandbridge <jasonlandbridge@protonmail.com>
2025-04-26 11:06:32 +0200

    test(WebAPI): Added unit test for mapping GetMediaMetaDataMetadata to LibraryMediaItemDTO

---
+++ tests/UnitTests/PlexApi.UnitTests/Mappers/MediaContainer/MediaContainerMappers.ToMediaItemDTO.UnitTests.cs
@@ -0,0 +1,215 @@
+using LukeHagar.PlexAPI.SDK.Models.Requests;
+using NodaTime;
+using PlexRipper.PlexApi;
+
+namespace PlexApi.UnitTests
+{
+    public class MediaContainerMappersUnitTests : BaseUnitTest
+    {
+        public MediaContainerMappersUnitTests(ITestOutputHelper output)
+            : base(output) { }
+
+        [Fact]
+        public void ShouldMapAllPropertiesCorrectly_WhenApiResponseHasValues()
+        {
+            // Arrange
+            var now = DateTimeOffset.UtcNow;
+            var addedAtUnixTime = now.ToUnixTimeSeconds();
+            var updatedAtUnixTime = now.AddDays(1).ToUnixTimeSeconds();
+
+            var sourceData = new GetMediaMetaDataMetadata
+            {
+                RatingKey = "123",
+                Key = "/library/metadata/123",
+                Type = "movie",
+                Title = "Test Movie",
+                Summary = "Test summary",
+                Year = 2023,
+                OriginalTitle = "Original Test Movie",
+                ChildCount = 5,
+                Studio = "Test Studio",
+                ContentRating = "PG-13",
+                Duration = 5400000, // 90 minutes in milliseconds
+                Thumb = "/thumb/path",
+                Art = "/art/path",
+                Theme = "/theme/path",
+                Guid = "plex://movie/guid",
+                AddedAt = addedAtUnixTime,
+                UpdatedAt = updatedAtUnixTime,
+                OriginallyAvailableAt = LocalDate.FromDateOnly(new DateOnly(2023, 01, 01)),
+                AudienceRating = 8.5f,
+                Rating = 9.0f,
+                GrandparentTitle = "Series Title",
+                ParentTitle = "Season 1",
+                ParentGuid = "plex://season/guid",
+                ParentRatingKey = "456",
+
+                // Collections of nested objects
+                Media =
+                [
+                    new GetMediaMetaDataMedia
+                    {
+                        Id = 789,
+                        Duration = 5400000,
+                        Bitrate = 5000,
+                        Width = 1920,
+                        Height = 1080,
+                        AspectRatio = 1.78f,
+                        AudioChannels = 6,
+                        AudioCodec = "aac",
+                        VideoCodec = "h264",
+                        VideoResolution = "1080p",
+                        Container = "mp4",
+                        VideoFrameRate = "24p",
+                        VideoProfile = "high",
+                        AudioProfile = "lc",
+                        HasVoiceActivity = true,
+                        Part =
+                        [
+                            new GetMediaMetaDataPart
+                            {
+                                Id = 101,
+                                Accessible = true,
+                                Exists = true,
+                                Key = "/library/parts/101",
+                                Indexes = "sd",
+                                Duration = 5400000,
+                                File = "/movies/testmovie.mp4",
+                                Size = 5000000000,
+                                Container = "mp4",
+                                VideoProfile = "high",
+                                AudioProfile = "lc",
+                                Stream =
+                                [
+                                    new GetMediaMetaDataStream
+                                    {
+                                        Id = 201,
+                                        StreamType = 1,
+                                        Default = true,
+                                        Codec = "h264",
+                                        Index = 0,
+                                        Bitrate = 4000,
+                                        Language = "English",
+                                        LanguageTag = "en",
+                                        LanguageCode = "eng",
+                                        Selected = true,
+                                    },
+                                ],
+                            },
+                        ],
+                    },
+                ],
+                Genre = [new GetMediaMetaDataGenre { Tag = "Action" }, new GetMediaMetaDataGenre { Tag = "Sci-Fi" }],
+                Country = [new GetMediaMetaDataCountry { Tag = "USA" }],
+                Role = [new GetMediaMetaDataRole { Tag = "Actor Name" }],
+                Ratings =
+                [
+                    new Ratings
+                    {
+                        Image = "imdb://image",
+                        Type = "imdb",
+                        Value = 8.7f,
+                    },
+                ],
+                Guids = [new GetMediaMetaDataGuids { Id = "imdb://tt1234567" }],
+            };
+
+            // Act
+            var result = sourceData.ToMediaItemDTO();
+
+            // Assert
+            result.ShouldNotBeNull();
+            result.RatingKey.ShouldBe(sourceData.RatingKey);
+            result.Key.ShouldBe(sourceData.Key);
+            result.Title.ShouldBe(sourceData.Title);
+            result.Summary.ShouldBe(sourceData.Summary);
+            result.Year.ShouldBe(sourceData.Year);
+            result.OriginalTitle.ShouldBe(sourceData.OriginalTitle);
+            result.ChildCount.ShouldBe(sourceData.ChildCount.Value);
+            result.Studio.ShouldBe(sourceData.Studio);
+            result.ContentRating.ShouldBe(sourceData.ContentRating);
+
+            // Duration is in milliseconds and we want seconds
+            result.Duration.ShouldBe(sourceData.Duration / 1000);
+
+            result.Thumb.ShouldBe(sourceData.Thumb);
+            result.Art.ShouldBe(sourceData.Art);
+            result.Theme.ShouldBe(sourceData.Theme);
+            result.Guid.ShouldBe(sourceData.Guid);
+
+            // DateTime conversions
+            result.AddedAt.ShouldBe(
+                DateTimeExtensions.FromUnixTime(sourceData.AddedAt),
+                tolerance: TimeSpan.FromSeconds(1)
+            );
+            result.UpdatedAt.ShouldBe(
+                DateTimeExtensions.FromUnixTime(sourceData.UpdatedAt),
+                tolerance: TimeSpan.FromSeconds(1)
+            );
+
+            // Parse the string date to NodaTime.LocalDate if not null
+            if (sourceData.OriginallyAvailableAt != null)
+            {
+                result.OriginallyAvailableAt.ShouldBe(sourceData.OriginallyAvailableAt.ToString());
+            }
+            else
+            {
+                result.OriginallyAvailableAt.ShouldBeNull();
+            }
+
+            result.GrandparentTitle.ShouldBe(sourceData.GrandparentTitle);
+            result.ParentTitle.ShouldBe(sourceData.ParentTitle);
+            result.ParentGuid.ShouldBe(sourceData.ParentGuid);
+            result.ParentRatingKey.ShouldBe(sourceData.ParentRatingKey);
+            result.AudienceRating.ShouldBe(sourceData.AudienceRating);
+            result.Rating.ShouldBe(sourceData.Rating.Value);
+
+            // Collections
+            result.Genre.Count.ShouldBe(sourceData.Genre.Count);
+            result.Genre.First().Tag.ShouldBe(sourceData.Genre.First().Tag);
+
+            result.Country.Count.ShouldBe(sourceData.Country.Count);
+            result.Country.First().Tag.ShouldBe(sourceData.Country.First().Tag);
+
+            result.Role.Count.ShouldBe(sourceData.Role.Count);
+            result.Role.First().Tag.ShouldBe(sourceData.Role.First().Tag);
+
+            result.Ratings.Count.ShouldBe(sourceData.Ratings.Count);
+            result.Ratings.First().Type.ShouldBe(sourceData.Ratings.First().Type);
+            result.Ratings.First().Value.ShouldBe(sourceData.Ratings.First().Value);
+
+            result.Guids.Count.ShouldBe(sourceData.Guids.Count);
+            result.Guids.First().Id.ShouldBe(sourceData.Guids.First().Id);
+
+            // Media
+            result.Media.Count.ShouldBe(1);
+            var media = result.Media.First();
+            var sourceMedia = sourceData.Media.First();
+
+            media.Id.ShouldBe(sourceMedia.Id);
+            media.Duration.ShouldBe(sourceMedia.Duration!.Value);
+            media.Bitrate.ShouldBe(sourceMedia.Bitrate!.Value);
+            media.Width.ShouldBe(sourceMedia.Width!.Value);
+            media.Height.ShouldBe(sourceMedia.Height!.Value);
+            media.HasVoiceActivity.ShouldBe(sourceMedia.HasVoiceActivity!.Value);
+
+            // Parts
+            media.Parts.Count.ShouldBe(1);
+            var part = media.Parts.First();
+            var sourcePart = sourceMedia.Part!.First();
+
+            part.Id.ShouldBe(sourcePart.Id);
+            part.Key.ShouldBe(sourcePart.Key);
+            part.File.ShouldBe(sourcePart.File);
+
+            // Streams
+            part.Stream.Count.ShouldBe(1);
+            var stream = part.Stream.First();
+            var sourceStream = sourcePart.Stream!.First();
+
+            stream.Id.ShouldBe(sourceStream.Id);
+            stream.Codec.ShouldBe(sourceStream.Codec);
+            stream.Language.ShouldBe(sourceStream.Language);
+        }
+    }
+}

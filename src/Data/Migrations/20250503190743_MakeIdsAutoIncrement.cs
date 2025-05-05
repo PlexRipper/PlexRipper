ad0d0b1df8898eff2ae6c801b417ba791660ba47
JasonLandbridge <jasonlandbridge@protonmail.com>
2025-05-05 09:48:25 +0200

    style(WebAPI): formatting

--- src/Data/Migrations/20250503190743_MakeIdsAutoIncrement.cs
+++ src/Data/Migrations/20250503190743_MakeIdsAutoIncrement.cs
@@ -10,116 +10,140 @@ namespace PlexRipper.Data.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AlterColumn<int>(
-                name: "Id",
-                table: "PlexTvShowEpisodeDataStreams",
-                type: "INTEGER",
-                nullable: false,
-                oldClrType: typeof(int),
-                oldType: "INTEGER")
+            migrationBuilder
+                .AlterColumn<int>(
+                    name: "Id",
+                    table: "PlexTvShowEpisodeDataStreams",
+                    type: "INTEGER",
+                    nullable: false,
+                    oldClrType: typeof(int),
+                    oldType: "INTEGER"
+                )
                 .Annotation("Sqlite:Autoincrement", true);
 
-            migrationBuilder.AlterColumn<int>(
-                name: "Id",
-                table: "PlexTvShowEpisodeDataParts",
-                type: "INTEGER",
-                nullable: false,
-                oldClrType: typeof(int),
-                oldType: "INTEGER")
+            migrationBuilder
+                .AlterColumn<int>(
+                    name: "Id",
+                    table: "PlexTvShowEpisodeDataParts",
+                    type: "INTEGER",
+                    nullable: false,
+                    oldClrType: typeof(int),
+                    oldType: "INTEGER"
+                )
                 .Annotation("Sqlite:Autoincrement", true);
 
-            migrationBuilder.AlterColumn<int>(
-                name: "Id",
-                table: "PlexTvShowEpisodeData",
-                type: "INTEGER",
-                nullable: false,
-                oldClrType: typeof(int),
-                oldType: "INTEGER")
+            migrationBuilder
+                .AlterColumn<int>(
+                    name: "Id",
+                    table: "PlexTvShowEpisodeData",
+                    type: "INTEGER",
+                    nullable: false,
+                    oldClrType: typeof(int),
+                    oldType: "INTEGER"
+                )
                 .Annotation("Sqlite:Autoincrement", true);
 
-            migrationBuilder.AlterColumn<int>(
-                name: "Id",
-                table: "PlexMovieDataStreams",
-                type: "INTEGER",
-                nullable: false,
-                oldClrType: typeof(int),
-                oldType: "INTEGER")
+            migrationBuilder
+                .AlterColumn<int>(
+                    name: "Id",
+                    table: "PlexMovieDataStreams",
+                    type: "INTEGER",
+                    nullable: false,
+                    oldClrType: typeof(int),
+                    oldType: "INTEGER"
+                )
                 .Annotation("Sqlite:Autoincrement", true);
 
-            migrationBuilder.AlterColumn<int>(
-                name: "Id",
-                table: "PlexMovieDataParts",
-                type: "INTEGER",
-                nullable: false,
-                oldClrType: typeof(int),
-                oldType: "INTEGER")
+            migrationBuilder
+                .AlterColumn<int>(
+                    name: "Id",
+                    table: "PlexMovieDataParts",
+                    type: "INTEGER",
+                    nullable: false,
+                    oldClrType: typeof(int),
+                    oldType: "INTEGER"
+                )
                 .Annotation("Sqlite:Autoincrement", true);
 
-            migrationBuilder.AlterColumn<int>(
-                name: "Id",
-                table: "PlexMovieData",
-                type: "INTEGER",
-                nullable: false,
-                oldClrType: typeof(int),
-                oldType: "INTEGER")
+            migrationBuilder
+                .AlterColumn<int>(
+                    name: "Id",
+                    table: "PlexMovieData",
+                    type: "INTEGER",
+                    nullable: false,
+                    oldClrType: typeof(int),
+                    oldType: "INTEGER"
+                )
                 .Annotation("Sqlite:Autoincrement", true);
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AlterColumn<int>(
-                name: "Id",
-                table: "PlexTvShowEpisodeDataStreams",
-                type: "INTEGER",
-                nullable: false,
-                oldClrType: typeof(int),
-                oldType: "INTEGER")
+            migrationBuilder
+                .AlterColumn<int>(
+                    name: "Id",
+                    table: "PlexTvShowEpisodeDataStreams",
+                    type: "INTEGER",
+                    nullable: false,
+                    oldClrType: typeof(int),
+                    oldType: "INTEGER"
+                )
                 .OldAnnotation("Sqlite:Autoincrement", true);
 
-            migrationBuilder.AlterColumn<int>(
-                name: "Id",
-                table: "PlexTvShowEpisodeDataParts",
-                type: "INTEGER",
-                nullable: false,
-                oldClrType: typeof(int),
-                oldType: "INTEGER")
+            migrationBuilder
+                .AlterColumn<int>(
+                    name: "Id",
+                    table: "PlexTvShowEpisodeDataParts",
+                    type: "INTEGER",
+                    nullable: false,
+                    oldClrType: typeof(int),
+                    oldType: "INTEGER"
+                )
                 .OldAnnotation("Sqlite:Autoincrement", true);
 
-            migrationBuilder.AlterColumn<int>(
-                name: "Id",
-                table: "PlexTvShowEpisodeData",
-                type: "INTEGER",
-                nullable: false,
-                oldClrType: typeof(int),
-                oldType: "INTEGER")
+            migrationBuilder
+                .AlterColumn<int>(
+                    name: "Id",
+                    table: "PlexTvShowEpisodeData",
+                    type: "INTEGER",
+                    nullable: false,
+                    oldClrType: typeof(int),
+                    oldType: "INTEGER"
+                )
                 .OldAnnotation("Sqlite:Autoincrement", true);
 
-            migrationBuilder.AlterColumn<int>(
-                name: "Id",
-                table: "PlexMovieDataStreams",
-                type: "INTEGER",
-                nullable: false,
-                oldClrType: typeof(int),
-                oldType: "INTEGER")
+            migrationBuilder
+                .AlterColumn<int>(
+                    name: "Id",
+                    table: "PlexMovieDataStreams",
+                    type: "INTEGER",
+                    nullable: false,
+                    oldClrType: typeof(int),
+                    oldType: "INTEGER"
+                )
                 .OldAnnotation("Sqlite:Autoincrement", true);
 
-            migrationBuilder.AlterColumn<int>(
-                name: "Id",
-                table: "PlexMovieDataParts",
-                type: "INTEGER",
-                nullable: false,
-                oldClrType: typeof(int),
-                oldType: "INTEGER")
+            migrationBuilder
+                .AlterColumn<int>(
+                    name: "Id",
+                    table: "PlexMovieDataParts",
+                    type: "INTEGER",
+                    nullable: false,
+                    oldClrType: typeof(int),
+                    oldType: "INTEGER"
+                )
                 .OldAnnotation("Sqlite:Autoincrement", true);
 
-            migrationBuilder.AlterColumn<int>(
-                name: "Id",
-                table: "PlexMovieData",
-                type: "INTEGER",
-                nullable: false,
-                oldClrType: typeof(int),
-                oldType: "INTEGER")
+            migrationBuilder
+                .AlterColumn<int>(
+                    name: "Id",
+                    table: "PlexMovieData",
+                    type: "INTEGER",
+                    nullable: false,
+                    oldClrType: typeof(int),
+                    oldType: "INTEGER"
+                )
                 .OldAnnotation("Sqlite:Autoincrement", true);
         }
     }

30be4b6ef860b8e52c8feaa7f79522d3705e18e5
JasonLandbridge <jasonlandbridge@protonmail.com>
2025-06-03 16:36:52 +0200

    refactor(WebAPI): adjust migration to improve code readability and maintain consistency

--- src/Data/Migrations/20250529205108_AddGenreCountryAndActorCountToPlexLibrary.cs
+++ src/Data/Migrations/20250529205108_AddGenreCountryAndActorCountToPlexLibrary.cs
@@ -10,45 +10,45 @@ namespace PlexRipper.Data.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AddColumn<int>(
-                name: "ActorsCount",
-                table: "PlexLibraries",
-                type: "INTEGER",
-                nullable: false,
-                defaultValue: 0)
+            migrationBuilder
+                .AddColumn<int>(
+                    name: "ActorsCount",
+                    table: "PlexLibraries",
+                    type: "INTEGER",
+                    nullable: false,
+                    defaultValue: 0
+                )
                 .Annotation("Relational:ColumnOrder", 15);
 
-            migrationBuilder.AddColumn<int>(
-                name: "CountriesCount",
-                table: "PlexLibraries",
-                type: "INTEGER",
-                nullable: false,
-                defaultValue: 0)
+            migrationBuilder
+                .AddColumn<int>(
+                    name: "CountriesCount",
+                    table: "PlexLibraries",
+                    type: "INTEGER",
+                    nullable: false,
+                    defaultValue: 0
+                )
                 .Annotation("Relational:ColumnOrder", 17);
 
-            migrationBuilder.AddColumn<int>(
-                name: "GenresCount",
-                table: "PlexLibraries",
-                type: "INTEGER",
-                nullable: false,
-                defaultValue: 0)
+            migrationBuilder
+                .AddColumn<int>(
+                    name: "GenresCount",
+                    table: "PlexLibraries",
+                    type: "INTEGER",
+                    nullable: false,
+                    defaultValue: 0
+                )
                 .Annotation("Relational:ColumnOrder", 16);
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropColumn(
-                name: "ActorsCount",
-                table: "PlexLibraries");
+            migrationBuilder.DropColumn(name: "ActorsCount", table: "PlexLibraries");
 
-            migrationBuilder.DropColumn(
-                name: "CountriesCount",
-                table: "PlexLibraries");
+            migrationBuilder.DropColumn(name: "CountriesCount", table: "PlexLibraries");
 
-            migrationBuilder.DropColumn(
-                name: "GenresCount",
-                table: "PlexLibraries");
+            migrationBuilder.DropColumn(name: "GenresCount", table: "PlexLibraries");
         }
     }
 }

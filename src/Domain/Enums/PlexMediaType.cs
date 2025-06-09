4e42fb28f23a87a4c2d0339f39595fb1dd6ebe50
JasonLandbridge <jasonlandbridge@protonmail.com>
2025-06-09 09:47:44 +0200

    refactor(WebAPI): update media type enum values

--- src/Domain/Enums/PlexMediaType.cs
+++ src/Domain/Enums/PlexMediaType.cs
@@ -26,21 +26,27 @@ public enum PlexMediaType
     [EnumMember(Value = nameof(Music))]
     Music = 5,
 
+    [EnumMember(Value = nameof(Artist))]
+    Artist = 6,
+
     [EnumMember(Value = nameof(Album))]
-    Album = 6,
+    Album = 7,
 
     [EnumMember(Value = nameof(Song))]
-    Song = 7,
+    Song = 8,
+
+    [EnumMember(Value = nameof(PhotoAlbum))]
+    PhotoAlbum = 9,
 
     [EnumMember(Value = nameof(Photos))]
-    Photos = 8,
+    Photos = 10,
 
     [EnumMember(Value = nameof(OtherVideos))]
-    OtherVideos = 9,
+    OtherVideos = 11,
 
     [EnumMember(Value = nameof(Games))]
-    Games = 10,
+    Games = 12,
 
     [EnumMember(Value = nameof(Unknown))]
-    Unknown = 11,
+    Unknown = 13,
 }
--- src/PlexApi/Common/Extensions/PlexMediaTypeToApiTypeExtensions.cs
+++ src/PlexApi/Common/Extensions/PlexMediaTypeToApiTypeExtensions.cs
@@ -59,4 +59,33 @@ public static class PlexMediaTypeToApiTypeExtensions
 
         return (T)Enum.ToObject(typeof(T), enumInt);
     }
+
+    /// <summary>
+    /// Converts a string representation of a Plex media type to the corresponding PlexMediaType enum value.
+    /// </summary>
+    /// <remarks>NOTE: This method assumes the string is given as an int in string format.</remarks>
+    /// <example> 1 => PlexMediaType.Movie</example>
+    /// <example> 2 => PlexMediaType.TvShow</example>
+    /// <example> 3 => PlexMediaType.Season</example>
+    /// <example> 4 => PlexMediaType.Episode</example>
+    /// <exception cref="ArgumentOutOfRangeException"> Thrown when the provided string does not match any known Plex media type.</exception>
+    public static PlexMediaType ToPlexMediaTypeFromTypeInt(this string apiType)
+    {
+        if (!int.TryParse(apiType, out var i))
+            return PlexMediaType.Unknown;
+
+        return i switch
+        {
+            1 => PlexMediaType.Movie,
+            2 => PlexMediaType.TvShow,
+            3 => PlexMediaType.Season,
+            4 => PlexMediaType.Episode,
+            5 => PlexMediaType.Artist,
+            6 => PlexMediaType.Album,
+            7 => PlexMediaType.Song,
+            8 => PlexMediaType.PhotoAlbum,
+            9 => PlexMediaType.Photos,
+            _ => throw new ArgumentOutOfRangeException(nameof(apiType), $"Unknown media type value: {apiType}"),
+        };
+    }
 }

8a81d4c1e3f7ff386e95d9d2f27a6469ce8a121a
JasonLandbridge <jasonlandbridge@protonmail.com>
2025-07-20 00:19:32 +0200

    fix(WebAPI): Fixed missing quality chips from media overview

--- src/Application/PlexMedia/GetMediaDetailById/GetMediaDetailByIdEndpoint.cs
+++ src/Application/PlexMedia/GetMediaDetailById/GetMediaDetailByIdEndpoint.cs
@@ -113,7 +113,8 @@ public class GetMediaDetailByIdEndpoint : BaseEndpoint<GetMediaDetailByIdEndpoin
 
         foreach (var season in plexTvShow.Seasons)
             season.Episodes = _dbContext
-                .PlexTvShowEpisodes.Where(x => x.TvShowSeasonId == season.Id)
+                .PlexTvShowEpisodes.IncludeMediaData()
+                .Where(x => x.TvShowSeasonId == season.Id)
                 .Take(season.ChildCount)
                 .ToList();
 

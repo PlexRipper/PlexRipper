5ca067eebe12333dc90b0f2b59eb40fd68cc72e2
JasonLandbridge <jasonlandbridge@protonmail.com>
2025-07-19 17:04:27 +0200

    feat(Web-UI): Added a metadata search input field so you can find a specific actor more easily

--- src/WebAPI/ClientApp/src/components/MediaOverview/MediaFilterMenu.vue
+++ src/WebAPI/ClientApp/src/components/MediaOverview/MediaFilterMenu.vue
@@ -9,7 +9,7 @@
 				<q-item
 					v-close-popup
 					clickable
-					@click="mediaOverviewStore.clearMetaDataFilter()">
+					@click="clearMetadataFilter()">
 					<q-item-section>{{ t('components.media-filter-menu.all') }}</q-item-section>
 				</q-item>
 				<q-separator />
@@ -24,7 +24,7 @@
 			<template v-else>
 				<q-item
 					clickable
-					@click="menuIndex = MediaMetaDataTypes.None">
+					@click="goBackToMainMenu()">
 					<q-item-section avatar>
 						<q-icon
 							size="2em"
@@ -33,6 +33,21 @@
 					<q-item-section>
 						{{ menuItems.find((item) => item.type === menuIndex)?.text }}
 					</q-item-section>
+					<q-item-section side>
+						<IconButton
+							icon="mdi-magnify"
+							@click.stop="toggleMenuSearch" />
+					</q-item-section>
+				</q-item>
+				<!-- Search Input -->
+				<q-item
+					v-if="showMenuSearch"
+					clickable>
+					<q-input
+						v-model="menuFilterQuery"
+						color="red"
+						class="full-width"
+						dense />
 				</q-item>
 				<q-separator />
 				<QScroll
@@ -41,7 +56,7 @@
 					<!-- Show Genres Sub-Menu -->
 					<template v-if="menuIndex === MediaMetaDataTypes.Genres">
 						<q-item
-							v-for="genre in mediaOverviewStore.getGenres"
+							v-for="genre in mediaOverviewStore.getGenres.filter(x => x.name.toLowerCase().includes(menuFilterQuery.toLowerCase()))"
 							:key="genre.id"
 							clickable
 							@click="setMetadataFilter({ genreId: genre.id })">
@@ -59,7 +74,7 @@
 					<!-- Show Countries Sub-Menu -->
 					<template v-if="menuIndex === MediaMetaDataTypes.Country">
 						<q-item
-							v-for="country in mediaOverviewStore.getCountries"
+							v-for="country in mediaOverviewStore.getCountries.filter(x => x.name.toLowerCase().includes(menuFilterQuery.toLowerCase()))"
 							:key="country.id"
 							clickable
 							@click="setMetadataFilter({ countryId: country.id })">
@@ -75,7 +90,7 @@
 					<!-- Show Roles Sub-Menu -->
 					<template v-if="menuIndex === MediaMetaDataTypes.Roles">
 						<q-item
-							v-for="role in mediaOverviewStore.getRoles"
+							v-for="role in mediaOverviewStore.getRoles.filter(x => x.name.toLowerCase().includes(menuFilterQuery.toLowerCase()))"
 							:key="role.id"
 							clickable
 							@click="setMetadataFilter({ roleId: role.id })">
@@ -94,13 +109,17 @@
 </template>
 
 <script setup lang="ts">
-import { set } from '@vueuse/core';
+import { get, set } from '@vueuse/core';
 import { useMediaOverviewStore } from '@store';
 import { MediaMetaDataTypes } from '@enums';
+import IconButton from '@components/Buttons/IconButton.vue';
 
 const menuIndex = ref<MediaMetaDataTypes>(MediaMetaDataTypes.None);
 const mediaOverviewStore = useMediaOverviewStore();
 const { t } = useI18n();
+
+const showMenuSearch = ref(false);
+const menuFilterQuery = ref<string>('');
 
 withDefaults(defineProps<{
 	libraryId?: number;
@@ -127,6 +146,17 @@ function onMenuOpen(category: MediaMetaDataTypes) {
 	set(menuIndex, category);
 }
 
+function toggleMenuSearch() {
+	set(showMenuSearch, !get(showMenuSearch));
+	set(menuFilterQuery, '');
+}
+
+function goBackToMainMenu() {
+	set(menuIndex, MediaMetaDataTypes.None);
+	set(showMenuSearch, false);
+	set(menuFilterQuery, '');
+}
+
 function setMetadataFilter({
 	countryId,
 	roleId,
@@ -142,5 +172,9 @@ function setMetadataFilter({
 			roleId,
 			genreId,
 		}).subscribe());
+}
+
+function clearMetadataFilter() {
+	useSubscription(mediaOverviewStore.clearMetaDataFilter().subscribe());
 }
 </script>

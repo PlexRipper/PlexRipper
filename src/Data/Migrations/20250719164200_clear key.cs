766309fd19b3f8a2e73f4ca2868ce2dacb59551c
JasonLandbridge <jasonlandbridge@protonmail.com>
2025-07-19 18:42:25 +0200

    build(WebAPI): Add temp migration

---
+++ src/Data/Migrations/20250719164200_clear key.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlexRipper.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class clearkey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PlexKey",
+                table: "PlexActors");
+
+            migrationBuilder.DropColumn(
+                name: "Thumb",
+                table: "PlexActors");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PlexKey",
+                table: "PlexActors",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Thumb",
+                table: "PlexActors",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}

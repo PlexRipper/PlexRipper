ec84261c78249c44317dc6b4f1151d58aab86e25
JasonLandbridge <jasonlandbridge@protonmail.com>
2025-07-19 13:16:19 +0200

    build(WebAPI): Remove plexkey from metadata tables

---
+++ src/Data/Migrations/20250719111539_RemovePlexKey.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlexRipper.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovePlexKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PlexKey",
+                table: "PlexLibraryGenres");
+
+            migrationBuilder.DropColumn(
+                name: "PlexKey",
+                table: "PlexLibraryCountries");
+
+            migrationBuilder.DropColumn(
+                name: "PlexKey",
+                table: "PlexLibraryActors");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PlexKey",
+                table: "PlexLibraryGenres",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("Relational:ColumnOrder", 3);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PlexKey",
+                table: "PlexLibraryCountries",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("Relational:ColumnOrder", 3);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PlexKey",
+                table: "PlexLibraryActors",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("Relational:ColumnOrder", 3);
+        }
+    }
+}
--- src/Domain/Entities/JoinTables/PlexLibraryCountries.cs
+++ src/Domain/Entities/JoinTables/PlexLibraryCountries.cs
@@ -8,23 +8,15 @@ public class PlexLibraryCountries
     public PlexLibraryCountries() { }
 
     [SetsRequiredMembers]
-    public PlexLibraryCountries(int plexLibraryId, int plexCountryId, int plexKey)
+    public PlexLibraryCountries(int plexLibraryId, int plexCountryId)
     {
         PlexLibraryId = plexLibraryId;
         PlexCountryId = plexCountryId;
-        PlexKey = plexKey;
     }
 
     [Column(Order = 1)]
-    public required int PlexLibraryId { get; set; }
+    public required int PlexLibraryId { get; init; }
 
     [Column(Order = 2)]
-    public required int PlexCountryId { get; set; }
-
-    /// <summary>
-    /// The PlexKey is the unique identifier for the country in Plex in the context of the PlexLibrary.
-    /// Meaning it is not globally unique across all Plex servers.
-    /// </summary>
-    [Column(Order = 3)]
-    public required int PlexKey { get; init; }
+    public required int PlexCountryId { get; init; }
 }

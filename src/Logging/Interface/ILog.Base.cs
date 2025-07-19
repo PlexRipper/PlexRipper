ee0d04f4d1034f53b5f78657b19369617c62639b
JasonLandbridge <jasonlandbridge@protonmail.com>
2025-07-19 13:11:44 +0200

    refactor(WebAPI): add verbose and debug log level curl logging for requests

--- src/Logging/Interface/ILog.Base.cs
+++ src/Logging/Interface/ILog.Base.cs
@@ -5,4 +5,8 @@ namespace Logging.Interface;
 public partial interface ILog
 {
     bool IsLogLevelEnabled(LogEventLevel logLevel = LogEventLevel.Debug);
+
+    bool IsLogLevelVerbose();
+
+    bool IsLogLevelDebug();
 }

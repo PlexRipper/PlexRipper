5da85abad63fde348a53cd6a8db05983c245df04
JasonLandbridge <jasonlandbridge@protonmail.com>
2025-08-03 11:07:32 +0200

    refactor(Web-UI): Add missing component

---
+++ src/WebAPI/ClientApp/src/components/DebugTools/MediaQualityDebug.vue
@@ -0,0 +1,37 @@
+<template>
+	<QSection :header="$t('pages.debug.buttons.media-quality-header')">
+		<QRow
+			justify="between"
+			class="q-my-md">
+			<MediaQuality
+				:qualities="qualities"
+				:truncated="false" />
+		</QRow>
+		<QRow
+			justify="between"
+			class="q-my-md">
+			<MediaQuality
+				:qualities="qualities"
+				:truncated="true" />
+		</QRow>
+	</QSection>
+</template>
+
+<script setup lang="ts">
+import { type PlexMediaQualityDTO, PlexMediaType, VideoQuality } from '@dto';
+import QSection from '@components/Common/QSection.vue';
+
+const qualities: PlexMediaQualityDTO[] = [
+	{ quality: VideoQuality.Unknown, dataId: 0, mediaDataType: PlexMediaType.Unknown, mediaId: 0 },
+	{ quality: VideoQuality.SubSD144P, dataId: 0, mediaDataType: PlexMediaType.Unknown, mediaId: 0 },
+	{ quality: VideoQuality.SubSDCIF, dataId: 0, mediaDataType: PlexMediaType.Unknown, mediaId: 0 },
+	{ quality: VideoQuality.NHD, dataId: 0, mediaDataType: PlexMediaType.Unknown, mediaId: 0 },
+	{ quality: VideoQuality.SD, dataId: 0, mediaDataType: PlexMediaType.Unknown, mediaId: 0 },
+	{ quality: VideoQuality.DVD, dataId: 0, mediaDataType: PlexMediaType.Unknown, mediaId: 0 },
+	{ quality: VideoQuality.HD, dataId: 0, mediaDataType: PlexMediaType.Unknown, mediaId: 0 },
+	{ quality: VideoQuality.FullHD, dataId: 0, mediaDataType: PlexMediaType.Unknown, mediaId: 0 },
+	{ quality: VideoQuality.QHD, dataId: 0, mediaDataType: PlexMediaType.Unknown, mediaId: 0 },
+	{ quality: VideoQuality.UHD4K, dataId: 0, mediaDataType: PlexMediaType.Unknown, mediaId: 0 },
+	{ quality: VideoQuality.UHD8K, dataId: 0, mediaDataType: PlexMediaType.Unknown, mediaId: 0 },
+];
+</script>

a98e5537574133d8a3c1c97b8e96e6f72a26cf85
JasonLandbridge <jasonlandbridge@protonmail.com>
2025-09-02 10:55:16 +0200

    fix(WebAPI): Fixed the source generated info not being passed down when using LogByType

--- src/FluentResultExtensions/ResultExtensions.Logging.cs
+++ src/FluentResultExtensions/ResultExtensions.Logging.cs
@@ -27,22 +27,28 @@ public static partial class ResultExtensions
         switch (logLevel)
         {
             case LogEventLevel.Verbose:
-                _log.Here().Verbose(messageTemplate, memberName, sourceFilePath, sourceLineNumber);
+                _log.Here(sourceFilePath, memberName, sourceLineNumber)
+                    .Verbose(messageTemplate, memberName, sourceFilePath, sourceLineNumber);
                 break;
             case LogEventLevel.Debug:
-                _log.Here().Debug(messageTemplate, memberName, sourceFilePath, sourceLineNumber);
+                _log.Here(sourceFilePath, memberName, sourceLineNumber)
+                    .Debug(messageTemplate, memberName, sourceFilePath, sourceLineNumber);
                 break;
             case LogEventLevel.Information:
-                _log.Here().Information(messageTemplate, memberName, sourceFilePath, sourceLineNumber);
+                _log.Here(sourceFilePath, memberName, sourceLineNumber)
+                    .Information(messageTemplate, memberName, sourceFilePath, sourceLineNumber);
                 break;
             case LogEventLevel.Warning:
-                _log.Here().Warning(e, messageTemplate, memberName, sourceFilePath, sourceLineNumber);
+                _log.Here(sourceFilePath, memberName, sourceLineNumber)
+                    .Warning(e, messageTemplate, memberName, sourceFilePath, sourceLineNumber);
                 break;
             case LogEventLevel.Error:
-                _log.Here().ErrorResult(e, messageTemplate, memberName, sourceFilePath, sourceLineNumber);
+                _log.Here(sourceFilePath, memberName, sourceLineNumber)
+                    .ErrorResult(e, messageTemplate, memberName, sourceFilePath, sourceLineNumber);
                 break;
             case LogEventLevel.Fatal:
-                _log.Here().Fatal(e, messageTemplate, memberName, sourceFilePath, sourceLineNumber);
+                _log.Here(sourceFilePath, memberName, sourceLineNumber)
+                    .Fatal(e, messageTemplate, memberName, sourceFilePath, sourceLineNumber);
                 break;
             default:
                 throw new ArgumentOutOfRangeException(nameof(logLevel), logLevel, null);

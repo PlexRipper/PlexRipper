5dbff598ab449e99c49a52dd0393d15459407f17
JasonLandbridge <jasonlandbridge@protonmail.com>
2025-09-06 17:18:05 +0200

    build(WebAPI): Add computed MediaCount migration column to PlexLibraries

--- src/Data/Migrations/20250906151309_AddMediaCountComputedColumn.Designer.cs
+++ src/Data/Migrations/20250906151309_AddMediaCountComputedColumn.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Reaparr.Data;
 
@@ -10,9 +11,11 @@ using Reaparr.Data;
 namespace Reaparr.Data.Migrations
 {
     [DbContext(typeof(ReaparrDbContext))]
-    partial class ReaparrDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250906151309_AddMediaCountComputedColumn")]
+    partial class AddMediaCountComputedColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Data/Migrations/20250906151309_AddMediaCountComputedColumn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Reaparr.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMediaCountComputedColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MediaCount",
+                table: "PlexLibraries",
+                type: "INTEGER",
+                nullable: false,
+                computedColumnSql: "CASE WHEN Type = 'Movie' THEN MovieCount WHEN Type = 'TvShow' THEN TvShowCount ELSE -1 END"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(name: "MediaCount", table: "PlexLibraries");
+        }
+    }
+}

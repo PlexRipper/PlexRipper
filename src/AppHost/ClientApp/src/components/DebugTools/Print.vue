086b2b1038856711950386107248a779f3628089
JasonLandbridge <jasonlandbridge@protonmail.com>
2025-09-13 13:00:29 +0200

    refactor(Web-UI): Add missing files

---
+++ src/AppHost/ClientApp/src/components/DebugTools/Print.vue
@@ -0,0 +1,42 @@
+<template>
+	<q-list
+		v-if="settingsStore.debugMode || forceShow"
+		bordered
+		class="rounded-borders">
+		<q-expansion-item
+			switch-toggle-side
+			expand-separator
+			:label="title"
+			icon="mdi-debug"
+			:default-opened="expanded">
+			<q-card
+				:style="{ maxHeight: height + 'px' }"
+				class="scroll">
+				<q-card-section>
+					<pre><slot /></pre>
+				</q-card-section>
+			</q-card>
+		</q-expansion-item>
+	</q-list>
+</template>
+
+<script setup lang="ts">
+import { useSettingsStore } from '@store';
+
+const settingsStore = useSettingsStore();
+
+withDefaults(
+	defineProps<{
+		title?: string;
+		height?: number;
+		expanded?: boolean;
+		forceShow?: boolean;
+	}>(),
+	{
+		title: 'Print',
+		height: 500,
+		expanded: false,
+		forceShow: false,
+	},
+);
+</script>

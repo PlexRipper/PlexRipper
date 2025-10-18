e4822ea9e345e3cd45974f132bb262ee396a2ed7
JasonLandbridge <jasonlandbridge@protonmail.com>
2025-10-18 20:02:32 +0200

    refactor(Web-UI): Add comment

--- src/AppHost/ClientApp/cypress/support/commands/snippets/correct-default-folder-paths.ts
+++ src/AppHost/ClientApp/cypress/support/commands/snippets/correct-default-folder-paths.ts
@@ -4,10 +4,12 @@ import { kebabCase } from 'lodash-es';
 Cypress.Commands.add('correctDefaultFolderPaths', () => {
 	const folderTypes = [FolderType.DownloadFolder, FolderType.MovieFolder, FolderType.TvShowFolder];
 
+	// NOTE: FolderPaths are already set in /cypress/fixtures/api-mock/folder-paths.mock-api.ts
 	for (const folderType of folderTypes) {
 		// Update DownloadFolder
 		cy.getCy(`default-${kebabCase(folderType)}-valid-icon`).should('have.class', 'invalid-icon');
 		cy.getCy(`default-${kebabCase(folderType)}-edit-button`).click();
+
 		// Go to the root directory
 		cy.getCy('directory-browser-row-return').click();
 		const directory = folderType.replace('Folder', 's');

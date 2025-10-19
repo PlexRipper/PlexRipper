a9901cc2fa317fca5755f6a7dd402460ee905d80
JasonLandbridge <jasonlandbridge@protonmail.com>
2025-10-19 13:33:59 +0200

    build(WebAPI): Installed Microsoft.AspNetCore.SignalR.Protocols.MessagePack

--- src/AppHost/Startup/Startup.Services.cs
+++ src/AppHost/Startup/Startup.Services.cs
@@ -3,6 +3,7 @@ using System.Text.Json.Serialization;
 using FastEndpoints;
 using FastEndpoints.Security;
 using FastEndpoints.Swagger;
+using MessagePack;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Identity;
@@ -88,9 +89,11 @@ public static partial class Startup
             // Setup SignalR
             services
                 .AddSignalR()
-                .AddJsonProtocol(options =>
-                    options.PayloadSerializerOptions = DefaultJsonSerializerOptions.ConfigStandard
-                );
+                .AddMessagePackProtocol(options =>
+                {
+                    options.SerializerOptions = MessagePackSerializerOptions.Standard
+                        .WithSecurity(MessagePackSecurity.UntrustedData);
+                });
 
             services.SwaggerDocument(options =>
             {

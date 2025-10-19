8d32e3bb73e011ee41057378f4d7178ca42b549b
JasonLandbridge <jasonlandbridge@protonmail.com>
2025-10-19 13:40:55 +0200

    fix(WebAPI): Fixed an issue where restarting a download task will throw an error because it could not find the filepath

--- src/Domain/Entities/Download/Base/DownloadTaskFileBase.cs
+++ src/Domain/Entities/Download/Base/DownloadTaskFileBase.cs
@@ -88,7 +88,7 @@ public abstract class DownloadTaskFileBase : DownloadTaskBase, IDownloadTaskProg
     public string DestinationFilePath => Path.Join(DestinationDirectory, FileName);
 
     [NotMapped]
-    public string FilePath => DownloadWorkerTasks.Select(x => x.DownloadFilePath).Distinct().First();
+    public string FilePath => Path.Combine(DownloadDirectory, FileName);
 
     [NotMapped]
     public decimal Percentage => DownloadTaskPhaseExtensions.Percentage(DownloadTaskPhase, this, this);

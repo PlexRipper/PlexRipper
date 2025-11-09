0016287be3eaa19364ce8d8770fb9e8a8adedbe9
JasonLandbridge <jasonlandbridge@protonmail.com>
2025-11-09 14:59:53 +0100

    build(WebAPI): Add DownloadClientSessions table migration

--- src/Identity/Migrations/20251109135802_AddedDownloadClientSessions.Designer.cs
+++ src/Identity/Migrations/20251109135802_AddedDownloadClientSessions.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Reaparr.Identity;
 
@@ -10,9 +11,11 @@ using Reaparr.Identity;
 namespace Reaparr.Identity.Migrations
 {
     [DbContext(typeof(AuthDbContext))]
-    partial class AuthDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251109135802_AddedDownloadClientSessions")]
+    partial class AddedDownloadClientSessions
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.8");

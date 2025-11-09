fda45e634ddbb0d10ea472120e3d573f770446fb
JasonLandbridge <jasonlandbridge@protonmail.com>
2025-11-09 14:57:46 +0100

    style(WebAPI): Formatting

--- src/Identity.Contracts/Models/DownloadClientSession.cs
+++ src/Identity.Contracts/Models/DownloadClientSession.cs
@@ -6,10 +6,8 @@ public class DownloadClientSession
 {
     [Key]
     public required string Sid { get; set; }
-    
+
     public required DateTimeOffset ExpiresAt { get; set; }
-    
+
     public required string Username { get; set; }
 }
-
-

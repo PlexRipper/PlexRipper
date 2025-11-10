3fb8f56f7dd5b5af09b3bd560cd513e5429940e1
JasonLandbridge <jasonlandbridge@protonmail.com>
2025-11-10 15:12:17 +0100

    refactor(WebAPI): Added Radarr settings to ReaparrCongfig.json

--- src/Settings.Contracts/Interfaces/Models/IRadarrSettings.cs
+++ src/Settings.Contracts/Interfaces/Models/IRadarrSettings.cs
@@ -2,7 +2,19 @@ namespace Reaparr.Settings.Contracts;
 
 public interface IRadarrSettings
 {
-    string BaseUrl { get; set; }
+    /// <summary>
+    /// Gets or sets the base URL of the Radarr instance.
+    /// <example>http://localhost:8989</example>
+    /// </summary>
+    string RadarrBaseUrl { get; set; }
 
-    string ApiKey { get; set; }
+    /// <summary>
+    /// Gets or sets the Radarr API Key used to authenticate API calls to Radarr.
+    /// </summary>
+    string RadarrApiKey { get; set; }
+
+    /// <summary>
+    /// Indicates whether Radarr has been configured by Reaparr
+    /// </summary>
+    bool IsConfigured { get; set; }
 }
--- src/Settings.Contracts/Modules/RadarrSettings.cs
+++ src/Settings.Contracts/Modules/RadarrSettings.cs
@@ -2,20 +2,36 @@ namespace Reaparr.Settings.Contracts;
 
 public record RadarrSettings : BaseSettingsModule<RadarrSettings>, IRadarrSettings
 {
-    private string _baseUrl = string.Empty;
-    private string _apiKey = string.Empty;
+    private string _radarrBaseUrl = string.Empty;
+    private string _radarrApiKey = string.Empty;
+    private bool _isConfigured;
 
-    public static RadarrSettings Create() => new() { BaseUrl = string.Empty, ApiKey = string.Empty };
+    public static RadarrSettings Create() =>
+        new()
+        {
+            IsConfigured = false,
+            RadarrBaseUrl = string.Empty,
+            RadarrApiKey = string.Empty,
+        };
 
-    public required string BaseUrl
+    /// <inheritdoc/>
+    public required bool IsConfigured
     {
-        get => _baseUrl;
-        set => SetProperty(ref _baseUrl, value);
+        get => _isConfigured;
+        set => SetProperty(ref _isConfigured, value);
     }
 
-    public required string ApiKey
+    /// <inheritdoc/>
+    public required string RadarrBaseUrl
     {
-        get => _apiKey;
-        set => SetProperty(ref _apiKey, value);
+        get => _radarrBaseUrl;
+        set => SetProperty(ref _radarrBaseUrl, value);
+    }
+
+    /// <inheritdoc/>
+    public required string RadarrApiKey
+    {
+        get => _radarrApiKey;
+        set => SetProperty(ref _radarrApiKey, value);
     }
 }

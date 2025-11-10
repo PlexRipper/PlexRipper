7309d2bafff6bae4ac2d918e6f07cc2d7b375ff9
JasonLandbridge <jasonlandbridge@protonmail.com>
2025-11-10 19:52:28 +0100

    feat(Web-UI): Links in help text now open a new tab

--- src/AppHost/ClientApp/src/components/Dialogs/HelpDialog.vue
+++ src/AppHost/ClientApp/src/components/Dialogs/HelpDialog.vue
@@ -1,37 +1,40 @@
 <template>
 	<QCardDialog
-		:name="DialogType.HelpInfoDialog"
 		:loading="false"
+		:name="DialogType.HelpInfoDialog"
 		:type="{} as IHelp"
 		width="500px"
-		@opened="onOpen"
-		@closed="onClose">
+		@closed="onClose"
+		@opened="onOpen">
 		<template #title>
 			{{ helpTitle ? helpTitle : missingHelpTitle }}
 		</template>
 		<!--	Help text	-->
 		<template #default>
 			<div class="i18n-formatting">
-				<VueMarkdown :source="helpText ? helpText : missingHelpText" />
+				<VueMarkdown
+					:plugins="[markdownItTargetBlank]"
+					:source="helpText ? helpText : missingHelpText" />
 			</div>
 		</template>
 		<!--	Close action	-->
 		<template #actions="{ close }">
 			<q-space />
 			<q-btn
-				flat
 				:label="t('general.commands.close')"
+				flat
 				@click="close" />
 		</template>
 	</QCardDialog>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
 import { get, set } from '@vueuse/core';
 import type { IHelp } from '@interfaces';
 import { DialogType } from '@enums';
 import { useI18n } from 'vue-i18n';
 import VueMarkdown from 'vue-markdown-render';
+import type MarkdownIt from 'markdown-it';
 
 const { t } = useI18n();
 const helpTitle = ref('');
@@ -48,5 +51,17 @@ function onOpen(event: IHelp): void {
 function onClose() {
 	set(helpTitle, '');
 	set(helpText, '');
+}
+
+// Markdown-it plugin to add target="_blank" and rel="noopener noreferrer" to all links
+function markdownItTargetBlank(md: MarkdownIt) {
+	const d = md.renderer.rules.link_open || ((t, i, o, _, s) => s.renderToken(t, i, o));
+	md.renderer.rules.link_open = (t, i, o, e, s) => {
+		const a = t[i].attrIndex('target');
+		if (a < 0) t[i].attrPush(['target', '_blank']);
+		else t[i].attrs[a][1] = '_blank';
+		t[i].attrPush(['rel', 'noopener noreferrer']);
+		return d(t, i, o, e, s);
+	};
 }
 </script>

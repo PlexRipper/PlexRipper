6d1e91ef620432f51f5af0937f2076f2d6a9d38a
JasonLandbridge <jasonlandbridge@protonmail.com>
2025-12-23 12:01:00 +0100

    refactor(WebAPI): Implement command to reset failed library sync jobs

--- src/Application/PlexServers/ServerOnlineStatusChanged/ServerOnlineStatusChangedNotification.cs
+++ src/Application/PlexServers/ServerOnlineStatusChanged/ServerOnlineStatusChangedNotification.cs
@@ -1,5 +1,6 @@
 using FastEndpoints;
 using Reaparr.Application.Contracts;
+using Reaparr.BackgroundJobs.Contracts;
 using Reaparr.Data.Contracts;
 
 namespace Reaparr.Application;
@@ -22,12 +23,19 @@ public class ServerOnlineStatusChangedHandler : IEventHandler<ServerOnlineStatus
     private readonly ILogger _log;
     private readonly IReaparrDbContext _dbContext;
     private readonly IDownloadQueue _downloadQueue;
+    private readonly ICommandExecutor _commandExecutor;
 
-    public ServerOnlineStatusChangedHandler(ILogger log, IReaparrDbContext dbContext, IDownloadQueue downloadQueue)
+    public ServerOnlineStatusChangedHandler(
+        ILogger log,
+        IReaparrDbContext dbContext,
+        IDownloadQueue downloadQueue,
+        ICommandExecutor commandExecutor
+    )
     {
         _log = log.ForContext<ServerOnlineStatusChangedHandler>();
         _dbContext = dbContext;
         _downloadQueue = downloadQueue;
+        _commandExecutor = commandExecutor;
     }
 
     public async Task HandleAsync(
@@ -43,10 +51,15 @@ public class ServerOnlineStatusChangedHandler : IEventHandler<ServerOnlineStatus
             );
             _log.Here()
                 .Information(
-                    "Server {PlexServerName} came online, checking DownloadQueue to resume downloads",
+                    "Server {PlexServerName} came online, checking DownloadQueue and LibrarySyncQueue to resume",
                     plexServerName
                 );
+
             await _downloadQueue.CheckDownloadQueue([notification.PlexServerId]);
+            await _commandExecutor.Send(
+                new ResetFailedLibrarySyncJobsCommand(notification.PlexServerId),
+                cancellationToken
+            );
         }
     }
 }
---
+++ src/BackgroundJobs.Contracts/ResetFailedLibrarySyncJobsCommand.cs
@@ -0,0 +1,8 @@
+using FastEndpoints;
+using FluentResults;
+
+namespace Reaparr.BackgroundJobs.Contracts;
+
+public record ResetFailedLibrarySyncJobsCommand(int PlexServerId) : ICommand<Result>;
+
+

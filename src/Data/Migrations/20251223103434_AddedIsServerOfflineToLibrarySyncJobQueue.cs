446944b8482dfb5dfde6214523928d5a8b95bb15
JasonLandbridge <jasonlandbridge@protonmail.com>
2025-12-23 12:17:41 +0100

    style(WebAPI): Formatting

--- src/Data/Migrations/20251223103434_AddedIsServerOfflineToLibrarySyncJobQueue.cs
+++ src/Data/Migrations/20251223103434_AddedIsServerOfflineToLibrarySyncJobQueue.cs
@@ -10,41 +10,45 @@ namespace Reaparr.Data.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AlterColumn<string>(
-                name: "ErrorMessage",
-                table: "BackgroundJobLibrarySyncJobQueues",
-                type: "TEXT",
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "TEXT",
-                oldNullable: true)
+            migrationBuilder
+                .AlterColumn<string>(
+                    name: "ErrorMessage",
+                    table: "BackgroundJobLibrarySyncJobQueues",
+                    type: "TEXT",
+                    nullable: true,
+                    oldClrType: typeof(string),
+                    oldType: "TEXT",
+                    oldNullable: true
+                )
                 .Annotation("Relational:ColumnOrder", 7)
                 .OldAnnotation("Relational:ColumnOrder", 6);
 
-            migrationBuilder.AddColumn<bool>(
-                name: "IsServerOffline",
-                table: "BackgroundJobLibrarySyncJobQueues",
-                type: "INTEGER",
-                nullable: false,
-                defaultValue: false)
+            migrationBuilder
+                .AddColumn<bool>(
+                    name: "IsServerOffline",
+                    table: "BackgroundJobLibrarySyncJobQueues",
+                    type: "INTEGER",
+                    nullable: false,
+                    defaultValue: false
+                )
                 .Annotation("Relational:ColumnOrder", 6);
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropColumn(
-                name: "IsServerOffline",
-                table: "BackgroundJobLibrarySyncJobQueues");
+            migrationBuilder.DropColumn(name: "IsServerOffline", table: "BackgroundJobLibrarySyncJobQueues");
 
-            migrationBuilder.AlterColumn<string>(
-                name: "ErrorMessage",
-                table: "BackgroundJobLibrarySyncJobQueues",
-                type: "TEXT",
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "TEXT",
-                oldNullable: true)
+            migrationBuilder
+                .AlterColumn<string>(
+                    name: "ErrorMessage",
+                    table: "BackgroundJobLibrarySyncJobQueues",
+                    type: "TEXT",
+                    nullable: true,
+                    oldClrType: typeof(string),
+                    oldType: "TEXT",
+                    oldNullable: true
+                )
                 .Annotation("Relational:ColumnOrder", 6)
                 .OldAnnotation("Relational:ColumnOrder", 7);
         }

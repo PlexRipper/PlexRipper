56d531c5d2f02fbb5c2bf62972e00f874727f2c7
JasonLandbridge <jasonlandbridge@protonmail.com>
2025-12-24 15:00:45 +0100

    refactor(WebAPI): Prevent non movie or tv-shows from syncing

--- src/BackgroundJobs/LibrarySync/Commands/QueueLibrarySyncJob/QueueLibrarySyncJobCommandHandler.cs
+++ src/BackgroundJobs/LibrarySync/Commands/QueueLibrarySyncJob/QueueLibrarySyncJobCommandHandler.cs
@@ -30,8 +30,10 @@ public class QueueLibrarySyncJobCommandHandler : ICommandHandler<QueueLibrarySyn
 
     public async Task<Result> ExecuteAsync(QueueLibrarySyncJobCommand command, CancellationToken cancellationToken)
     {
+        // TODO Remove extra where clause once we support other library types
         var libraries = await _dbContext
             .PlexLibraries.Where(x => command.PlexLibraryIds.Contains(x.Id))
+            .Where(x => x.Type == PlexMediaType.Movie || x.Type == PlexMediaType.TvShow)
             .Select(x => new
             {
                 x.Id,

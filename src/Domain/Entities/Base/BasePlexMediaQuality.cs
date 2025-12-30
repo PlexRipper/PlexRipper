7f6438d51796cfcaf97af3e38e5afe650e4142cd
JasonLandbridge <jasonlandbridge@protonmail.com>
2025-12-30 15:29:05 +0100

    refactor(WebAPI): Adjust column order for media data properties

--- src/Domain/Entities/Base/BasePlexMediaQuality.cs
+++ src/Domain/Entities/Base/BasePlexMediaQuality.cs
@@ -2,7 +2,7 @@ namespace Reaparr.Domain;
 
 public abstract class BasePlexMediaQuality : BaseEntity
 {
-    [Column(Order = 1)]
+    [Column(Order = 3)]
     public required VideoQuality Quality { get; init; }
 
     [NotMapped]

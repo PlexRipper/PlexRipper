06d1820a06d1baca3a4b8e4aff7e5779a67920db
JasonLandbridge <jasonlandbridge@protonmail.com>
2026-01-10 17:34:52 +0100

    fix(WebAPI): Avoid failing when cleanup directory has files

--- tests/UnitTests/Application.UnitTests/PlexDownloads/MoveDownloadFile/Jobs/CleanUpDownloadTaskFoldersCommand.UnitTests.cs
+++ tests/UnitTests/Application.UnitTests/PlexDownloads/MoveDownloadFile/Jobs/CleanUpDownloadTaskFoldersCommand.UnitTests.cs
@@ -3,9 +3,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Reaparr.Application.UnitTests;
 
-public class CreateDirectoryFromFilePathUnitTests : BaseUnitTest<CleanUpDownloadTaskFoldersHandler>
+public class CleanUpDownloadTaskFoldersCommandUnitTests : BaseUnitTest<CleanUpDownloadTaskFoldersHandler>
 {
-    public CreateDirectoryFromFilePathUnitTests(ITestOutputHelper output)
+    public CleanUpDownloadTaskFoldersCommandUnitTests(ITestOutputHelper output)
         : base(output) { }
 
     [Fact]
@@ -85,10 +85,20 @@ public class CreateDirectoryFromFilePathUnitTests : BaseUnitTest<CleanUpDownload
 
         Mock.Mock<IPath>()
             .Setup(x => x.GetDirectoryName(It.IsAny<string>()))
-            .Returns("/mnt/DATA/ReaparrCache/Downloads/TvShows/Reno 911!/Season 1/");
+            .Returns("/mnt/DATA/ReaparrCache/Downloads/TvShows/Reno 911!/Season 1/")
+            .Verifiable(Times.Exactly(2));
 
-        Mock.Mock<IDirectory>().Setup(x => x.GetFiles(It.IsAny<string>())).Returns([]);
-        Mock.Mock<IDirectory>().Setup(x => x.Delete(It.IsAny<string>()));
+        Mock.Mock<IPath>()
+            .Setup(x => x.GetFileName(It.IsAny<string>()))
+            .Returns("Reno 911! - S01E01 - How We Do It in Reno (Pilot) WEBDL-1080p.part1.mkv")
+            .Verifiable(Times.Never);
+
+        Mock.Mock<IDirectory>().Setup(x => x.Exists(It.IsAny<string>())).Returns(true).Verifiable(Times.Exactly(2));
+        Mock.Mock<IDirectory>()
+            .Setup(x => x.GetFileSystemEntries(It.IsAny<string>()))
+            .Returns([])
+            .Verifiable(Times.Exactly(2));
+        Mock.Mock<IDirectory>().Setup(x => x.Delete(It.IsAny<string>())).Verifiable(Times.Exactly(2));
 
         // Act
         var request = new CleanUpDownloadTaskFoldersCommand(downloadTask.ToKey());
@@ -97,6 +107,80 @@ public class CreateDirectoryFromFilePathUnitTests : BaseUnitTest<CleanUpDownload
         // Assert
         result.ShouldNotBeNull();
         result.IsSuccess.ShouldBeTrue();
+        Mock.Mock<IPath>().Verify();
+        Mock.Mock<IDirectory>().Verify();
+    }
+
+    [Fact]
+    public async Task ShouldReturnSuccessResult_WhenDirectoryContainsEntries()
+    {
+        // Arrange
+        await SetupDatabase(
+            25,
+            config =>
+            {
+                config.TvShowDownloadTasksCount = 1;
+                config.TvShowSeasonDownloadTasksCount = 1;
+                config.TvShowEpisodeDownloadTasksCount = 1;
+                config.DownloadWorkerTasks = 4;
+            }
+        );
+
+        var dbContext = IDbContext;
+        var downloadTask = await dbContext
+            .DownloadTaskTvShowEpisodeFile.Include(x => x.DownloadWorkerTasks)
+            .FirstOrDefaultAsync(CancellationToken);
+        downloadTask.ShouldNotBeNull();
+
+        var downloadWorkerTaskIds = downloadTask.DownloadWorkerTasks.Select(x => x.Id).ToList();
+        await dbContext
+            .DownloadWorkerTasks.Where(x => downloadWorkerTaskIds.Contains(x.Id))
+            .ExecuteUpdateAsync(
+                p =>
+                    p.SetProperty(
+                            x => x.DownloadDirectory,
+                            "/mnt/DATA/ReaparrCache/Downloads/TvShows/Reno 911!/Season 1"
+                        )
+                        .SetProperty(
+                            x => x.FileName,
+                            "Reno 911! - S01E01 - How We Do It in Reno (Pilot) WEBDL-1080p.part1.mkv"
+                        ),
+                CancellationToken
+            );
+
+        Mock.Mock<IPath>()
+            .Setup(x => x.GetDirectoryName(It.IsAny<string>()))
+            .Returns("/mnt/DATA/ReaparrCache/Downloads/TvShows/Reno 911!/Season 1/")
+            .Verifiable(Times.Exactly(2));
+
+        Mock.Mock<IPath>()
+            .Setup(x => x.GetFileName(It.IsAny<string>()))
+            .Returns("file.mkv")
+            .Verifiable(Times.Exactly(4));
+
+        Mock.Mock<IDirectory>().Setup(x => x.Exists(It.IsAny<string>())).Returns(true).Verifiable(Times.Exactly(2));
+
+        Mock.Mock<IDirectory>()
+            .Setup(x => x.GetFileSystemEntries(It.IsAny<string>()))
+            .Returns(
+                [
+                    "/mnt/DATA/ReaparrCache/Downloads/TvShows/Reno 911!/Season 1/Reno 911! - S01E01.mkv",
+                    "/mnt/DATA/ReaparrCache/Downloads/TvShows/Reno 911!/Season 1/Reno 911! - S01E02.mkv",
+                ]
+            )
+            .Verifiable(Times.Exactly(2));
+
+        Mock.Mock<IDirectory>().Setup(x => x.Delete(It.IsAny<string>())).Verifiable(Times.Never);
+
+        // Act
+        var request = new CleanUpDownloadTaskFoldersCommand(downloadTask.ToKey());
+        var result = await Sut.ExecuteAsync(request, CancellationToken);
+
+        // Assert
+        result.ShouldNotBeNull();
+        result.IsSuccess.ShouldBeTrue();
+        Mock.Mock<IPath>().Verify();
+        Mock.Mock<IDirectory>().Verify();
     }
 
     [Fact]
@@ -142,8 +226,10 @@ public class CreateDirectoryFromFilePathUnitTests : BaseUnitTest<CleanUpDownload
             .Returns("/mnt/DATA/ReaparrCache/Downloads/TvShows/Reno 911!/Season 1/")
             .Verifiable(Times.Once);
 
+        Mock.Mock<IDirectory>().Setup(x => x.Exists(It.IsAny<string>())).Returns(true).Verifiable(Times.Once);
+
         Mock.Mock<IDirectory>()
-            .Setup(x => x.GetFiles(It.IsAny<string>()))
+            .Setup(x => x.GetFileSystemEntries(It.IsAny<string>()))
             .Throws(new UnauthorizedAccessException())
             .Verifiable(Times.Once);
 
@@ -155,5 +241,7 @@ public class CreateDirectoryFromFilePathUnitTests : BaseUnitTest<CleanUpDownload
         result.ShouldNotBeNull();
         result.IsFailed.ShouldBeTrue();
         result.HasException<UnauthorizedAccessException>().ShouldBeTrue();
+        Mock.Mock<IPath>().Verify();
+        Mock.Mock<IDirectory>().Verify();
     }
 }

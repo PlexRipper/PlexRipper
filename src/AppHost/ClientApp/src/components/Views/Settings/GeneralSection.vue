2ea80595cf257e23b7d17b6cda6b819d2d3b01ff
JasonLandbridge <jasonlandbridge@protonmail.com>
2026-02-11 11:32:22 +0100

    feat(Web-UI): Moved the MaskServerNames and MaskLibraryNames settings to the UI => General page

--- src/AppHost/ClientApp/src/components/Views/Settings/GeneralSection.vue
+++ src/AppHost/ClientApp/src/components/Views/Settings/GeneralSection.vue
@@ -11,6 +11,24 @@
 					size="lg"
 					data-cy="toggle-animated-background" />
 			</HelpRow>
+			<!--	Mask Plex Server Names	-->
+			<HelpRow
+				:label="$t('help.settings.advanced.debug-section.mask-plex-servers.label')"
+				:title="$t('help.settings.advanced.debug-section.mask-plex-servers.title')"
+				:text="$t('help.settings.advanced.debug-section.mask-plex-servers.text')">
+				<q-toggle
+					v-model:model-value="settingsStore.debugSettings.maskServerNames"
+					size="lg" />
+			</HelpRow>
+			<!--	Mask Plex Library Names	-->
+			<HelpRow
+				:label="$t('help.settings.advanced.debug-section.mask-plex-libraries.label')"
+				:title="$t('help.settings.advanced.debug-section.mask-plex-libraries.title')"
+				:text="$t('help.settings.advanced.debug-section.mask-plex-libraries.text')">
+				<q-toggle
+					v-model:model-value="settingsStore.debugSettings.maskLibraryNames"
+					size="lg" />
+			</HelpRow>
 		</HelpGroup>
 	</QSection>
 </template>

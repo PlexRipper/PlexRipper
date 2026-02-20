b7b75cf163176ee413772ffa555b0ab074020990
JasonLandbridge <jasonlandbridge@protonmail.com>
2026-02-20 19:47:24 +0100

    refactor(Web-UI): Fixed translation key inside mediaOverviewStore

--- src/AppHost/ClientApp/src/store/mediaOverviewStore.ts
+++ src/AppHost/ClientApp/src/store/mediaOverviewStore.ts
@@ -84,7 +84,7 @@ export const useMediaOverviewStore = defineStore(StoreNames.MediaOverviewStore, 
 	};
 
 	const state = reactive<IMediaOverviewStoreState>(cloneDeep(defaultState));
-
+	const { t } = useI18n({ useScope: 'global' });
 	const settingsStore = useSettingsStore();
 	const libraryStore = useLibraryStore();
 
@@ -493,7 +493,6 @@ export const useMediaOverviewStore = defineStore(StoreNames.MediaOverviewStore, 
 			return !(state.sortedState.field === MediaSortField.Title && state.sortedState.sort === SortDirection.Asc);
 		}),
 		getSortOptions: computed((): ISortOption[] => {
-			const { t } = useI18n();
 			const options: ISortOption[] = [
 				{ field: MediaSortField.Title, label: t('general.sort.title'), direction: SortDirection.NoSort },
 				{ field: MediaSortField.Year, label: t('general.sort.year'), direction: SortDirection.NoSort },

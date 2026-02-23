ab53a06cc15551992719284ec4050874165b6679
JasonLandbridge <jasonlandbridge@protonmail.com>
2026-02-23 11:27:24 +0100

    fix(WebAPI): Fixed Reaparr not being able to set the DownloadClient in Sonarr and Radarr

--- src/Application/Integrations/Radarr/ConfigureRadarr/SetupRadarrDownloadClientCommand.cs
+++ src/Application/Integrations/Radarr/ConfigureRadarr/SetupRadarrDownloadClientCommand.cs
@@ -119,6 +119,7 @@ public class SetupRadarrDownloadClientCommandHandler
         var derivedUrlBase = $"{basePath}api/public/download-client/";
 
         var useSsl = string.Equals(reaparrBaseUri.Scheme, Uri.UriSchemeHttps, StringComparison.OrdinalIgnoreCase);
+        var port = reaparrBaseUri.Port == -1 ? (useSsl ? 443 : 80) : reaparrBaseUri.Port;
 
         return new RadarrDownloadContractDTO
         {
@@ -131,7 +132,7 @@ public class SetupRadarrDownloadClientCommandHandler
             Fields =
             [
                 new() { Name = "host", Value = reaparrBaseUri.Host },
-                new() { Name = "port", Value = reaparrBaseUri.Port },
+                new() { Name = "port", Value = port },
                 new() { Name = "useSsl", Value = useSsl },
                 new() { Name = "urlBase", Value = derivedUrlBase },
                 new() { Name = "username", Value = _integrationsSettings.DownloadClientUsername },

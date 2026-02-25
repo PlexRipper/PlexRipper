7930cad12d7360ca04eb192dfbee201d1c4bb116
JasonLandbridge <jasonlandbridge@protonmail.com>
2026-02-25 23:45:15 +0100

    feat(Web-UI): Added a toggle to enable the stream downloader

--- src/AppHost/ClientApp/src/components/Dialogs/ServerDialog/Tabs/ServerConfigTabContent.vue
+++ src/AppHost/ClientApp/src/components/Dialogs/ServerDialog/Tabs/ServerConfigTabContent.vue
@@ -7,6 +7,16 @@
 			:text="$t('help.server-dialog.server-config.download-speed-limit.text')">
 			<DownloadLimitInput :machine-identifier="plexServer.machineIdentifier" />
 		</HelpRow>
+		<HelpRow
+			disable-responsive
+			:label="$t('help.server-dialog.server-config.allow-stream-downloader.label')"
+			:title="$t('help.server-dialog.server-config.allow-stream-downloader.title')"
+			:text="$t('help.server-dialog.server-config.allow-stream-downloader.text')">
+			<QToggle
+				size="lg"
+				:model-value="allowStreamDownloader"
+				@update:model-value="onAllowStreamDownloaderChanged" />
+		</HelpRow>
 	</HelpGroup>
 
 	<QAlert
@@ -18,8 +28,21 @@
 
 <script setup lang="ts">
 import type { PlexServerDTO } from '@dto';
+import { useSettingsStore } from '@store';
 
-defineProps<{
+const props = defineProps<{
 	plexServer: PlexServerDTO | null;
 }>();
+
+const settingsStore = useSettingsStore();
+
+const allowStreamDownloader = computed(
+	() => settingsStore.getServerSettings(props.plexServer?.machineIdentifier)?.allowStreamDownloader ?? false,
+);
+
+function onAllowStreamDownloaderChanged(value: boolean) {
+	if (props.plexServer) {
+		settingsStore.updateAllowStreamDownloader(props.plexServer.machineIdentifier, value);
+	}
+}
 </script>

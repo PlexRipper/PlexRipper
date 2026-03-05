17184368df16ccc16c82409472cde0425e38bbd5
JasonLandbridge <jasonlandbridge@protonmail.com>
2026-03-05 13:48:31 +0100

    refactor(WebAPI): Cleanup add documentation

--- src/Application/PlexDownloads/Execute/DirectDownloadClient/DirectPlexDownloadClient.cs
+++ src/Application/PlexDownloads/Execute/DirectDownloadClient/DirectPlexDownloadClient.cs
@@ -197,7 +197,8 @@ public class DirectPlexDownloadClient : IPlexDownloadClient
                             DataTotal = args.TotalBytesToReceive,
                             Percentage = Convert.ToDecimal(args.ProgressPercentage),
                             DataReceived = args.ReceivedBytesSize,
-                            DownloadSpeed = args.ProgressPercentage < 1 ? Convert.ToInt64(args.BytesPerSecondSpeed) : 0,
+                            DownloadSpeed =
+                                args.ProgressPercentage < 100 ? Convert.ToInt64(args.BytesPerSecondSpeed) : 0,
                         };
 
                         await dbContext.UpdateDownloadProgress(

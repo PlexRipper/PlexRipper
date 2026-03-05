ffa36c089b192d9af7dd3a3d50ce4f857cda4d59
JasonLandbridge <jasonlandbridge@protonmail.com>
2026-03-05 17:58:08 +0100

    fix(WebAPI): Avoid destructuring unsafe API request payloads

--- src/Logging/_Shared/Extensions/LogExtensions.Debug.cs
+++ src/Logging/_Shared/Extensions/LogExtensions.Debug.cs
@@ -12,6 +12,18 @@ public static partial class LogExtensions
     {
         if (request is not null)
         {
+            if (!ShouldDestructureRequest(request))
+            {
+                log.Verbose(
+                    "{Method}: {EndpointPath} with {RequestType} {Query}",
+                    context.Request.Method,
+                    context.Request.GetDisplayUrl(),
+                    request.GetType().Name,
+                    context.Request.Query
+                );
+                return;
+            }
+
             log.Verbose(
                 "{Method}: {EndpointPath} with {@Request} {Query}",
                 context.Request.Method,
@@ -34,6 +46,17 @@ public static partial class LogExtensions
     {
         if (request is not null)
         {
+            if (!ShouldDestructureRequest(request))
+            {
+                log.Debug(
+                    "{Method}: {EndpointPath} with {RequestType}",
+                    context.Request.Method,
+                    context.Request.GetDisplayUrl(),
+                    request.GetType().Name
+                );
+                return;
+            }
+
             log.Debug(
                 "{Method}: {EndpointPath} with {@Request}",
                 context.Request.Method,
@@ -49,6 +72,87 @@ public static partial class LogExtensions
             context.Request.GetDisplayUrl(),
             context.Request.Query
         );
+    }
+
+    /// <summary>
+    /// Why this exists:
+    /// Serilog destructuring can aggressively traverse object graphs. For request DTOs that contain
+    /// upload abstractions (IFormFile), streams, ASP.NET request/response objects, or dynamic proxies,
+    /// this can become extremely expensive and can appear as a hang in tests.
+    /// In those cases we log the request type only instead of destructuring the entire payload.
+    /// </summary>
+    /// <param name="request"></param>
+    /// <returns></returns>
+    private static bool ShouldDestructureRequest(object request)
+    {
+        var requestType = request.GetType();
+
+        // Moq/Castle proxy types frequently include deep/cyclic members and are unsafe to destructure.
+        if (string.Equals(requestType.Assembly.GetName().Name, "DynamicProxyGenAssembly2", StringComparison.Ordinal))
+            return false;
+
+        if (requestType.Namespace?.StartsWith("Castle.Proxies", StringComparison.Ordinal) == true)
+            return false;
+
+        if (IsPotentiallyUnsafeForDestructuring(requestType, new HashSet<Type>()))
+            return false;
+
+        return true;
+    }
+
+    private static bool IsPotentiallyUnsafeForDestructuring(Type type, HashSet<Type> visitedTypes)
+    {
+        if (!visitedTypes.Add(type))
+            return false;
+
+        if (typeof(IFormFile).IsAssignableFrom(type))
+            return true;
+
+        if (typeof(IFormFileCollection).IsAssignableFrom(type))
+            return true;
+
+        if (typeof(IFormCollection).IsAssignableFrom(type))
+            return true;
+
+        if (typeof(Stream).IsAssignableFrom(type))
+            return true;
+
+        if (typeof(HttpContext).IsAssignableFrom(type))
+            return true;
+
+        if (typeof(HttpRequest).IsAssignableFrom(type))
+            return true;
+
+        if (typeof(HttpResponse).IsAssignableFrom(type))
+            return true;
+
+        if (type == typeof(byte[]))
+            return true;
+
+        if (type.IsArray)
+            return IsPotentiallyUnsafeForDestructuring(type.GetElementType()!, visitedTypes);
+
+        if (type.IsGenericType)
+        {
+            var genericArguments = type.GetGenericArguments();
+            foreach (var genericArgument in genericArguments)
+            {
+                if (IsPotentiallyUnsafeForDestructuring(genericArgument, visitedTypes))
+                    return true;
+            }
+        }
+
+        var properties = type.GetProperties();
+
+        foreach (var property in properties)
+        {
+            var propertyType = property.PropertyType;
+
+            if (IsPotentiallyUnsafeForDestructuring(propertyType, visitedTypes))
+                return true;
+        }
+
+        return false;
     }
 
     [MessageTemplateFormatMethod("messageTemplate")]

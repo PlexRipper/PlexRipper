b6d261a8d664c0e70c50e27453a2873569b34d76
JasonLandbridge <jasonlandbridge@protonmail.com>
2026-03-06 19:25:52 +0100

    test(WebAPI): Added ShouldUseUniqueSandboxAndCleanupWhenContainerIsDisposed integration test

---
+++ tests/IntegrationTests/IntegrationTests/FileSystem/IntegrationTestFileSystemIsolation.IntegrationTests.cs
@@ -0,0 +1,31 @@
+namespace Reaparr.IntegrationTests;
+
+public class IntegrationTestFileSystemIsolationIntegrationTests : BaseIntegrationTests
+{
+    public IntegrationTestFileSystemIsolationIntegrationTests(ITestOutputHelper output)
+        : base(output) { }
+
+    [Fact]
+    public async Task ShouldUseUniqueSandboxAndCleanupWhenContainerIsDisposed()
+    {
+        string firstSandboxPath;
+        string secondSandboxPath;
+
+        using (var firstContainer = await CreateContainer(new Seed(101)))
+        {
+            firstSandboxPath = firstContainer.TestFileSystemRootPath;
+            Directory.Exists(firstSandboxPath).ShouldBeTrue();
+
+            using (var secondContainer = await CreateContainer(new Seed(202)))
+            {
+                secondSandboxPath = secondContainer.TestFileSystemRootPath;
+                secondSandboxPath.ShouldNotBe(firstSandboxPath);
+                Directory.Exists(secondSandboxPath).ShouldBeTrue();
+            }
+
+            Directory.Exists(secondSandboxPath).ShouldBeFalse();
+        }
+
+        Directory.Exists(firstSandboxPath).ShouldBeFalse();
+    }
+}

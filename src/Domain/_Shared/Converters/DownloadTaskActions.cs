0be0b10264bc6c5fa4c615fb710a5372c265c84b
JasonLandbridge <jasonlandbridge@protonmail.com>
2026-03-17 11:04:07 +0100

    refactor(WebAPI): Reorder download status enumeration

--- src/Domain/_Shared/Converters/DownloadTaskActions.cs
+++ src/Domain/_Shared/Converters/DownloadTaskActions.cs
@@ -19,11 +19,11 @@ public static class DownloadTaskActions
         DownloadStatus.MovePaused,
         DownloadStatus.Stopped,
         DownloadStatus.Restarting,
-        DownloadStatus.Queued,
         DownloadStatus.Moving,
         DownloadStatus.MoveFinished,
         DownloadStatus.DownloadFinished,
         DownloadStatus.Deleted,
+        DownloadStatus.Queued,
         DownloadStatus.Completed,
     ];
 

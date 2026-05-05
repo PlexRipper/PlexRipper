0431d81f124389a6762955d41e7343c76c85658e
JasonLandbridge <jasonlandbridge@protonmail.com>
2026-05-05 19:02:40 +0200

    build(WebAPI): Add Plex server owned override EF migration

---
+++ src/Data/Migrations/20260505165836_AddPlexServerOwnedOverride.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Reaparr.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPlexServerOwnedOverride : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Home",
+                table: "PlexServers");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "OwnedOverride",
+                table: "PlexServers",
+                type: "INTEGER",
+                nullable: true)
+                .Annotation("Relational:ColumnOrder", 17);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OwnedOverride",
+                table: "PlexServers");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Home",
+                table: "PlexServers",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false)
+                .Annotation("Relational:ColumnOrder", 17);
+        }
+    }
+}

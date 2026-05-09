ba3ba291e6db69e4ddc391dd930e0a2f9b213ed1
JasonLandbridge <jasonlandbridge@protonmail.com>
2026-05-09 20:58:52 +0200

    refactor(WebAPI): Add Id to PlexQualityDTO

--- src/Application.Contracts/_Shared/Mappings/PlexMedia/DTO/PlexQualityDTO.cs
+++ src/Application.Contracts/_Shared/Mappings/PlexMedia/DTO/PlexQualityDTO.cs
@@ -2,6 +2,23 @@ namespace Reaparr.Application.Contracts;
 
 public record PlexQualityDTO
 {
+    public int Id => Quality switch
+    {
+        VideoQuality.None => 0,
+        VideoQuality.Unknown => -1,
+        VideoQuality.SubSD_144p => 1,
+        VideoQuality.SubSD_CIF => 2,
+        VideoQuality.nHD => 3,
+        VideoQuality.SD => 4,
+        VideoQuality.DVD => 5,
+        VideoQuality.HD => 6,
+        VideoQuality.FullHD => 7,
+        VideoQuality.QHD => 8,
+        VideoQuality.UHD_4K => 9,
+        VideoQuality.UHD_8K => 10,
+        _ => throw new ArgumentOutOfRangeException()
+    };
+
     public required string Name { get; init; }
     public required int Count { get; init; }
 

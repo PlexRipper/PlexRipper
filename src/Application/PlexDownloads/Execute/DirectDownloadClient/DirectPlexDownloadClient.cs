e9264fe24f2dee878ca0fb3027403a1d13906eb6
JasonLandbridge <jasonlandbridge@protonmail.com>
2026-07-11 19:14:29 +0200

    fix(WebAPI): Cap download memory buffer at 50MB to prevent OOM on large files. Should prevent memory issues

--- src/Application/PlexDownloads/Execute/DirectDownloadClient/DirectPlexDownloadClient.cs
+++ src/Application/PlexDownloads/Execute/DirectDownloadClient/DirectPlexDownloadClient.cs
@@ -62,6 +62,7 @@ public class DirectPlexDownloadClient : IPlexDownloadClient
         _configuration.EnableAutoResumeDownload = false;
         _configuration.DownloadFileExtension = FilePathExtensions.TempDownloadFileSuffix;
         _configuration.CheckDiskSizeBeforeDownload = false;
+        _configuration.MaximumMemoryBufferBytes = 50 * 1024 * 1024; // 50MB memory buffer cap
 
         _downloader = downloadServiceFactory(_configuration);
     }

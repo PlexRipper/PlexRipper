74ec3fb698b1c2816067f9c22fb78e1d7a6c4940
JasonLandbridge <jasonlandbridge@protonmail.com>
2026-07-19 22:35:35 +0200

    refactor(Web-UI): Fixed dynamic translation

--- src/AppHost/ClientApp/src/store/downloadStore.ts
+++ src/AppHost/ClientApp/src/store/downloadStore.ts
@@ -21,7 +21,6 @@ import {
 import { StoreNames, type IDownloadsSelection, type IPTreeTableSelectionKeys, type ISetupResult } from '@interfaces';
 import { downloadApi } from '@api';
 import { useServerStore, useSignalrStore } from '@store';
-import { useI18n } from '#build/imports';
 
 interface IDownloadsStoreState {
 	serverDownloads: ServerDownloadProgressDTO[];
@@ -189,20 +188,86 @@ export const useDownloadStore = defineStore(StoreNames.DownloadStore, () => {
 						let message = t('general.download-notification.unknown');
 						if (movies > 0 && tvShows > 0) {
 							const variant = (movies > 1 ? 8 : 0) | (tvShows > 1 ? 4 : 0) | (seasons > 1 ? 2 : 0) | (episodes > 1 ? 1 : 0);
-
-							message = t(`general.download-notification.movie-and-tv-show-v${variant}`, {
-								movies, shows: tvShows, seasons, episodes,
-							});
-						}
-						if (movies > 0) {
+							switch (variant) {
+								case 0:
+									message = t('general.download-notification.movie-and-tv-show-v0', { movies, shows: tvShows, seasons, episodes });
+									break;
+								case 1:
+									message = t('general.download-notification.movie-and-tv-show-v1', { movies, shows: tvShows, seasons, episodes });
+									break;
+								case 2:
+									message = t('general.download-notification.movie-and-tv-show-v2', { movies, shows: tvShows, seasons, episodes });
+									break;
+								case 3:
+									message = t('general.download-notification.movie-and-tv-show-v3', { movies, shows: tvShows, seasons, episodes });
+									break;
+								case 4:
+									message = t('general.download-notification.movie-and-tv-show-v4', { movies, shows: tvShows, seasons, episodes });
+									break;
+								case 5:
+									message = t('general.download-notification.movie-and-tv-show-v5', { movies, shows: tvShows, seasons, episodes });
+									break;
+								case 6:
+									message = t('general.download-notification.movie-and-tv-show-v6', { movies, shows: tvShows, seasons, episodes });
+									break;
+								case 7:
+									message = t('general.download-notification.movie-and-tv-show-v7', { movies, shows: tvShows, seasons, episodes });
+									break;
+								case 8:
+									message = t('general.download-notification.movie-and-tv-show-v8', { movies, shows: tvShows, seasons, episodes });
+									break;
+								case 9:
+									message = t('general.download-notification.movie-and-tv-show-v9', { movies, shows: tvShows, seasons, episodes });
+									break;
+								case 10:
+									message = t('general.download-notification.movie-and-tv-show-v10', { movies, shows: tvShows, seasons, episodes });
+									break;
+								case 11:
+									message = t('general.download-notification.movie-and-tv-show-v11', { movies, shows: tvShows, seasons, episodes });
+									break;
+								case 12:
+									message = t('general.download-notification.movie-and-tv-show-v12', { movies, shows: tvShows, seasons, episodes });
+									break;
+								case 13:
+									message = t('general.download-notification.movie-and-tv-show-v13', { movies, shows: tvShows, seasons, episodes });
+									break;
+								case 14:
+									message = t('general.download-notification.movie-and-tv-show-v14', { movies, shows: tvShows, seasons, episodes });
+									break;
+								case 15:
+									message = t('general.download-notification.movie-and-tv-show-v15', { movies, shows: tvShows, seasons, episodes });
+									break;
+							}
+						} else if (movies > 0) {
 							message = t('general.download-notification.movie', { count: movies }, { plural: movies });
-						}
-						if (tvShows > 0) {
+						} else if (tvShows > 0) {
 							const variant = (tvShows > 1 ? 4 : 0) | (seasons > 1 ? 2 : 0) | (episodes > 1 ? 1 : 0);
-
-							message = t(`general.download-notification.tv-show-detail-v${variant}`, {
-								shows: tvShows, seasons, episodes,
-							});
+							switch (variant) {
+								case 0:
+									message = t('general.download-notification.tv-show-detail-v0', { shows: tvShows, seasons, episodes });
+									break;
+								case 1:
+									message = t('general.download-notification.tv-show-detail-v1', { shows: tvShows, seasons, episodes });
+									break;
+								case 2:
+									message = t('general.download-notification.tv-show-detail-v2', { shows: tvShows, seasons, episodes });
+									break;
+								case 3:
+									message = t('general.download-notification.tv-show-detail-v3', { shows: tvShows, seasons, episodes });
+									break;
+								case 4:
+									message = t('general.download-notification.tv-show-detail-v4', { shows: tvShows, seasons, episodes });
+									break;
+								case 5:
+									message = t('general.download-notification.tv-show-detail-v5', { shows: tvShows, seasons, episodes });
+									break;
+								case 6:
+									message = t('general.download-notification.tv-show-detail-v6', { shows: tvShows, seasons, episodes });
+									break;
+								case 7:
+									message = t('general.download-notification.tv-show-detail-v7', { shows: tvShows, seasons, episodes });
+									break;
+							}
 						}
 
 						Notify.create({

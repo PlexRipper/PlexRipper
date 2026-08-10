864b166cdf9a31aa9ed8b99475750c06fd450ea4
JasonLandbridge <jasonlandbridge@protonmail.com>
2026-08-10 11:59:20 +0200

    build(WebAPI): AddForceMediaRefreshToLibrarySyncQueue EF migration

---
+++ src/Data/Migrations/20260810095857_AddForceMediaRefreshToLibrarySyncQueue.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Reaparr.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddForceMediaRefreshToLibrarySyncQueue : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ForceMediaRefresh",
+                table: "BackgroundJobLibrarySyncJobQueues",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false)
+                .Annotation("Relational:ColumnOrder", 10);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ForceMediaRefresh",
+                table: "BackgroundJobLibrarySyncJobQueues");
+        }
+    }
+}

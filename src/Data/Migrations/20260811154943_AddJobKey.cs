1f8ac5cbf03445fb8c82be93621ff15df4e40f25
JasonLandbridge <jasonlandbridge@protonmail.com>
2026-08-11 17:52:55 +0200

    build(WebAPI): Add AddJobKey EF migration

---
+++ src/Data/Migrations/20260811154943_AddJobKey.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Reaparr.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddJobKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "JobType",
+                schema: "ticker",
+                table: "TimeTickers",
+                type: "TEXT",
+                unicode: false,
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "JobType",
+                schema: "ticker",
+                table: "CronTickers",
+                type: "TEXT",
+                unicode: false,
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TimeTickers_JobType",
+                schema: "ticker",
+                table: "TimeTickers",
+                column: "JobType");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CronTickers_JobType",
+                schema: "ticker",
+                table: "CronTickers",
+                column: "JobType");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_TimeTickers_JobType",
+                schema: "ticker",
+                table: "TimeTickers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CronTickers_JobType",
+                schema: "ticker",
+                table: "CronTickers");
+
+            migrationBuilder.DropColumn(
+                name: "JobType",
+                schema: "ticker",
+                table: "TimeTickers");
+
+            migrationBuilder.DropColumn(
+                name: "JobType",
+                schema: "ticker",
+                table: "CronTickers");
+        }
+    }
+}

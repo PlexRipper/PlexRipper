dad0159733322b7c5e852ad8d6ec4c9e7be52b00
JasonLandbridge <jasonlandbridge@protonmail.com>
2026-08-11 23:34:20 +0200

    refactor(WebAPI): Added TinkerQ dashboard

--- src/AppHost/Startup/Startup.Services.cs
+++ src/AppHost/Startup/Startup.Services.cs
@@ -14,6 +14,7 @@ using Reaparr.Application.Contracts;
 using Reaparr.Data.Contracts;
 using Reaparr.Identity.Contracts;
 using Serilog.Sinks.AspNetCore.App.SignalR.Extensions;
+using TickerQ.Dashboard.DependencyInjection;
 using TickerQ.DependencyInjection;
 using TickerQ.EntityFrameworkCore.Customizer;
 using TickerQ.EntityFrameworkCore.DependencyInjection;
@@ -213,10 +214,13 @@ public static partial class Startup
 
         // Removing all registered IHttpMessageHandlerBuilderFilter instances to disable built-in HttpClient logging
         services.RemoveAll<IHttpMessageHandlerBuilderFilter>();
-        
+
         // Register https://tickerq.net/
         services.AddTickerQ<JobTimeTicker, JobCronTicker>(opt =>
         {
+            if (env.IsDevelopment())
+                opt.AddDashboard();
+
             opt.AddOperationalStore(ef =>
             {
                 ef.UseApplicationDbContext<ReaparrDbContext>(ConfigurationType.IgnoreModelCustomizer);

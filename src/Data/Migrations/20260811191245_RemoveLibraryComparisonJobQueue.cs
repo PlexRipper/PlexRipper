38bd8b959f1cdd3d4abb943aefee4c360755f729
JasonLandbridge <jasonlandbridge@protonmail.com>
2026-08-11 21:13:20 +0200

    build(WebAPI): Add RemoveLibraryComparisonJobQueue Migration

---
+++ src/Data/Migrations/20260811191245_RemoveLibraryComparisonJobQueue.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Reaparr.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveLibraryComparisonJobQueue : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BackgroundJobLibraryComparisonJobQueues");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BackgroundJobLibraryComparisonJobQueues",
+                columns: table => new
+                {
+                    Priority = table.Column<int>(type: "INTEGER", nullable: false),
+                    Status = table.Column<string>(type: "TEXT", unicode: false, maxLength: 50, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    StartedAt = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    CompletedAt = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    Attempts = table.Column<int>(type: "INTEGER", nullable: false),
+                    ErrorMessage = table.Column<string>(type: "TEXT", nullable: true),
+                    RemotePlexLibraryId = table.Column<int>(type: "INTEGER", nullable: false),
+                    OwnedPlexLibraryId = table.Column<int>(type: "INTEGER", nullable: false),
+                    MediaType = table.Column<string>(type: "TEXT", unicode: false, maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BackgroundJobLibraryComparisonJobQueues", x => new { x.RemotePlexLibraryId, x.OwnedPlexLibraryId, x.MediaType });
+                    table.ForeignKey(
+                        name: "FK_BackgroundJobLibraryComparisonJobQueues_PlexLibraries_OwnedPlexLibraryId",
+                        column: x => x.OwnedPlexLibraryId,
+                        principalTable: "PlexLibraries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BackgroundJobLibraryComparisonJobQueues_PlexLibraries_RemotePlexLibraryId",
+                        column: x => x.RemotePlexLibraryId,
+                        principalTable: "PlexLibraries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BackgroundJobLibraryComparisonJobQueues_OwnedPlexLibraryId",
+                table: "BackgroundJobLibraryComparisonJobQueues",
+                column: "OwnedPlexLibraryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BackgroundJobLibraryComparisonJobQueues_Status_Priority_CreatedAt",
+                table: "BackgroundJobLibraryComparisonJobQueues",
+                columns: new[] { "Status", "Priority", "CreatedAt" });
+        }
+    }
+}

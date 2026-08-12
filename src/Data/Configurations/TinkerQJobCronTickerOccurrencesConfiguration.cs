9db95b611bfb189962620acc426cd6929cedcb42
JasonLandbridge <jasonlandbridge@protonmail.com>
2026-08-12 23:38:27 +0200

    refactor(WebAPI): Moved TinkerQJobCronTickerOccurrencesConfiguration

---
+++ src/Data/Configurations/TinkerQJobCronTickerOccurrencesConfiguration.cs
@@ -0,0 +1,12 @@
+using TickerQ.Utilities.Entities;
+
+namespace Reaparr.Data.Configurations;
+
+public class
+    TinkerQJobCronTickerOccurrencesConfiguration : IEntityTypeConfiguration<CronTickerOccurrenceEntity<JobCronTicker>>
+{
+    public void Configure(EntityTypeBuilder<CronTickerOccurrenceEntity<JobCronTicker>> builder)
+    {
+        builder.ToTable("TinkerQ_JobCronTickerOccurrences");
+    }
+}

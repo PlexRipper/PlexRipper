e410ec4186e9143ff87581234bdc7bf46acd822d
JasonLandbridge <jasonlandbridge@protonmail.com>
2026-08-29 14:35:30 +0200

    build(WebAPI): EF Migration remove DownloadClientSessions table from database

--- src/Identity/Migrations/20260829122748_DropDownloadClientSessions.Designer.cs
+++ src/Identity/Migrations/20260829122748_DropDownloadClientSessions.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Reaparr.Identity;
 
@@ -10,9 +11,11 @@ using Reaparr.Identity;
 namespace Reaparr.Identity.Migrations
 {
     [DbContext(typeof(AuthDbContext))]
-    partial class AuthDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260829122748_DropDownloadClientSessions")]
+    partial class DropDownloadClientSessions
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.10");

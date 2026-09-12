32608a980a71cbec010f288a5cf2d02a90beede4
JasonLandbridge <jasonlandbridge@protonmail.com>
2026-09-12 16:58:34 +0200

    build(WebAPI): Add Type property to PlexGenre and configure its database mapping

--- src/Data/Configurations/PlexGenreConfiguration.cs
+++ src/Data/Configurations/PlexGenreConfiguration.cs
@@ -5,5 +5,6 @@ public class PlexGenreConfiguration : IEntityTypeConfiguration<PlexGenre>
     public void Configure(EntityTypeBuilder<PlexGenre> builder)
     {
         builder.HasIndex(x => x.Key).IsUnique();
+        builder.Property(x => x.Type).HasConversion<string>().HasMaxLength(20).IsUnicode(false);
     }
 }

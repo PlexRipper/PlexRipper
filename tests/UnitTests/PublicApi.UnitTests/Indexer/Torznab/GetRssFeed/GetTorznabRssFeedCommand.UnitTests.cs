47174e8f2af8af20a3a8e3333a7b4d8f6534f057
JasonLandbridge <jasonlandbridge@protonmail.com>
2026-09-12 13:20:45 +0200

    test(WebAPI): Add unit tests for GetTorznabRssFeed command

--- tests/UnitTests/PublicApi.UnitTests/Indexer/Torznab/GetRssFeed/GetTorznabRssFeedCommand.UnitTests.cs
+++ tests/UnitTests/PublicApi.UnitTests/Indexer/Torznab/GetRssFeed/GetTorznabRssFeedCommand.UnitTests.cs
@@ -205,4 +205,177 @@ public class GetTorznabRssFeedCommandUnitTests : BaseCommandUnitTest<GetTorznabR
         result.IsValid.ShouldBeTrue();
         result.Errors.ShouldBeEmpty();
     }
+
+    [Test]
+    public async Task ShouldUseKnownCategory_WhenKnownAndUnknownCategoriesAreRequested()
+    {
+        // Arrange
+        await SetupDatabase(7635, config =>
+        {
+            config.PlexServerCount = 1;
+            config.PlexAccountCount = 1;
+            config.PlexMovieLibraryCount = 1;
+            config.MovieCount = 2;
+            config.RadarrIntegrationCount = 1;
+        });
+        var integration = (await IDbContext.RadarrIntegrations.SingleAsync(CancellationToken)).Id.ToRadarrIdentity();
+        var category = (await IDbContext.PlexMovieData.FirstAsync(CancellationToken)).ToTorznabMovieCategory();
+        var command = new GetTorznabRssFeedCommand
+        {
+            Integration = integration,
+            Categories = [category, 9999],
+            IncludeMovies = true,
+            IncludeEpisodes = false,
+            Limit = 50,
+            Offset = 0,
+            TorznabApiKey = "rss-key",
+        };
+
+        // Act
+        var result = await TestHandlerExecuteAsync<TorznabMediaSearchResponseDTO>(command);
+
+        // Assert
+        result.IsSuccess.ShouldBeTrue();
+        result.Value.Channel.Items.ShouldNotBeEmpty();
+        result.Value.Channel.Items.All(x => x.Attributes.Any(a => a.Name == "category" && a.Value == category.ToString())).ShouldBeTrue();
+    }
+
+    [Test]
+    public async Task ShouldNotDuplicateItems_WhenParentAndChildCategoriesAreRequested()
+    {
+        // Arrange
+        await SetupDatabase(7636, config =>
+        {
+            config.PlexServerCount = 1;
+            config.PlexAccountCount = 1;
+            config.PlexMovieLibraryCount = 1;
+            config.MovieCount = 3;
+            config.RadarrIntegrationCount = 1;
+        });
+        var integration = (await IDbContext.RadarrIntegrations.SingleAsync(CancellationToken)).Id.ToRadarrIdentity();
+        var totalParts = await IDbContext.PlexMovieData.CountAsync(CancellationToken);
+        var command = new GetTorznabRssFeedCommand
+        {
+            Integration = integration,
+            Categories = [(int)TorznabCategoryId.Movies, (int)TorznabCategoryId.Movies_HD],
+            IncludeMovies = true,
+            IncludeEpisodes = false,
+            Limit = 50,
+            Offset = 0,
+            TorznabApiKey = "rss-key",
+        };
+
+        // Act
+        var result = await TestHandlerExecuteAsync<TorznabMediaSearchResponseDTO>(command);
+
+        // Assert
+        result.IsSuccess.ShouldBeTrue();
+        result.Value.Channel.Response.Total.ShouldBe(totalParts);
+        result.Value.Channel.Items.Select(x => x.Guid.Value).Distinct().Count().ShouldBe(totalParts);
+    }
+
+    [Test]
+    public async Task ShouldReturnEmpty_WhenMovieSearchUsesOnlyTvCategories()
+    {
+        // Arrange
+        await SetupDatabase(7637, config =>
+        {
+            config.PlexServerCount = 1;
+            config.PlexAccountCount = 1;
+            config.PlexMovieLibraryCount = 1;
+            config.MovieCount = 2;
+            config.RadarrIntegrationCount = 1;
+        });
+        var integration = (await IDbContext.RadarrIntegrations.SingleAsync(CancellationToken)).Id.ToRadarrIdentity();
+        var command = new GetTorznabRssFeedCommand
+        {
+            Integration = integration,
+            Categories = [(int)TorznabCategoryId.TV],
+            IncludeMovies = true,
+            IncludeEpisodes = false,
+            Limit = 50,
+            Offset = 0,
+            TorznabApiKey = "rss-key",
+        };
+
+        // Act
+        var result = await TestHandlerExecuteAsync<TorznabMediaSearchResponseDTO>(command);
+
+        // Assert
+        result.IsSuccess.ShouldBeTrue();
+        result.Value.Channel.Response.Total.ShouldBe(0);
+        result.Value.Channel.Items.ShouldBeEmpty();
+    }
+
+    [Test]
+    public async Task ShouldReturnFilteredTotalBeforePaging()
+    {
+        // Arrange
+        await SetupDatabase(7638, config =>
+        {
+            config.PlexServerCount = 1;
+            config.PlexAccountCount = 1;
+            config.PlexMovieLibraryCount = 1;
+            config.MovieCount = 4;
+            config.RadarrIntegrationCount = 1;
+        });
+        var integration = (await IDbContext.RadarrIntegrations.SingleAsync(CancellationToken)).Id.ToRadarrIdentity();
+        var category = (await IDbContext.PlexMovieData.FirstAsync(CancellationToken)).ToTorznabMovieCategory();
+        var expectedTotal = (await IDbContext.PlexMovieData.ToListAsync(CancellationToken)).Count(x =>
+            x.ToTorznabMovieCategory() == category
+        );
+        var command = new GetTorznabRssFeedCommand
+        {
+            Integration = integration,
+            Categories = [category],
+            IncludeMovies = true,
+            IncludeEpisodes = false,
+            Limit = 1,
+            Offset = 1,
+            TorznabApiKey = "rss-key",
+        };
+
+        // Act
+        var result = await TestHandlerExecuteAsync<TorznabMediaSearchResponseDTO>(command);
+
+        // Assert
+        result.IsSuccess.ShouldBeTrue();
+        result.Value.Channel.Response.Total.ShouldBe(expectedTotal);
+        result.Value.Channel.Items.Count.ShouldBe(Math.Min(1, Math.Max(0, expectedTotal - 1)));
+    }
+
+    [Test]
+    public async Task ShouldReturnEmpty_WhenTvSearchUsesOnlyMovieCategories()
+    {
+        // Arrange
+        await SetupDatabase(7639, config =>
+        {
+            config.PlexServerCount = 1;
+            config.PlexAccountCount = 1;
+            config.PlexTvShowLibraryCount = 1;
+            config.TvShowCount = 1;
+            config.TvShowSeasonCount = 1;
+            config.TvShowEpisodeCount = 2;
+            config.SonarrIntegrationCount = 1;
+        });
+        var integration = (await IDbContext.SonarrIntegrations.SingleAsync(CancellationToken)).Id.ToSonarrIdentity();
+        var command = new GetTorznabRssFeedCommand
+        {
+            Integration = integration,
+            Categories = [(int)TorznabCategoryId.Movies],
+            IncludeMovies = false,
+            IncludeEpisodes = true,
+            Limit = 50,
+            Offset = 0,
+            TorznabApiKey = "rss-key",
+        };
+
+        // Act
+        var result = await TestHandlerExecuteAsync<TorznabMediaSearchResponseDTO>(command);
+
+        // Assert
+        result.IsSuccess.ShouldBeTrue();
+        result.Value.Channel.Response.Total.ShouldBe(0);
+        result.Value.Channel.Items.ShouldBeEmpty();
+    }
 }
